5df1827481abcd635da5d950c29942957708ade0
Austin Rovinski <rovinski@umich.edu>
2019-12-11 21:46:11 -0500

    Initial pdn integration

---
+++ flow/scripts/pdn.tcl
@@ -0,0 +1,15 @@
+if {![info exists standalone] || $standalone} {
+  # Read process files
+  read_lef $::env(OBJECTS_DIR)/merged.lef
+  
+  # Read design files
+  read_def $::env(RESULTS_DIR)/2_4_floorplan_macro.def
+}
+
+run_pdngen $::env(PDN_CFG)
+
+if {![info exists standalone] || $standalone} {
+  # write output
+  write_def $::env(RESULTS_DIR)/2_5_floorplan_pdn.def
+  exit
+}
