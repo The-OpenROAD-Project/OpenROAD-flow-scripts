923077fa7b120451d6947cd4f0321a5a70e89340
Austin Rovinski <rovinski@umich.edu>
2020-01-22 16:25:01 -0500

    Workaround for GDS DBU problem
    util/def2gds.py
      - Fix issue where GDS has incorrect scaling if DEF DBU differs from GDS DBU
      - This is a workaround, hopefully there is a cleaner solution
      - May not work if there are multiple GDSs and they have different DBUs

--- flow/util/def2gds.py
+++ flow/util/def2gds.py
@@ -19,8 +19,19 @@ with open(map_file) as mf:
     layer_map.map(s, index)
     index += 1
 
+# TODO(rovinski) If the GDS and LEF DB units are mismatched, the merged GDS is
+# incorrect because the GDS cell geometry is not scaled properly. I need to ask
+# what the proper way to do this is. In the meantime, we can get the GDS DBU and
+# use that to scale the LEF. Does not work if merging multiple GDSs with
+# different DBUs
+temp_layout = pya.Layout()
+temp_gds = in_gds.split()[0]
+print("INFO: Getting DBU from '" + temp_gds + "'")
+temp_layout.read(in_gds.split()[0])
+print("INFO: Using GDS DBU of " + str(temp_layout.dbu))
+
 # Load in the lef/def layout
-print("INFO: Opening LEF/DEF files...")
+print("INFO: Opening input files...")
 print("\tDEF: " + in_def)
 print("\tLEF: " + in_lef)
 
@@ -29,7 +40,7 @@ options = pya.LoadLayoutOptions()
 lefdef_options = pya.LEFDEFReaderConfiguration()
 lefdef_options.lef_files = [in_lef]
 lefdef_options.layer_map = layer_map
-lefdef_options.dbu = 0.0001
+lefdef_options.dbu = temp_layout.dbu
 options.lefdef_config = lefdef_options
 main_layout.read(in_def, options)
 
@@ -47,13 +58,16 @@ for i in range(main_layout.cells()):
 # Load in the gds to merge
 print("INFO: Merging gds files...")
 for gds in in_gds.split():
+  test = pya.Layout()
   print("\t" + gds)
+  test.read(gds)
+  main_layout.dbu = test.dbu
   main_layout.read(gds)
 
 # Copy the top level only to a new layout
 print("INFO: Copying toplevel cell '" + design_name + "'")
 top_only_layout = pya.Layout()
-top_only_layout.dbu = 0.0001
+top_only_layout.dbu = main_layout.dbu
 top = top_only_layout.create_cell(design_name)
 top.copy_tree(main_layout.cell(design_name))
 
