77c42d70cd6da30049012bf0c6bf182f8f9703c8
Tutu Ajayi <ajayi@umich.edu>
2020-02-13 09:42:38 -0500

    updating def2gds script

--- flow/util/def2gds.py
+++ flow/util/def2gds.py
@@ -1,48 +1,13 @@
 import pya
 
-# Create a layer map for conversion to gds
-layer_map = pya.LayerMap()
+# Load technology file
+tech = pya.Technology()
+tech.load(tech_file)
+layoutOptions = tech.load_layout_options
 
-# Manually parse layermap file
-# TODO(rovinski) find out if this can be read from the lyp file
-print("INFO: Opening layermap file: " + map_file)
-with open(map_file) as mf:
-  print("INFO: Mapping:")
-  index = 0
-  for line in mf:
-    s = line.strip()
-
-    # Comment or empty line
-    if s.startswith('#') or not s:
-      continue
-    print("\tlayer_map.map(" + s + ", " + str(index) + ")")
-    layer_map.map(s, index)
-    index += 1
-
-# TODO(rovinski) If the GDS and LEF DB units are mismatched, the merged GDS is
-# incorrect because the GDS cell geometry is not scaled properly. I need to ask
-# what the proper way to do this is. In the meantime, we can get the GDS DBU and
-# use that to scale the LEF. Does not work if merging multiple GDSs with
-# different DBUs
-temp_layout = pya.Layout()
-temp_gds = in_gds.split()[0]
-print("INFO: Getting DBU from '" + temp_gds + "'")
-temp_layout.read(in_gds.split()[0])
-print("INFO: Using GDS DBU of " + str(temp_layout.dbu))
-
-# Load in the lef/def layout
-print("INFO: Opening input files...")
-print("\tDEF: " + in_def)
-print("\tLEF: " + in_lef)
-
+# Load def file
 main_layout = pya.Layout()
-options = pya.LoadLayoutOptions()
-lefdef_options = pya.LEFDEFReaderConfiguration()
-lefdef_options.lef_files = [in_lef]
-lefdef_options.layer_map = layer_map
-lefdef_options.dbu = temp_layout.dbu
-options.lefdef_config = lefdef_options
-main_layout.read(in_def, options)
+main_layout.read(in_def, layoutOptions)
 
 # Clear cells
 top_cell_index = main_layout.cell(design_name).cell_index()
@@ -58,10 +23,7 @@ for i in range(main_layout.cells()):
 # Load in the gds to merge
 print("INFO: Merging gds files...")
 for gds in in_gds.split():
-  test = pya.Layout()
   print("\t" + gds)
-  test.read(gds)
-  main_layout.dbu = test.dbu
   main_layout.read(gds)
 
 # Copy the top level only to a new layout
