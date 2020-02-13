d744bf8cb220b0383394e0efb8bfcdb5d0b1bd1c
Tutu Ajayi <ajayi@umich.edu>
2020-02-13 09:44:27 -0500

    Adding gcd test for skywater

---
+++ flow/designs/skywater130/gcd.mk
@@ -0,0 +1,12 @@
+export DESIGN_NAME = gcd
+export PLATFORM    = skywater130
+
+export VERILOG_FILES = ./designs/src/gcd/gcd.v
+export SDC_FILE      = ./designs/src/gcd/gcd.sdc
+
+# These values must be multiples of placement site
+export DIE_AREA    = 0 0 199.96 200.128
+export CORE_AREA   = 9.996 10.08 189.964 190.048
+
+export CLOCK_PERIOD = 10.000
+export CLOCK_PORT   = clk
