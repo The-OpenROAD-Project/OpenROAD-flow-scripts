09ddbf2d490dde47d93b70030e50c2823af6cf78
Austin Rovinski <rovinski@umich.edu>
2020-04-09 15:01:07 -0400

    Update build script to have more options
    build_openroad.sh
      - Now accepts arguments; use --help to view
    setup_env_manual.sh
      - remove unnecessary file

--- build_openroad.sh
+++ build_openroad.sh
@@ -2,20 +2,82 @@
 # This script builds the OpenROAD tools locally
 
 # Exit on first error
-set -e
+set -ex
+
+function usage() {
+  echo "Usage: $0 [-h|--help] [-l|--latest] [-b|--branch BRANCH]"
+  echo "  -h, --help          print this help message"
+  echo "  -l, --latest        build using the head of branch 'openroad' for OpenROAD and TritonRoute"
+  echo "  -o, --local         force local build instead of docker build"
+  echo "  --or_branch BRANCH  build using the head of branch BRANCH for OpenROAD"
+  echo "  --tr_branch BRANCH  build using the head of branch BRANCH for TritonRoute"
+
+  echo "This script builds the openroad tools (OpenROAD, yosys, TritonRoute)"
+  echo "By default, the tools will be built from the linked submodule hashes"
+  echo "Using -b or -l will By default, the tools will be built from the linked submodule hashes"
+}
+
+# Parse arguments
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      usage
+      exit
+      ;;
+    --or_branch)
+      OR_BRANCH=$2
+      shift 2
+      ;;
+    --tr_branch)
+      TR_BRANCH=$2
+      shift 2
+      ;;
+    -l|--latest)
+      OR_BRANCH="openroad"
+      TR_BRANCH="openroad"
+      shift
+      break
+      ;;
+    -o|--local)
+      BUILD_METHOD="LOCAL"
+      shift
+      break
+      ;;
+    -n|--no_init)
+      NO_INIT=1
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "[ERROR][FLOW-1000] Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
 
 # Choose install method
-if which docker &> /dev/null; then
-  echo "INFO: using docker build method. This will create a docker image tagged 'openroad/flow'"
+if [ -z ${BUILD_METHOD+x} ] && which docker &> /dev/null; then
+  echo "[INFO][FLOW-0000] using docker build method. This will create a docker image tagged 'openroad/flow'"
   build_method="DOCKER"
 else
-  echo "INFO: using local build method. This will create binaries at tools/build/"
+  echo "[INFO][FLOW-0000] using local build method. This will create binaries at tools/build/"
   build_method="LOCAL"
 fi
 
 # Clone repositories
 if [ -z ${OPENROAD_FLOW_NO_GIT_INIT+x} ]; then
   git submodule update --init --recursive
+fi
+
+if [ ! -z ${OR_BRANCH+x} ]; then
+  (cd tools/OpenROAD && git checkout ${OR_BRANCH} && git pull && git submodule update --init --recursive)
+fi
+if [ ! -z ${TR_BRANCH+x} ]; then
+  (cd tools/TritonRoute && git checkout ${TR_BRANCH} && git pull)
 fi
 
 if [ -d flow/platforms/gf14 ]; then
