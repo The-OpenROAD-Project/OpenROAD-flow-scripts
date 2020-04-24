88df79bba2a939c4cb3716cc4bb469171a8a78d0
Colin Holehouse <colin.holehouse@arm.com>
2020-04-24 07:41:34 -0700

    Replace macros with wrapped version when necessary

--- flow/designs/gf14/bp_single.mk
+++ flow/designs/gf14/bp_single.mk
@@ -15,7 +15,13 @@ export ADDITIONAL_LEFS = ./platforms/gf14/bp/lef/gf14_1r1w_d32_w64_m1.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d512_w64_m2_byte.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d64_w124_m2_bit.lef \
                          ./platforms/gf14/bp/lef/gf14_1rw_d64_w62_m2_bit.lef \
-                         ./platforms/gf14/bp/lef/IN12LP_GPIO18_13M9S30P.lef
+                         ./platforms/gf14/bp/lef/IN12LP_GPIO18_13M9S30P.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1r1w_d32_w64_m1_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d128_w116_m2_bit_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d256_w48_m2_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d512_w64_m2_byte_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w124_m2_bit_mod.lef \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w62_m2_bit_mod.lef
 
 export ADDITIONAL_LIBS = ./platforms/gf14/bp/lib/gf14_1r1w_d32_w64_m1_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d128_w116_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
@@ -23,7 +29,13 @@ export ADDITIONAL_LIBS = ./platforms/gf14/bp/lib/gf14_1r1w_d32_w64_m1_tt_nominal
                          ./platforms/gf14/bp/lib/gf14_1rw_d512_w64_m2_byte_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d64_w124_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
                          ./platforms/gf14/bp/lib/gf14_1rw_d64_w62_m2_bit_tt_nominal_0p80v_0p80v_25c.lib \
-                         ./platforms/gf14/bp/lib/IN12LP_GPIO18_13M9S30P_TT_0P8_1P8_25.lib
+                         ./platforms/gf14/bp/lib/IN12LP_GPIO18_13M9S30P_TT_0P8_1P8_25.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1r1w_d32_w64_m1_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d128_w116_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d256_w48_m2_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d512_w64_m2_byte_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w124_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/gf14/bp/wrappers/gf14_1rw_d64_w62_m2_bit_mod_tt_nominal_0p80v_0p80v_25c.lib 
 
 export ADDITIONAL_GDS  = ./platforms/gf14/bp/gds/gf14_1r1w_d32_w64_m1.gds2 \
                          ./platforms/gf14/bp/gds/gf14_1rw_d128_w116_m2_bit.gds2 \
@@ -44,3 +56,6 @@ export SIG_MAP_FILE = ./platforms/gf14/bp/soc_bsg_black_parrot.sigmap
 export CLOCK_PERIOD = 5.600
 
 export PLACE_DENSITY = 0.20
+
+export MACRO_WRAPPERS = ./platforms/gf14/bp/wrappers/wrappers.tcl
+
