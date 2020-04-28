071d5e848f2c73be1189b29fefaedb35c0cb00c7
Matt Liberty <matt.liberty@gmail.com>
2020-04-28 11:06:00 -0700

    remove obsolete workaround

--- flow/scripts/tdms_place.tcl
+++ flow/scripts/tdms_place.tcl
@@ -35,10 +35,7 @@ proc find_macros {} {
 }
 
 if {[find_macros] != ""} {
-    #@workaround crash
-    if {$::env(DESIGN_NICKNAME)!="bp_single"} {
-	set_wire_rc -layer $::env(WIRE_RC_LAYER)
-    }
+    set_wire_rc -layer $::env(WIRE_RC_LAYER)
     global_placement -timing_driven -density $::env(PLACE_DENSITY)
 } else {
     puts "No macros found: Skipping global_placement"
