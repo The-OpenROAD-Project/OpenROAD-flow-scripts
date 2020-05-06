47db11fdb6951b846d65100b6c27bcbabaeb297f
mgwoo <mwoo@eng.ucsd.edu>
2020-05-05 20:13:32 -0700

    update tdms_place script for RD feature

--- flow/scripts/tdms_place.tcl
+++ flow/scripts/tdms_place.tcl
@@ -35,8 +35,11 @@ proc find_macros {} {
 }
 
 if {[find_macros] != ""} {
-    set_wire_rc -layer $::env(WIRE_RC_LAYER)
-    global_placement -timing_driven -density $::env(PLACE_DENSITY)
+    # 
+    # don't need set_wire_rc until TD is implemented!
+    #
+    # set_wire_rc -layer $::env(WIRE_RC_LAYER)
+    global_placement -density $::env(PLACE_DENSITY) -disable_routability_driven
 } else {
     puts "No macros found: Skipping global_placement"
 }
