0e66b1cc4fcd840c5ea88d524ec82ff3f7535c28
Tutu Ajayi <ajayi@umich.edu>
2020-05-27 20:45:44 -0400

    Defering compilation of verilog files in yosys to address non-deterministic behavior when file ordering is different

--- flow/scripts/synth.tcl
+++ flow/scripts/synth.tcl
@@ -17,13 +17,13 @@ if {[info exist ::env(VERILOG_INCLUDE_DIRS)]} {
 
 # read verilog files
 foreach file $::env(VERILOG_FILES) {
-  read_verilog -sv {*}$vIdirsArgs $file
+  read_verilog -defer -sv {*}$vIdirsArgs $file
 }
 
 
 # Read blackbox stubs of standard cells. This allows for standard cell (or
 # structural netlist) support in the input verilog
-read_verilog $::env(BLACKBOX_V_FILE)
+read_verilog -defer $::env(BLACKBOX_V_FILE)
 
 # Apply toplevel parameters (if exist)
 if {[info exist ::env(VERILOG_TOP_PARAMS)]} {
@@ -35,7 +35,7 @@ if {[info exist ::env(VERILOG_TOP_PARAMS)]} {
 
 # Read platform specific mapfile for OPENROAD_CLKGATE cells
 if {[info exist ::env(CLKGATE_MAP_FILE)]} {
-  read_verilog $::env(CLKGATE_MAP_FILE)
+  read_verilog -defer $::env(CLKGATE_MAP_FILE)
 }
 
 # Use hierarchy to automatically generate blackboxes for known memory macro.
