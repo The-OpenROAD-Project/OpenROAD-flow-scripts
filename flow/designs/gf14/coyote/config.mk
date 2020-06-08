12feac6be8b064b61f333fe4c59f5a738bb41f07
Austin Rovinski <rovinski@umich.edu>
2020-06-08 00:48:38 -0400

    Add gf14_coyote and gf14_swerv_wrapper designs

---
+++ flow/designs/gf14/coyote/config.mk
@@ -0,0 +1,36 @@
+export DESIGN_NICKNAME = coyote
+export DESIGN_NAME = bsg_rocket_node_client_rocc
+export PLATFORM    = gf14
+
+export VERILOG_FILES   = ./designs/src/$(DESIGN_NICKNAME)/coyote.sv2v.v \
+                         ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macros.v
+
+export SDC_FILE        = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export WRAP_LEFS       = ./platforms/$(PLATFORM)/lef/$(DESIGN_NICKNAME)/gf14_1rf_lg6_w80_bit.lef \
+                         ./platforms/$(PLATFORM)/lef/$(DESIGN_NICKNAME)/gf14_1rf_lg8_w128_all.lef \
+                         ./platforms/$(PLATFORM)/lef/$(DESIGN_NICKNAME)/gf14_2rf_lg6_w44_bit.lef \
+                         ./platforms/$(PLATFORM)/lef/$(DESIGN_NICKNAME)/gf14_2rf_lg8_w64_bit.lef
+
+export WRAP_LIBS       = ./platforms/$(PLATFORM)/lib/$(DESIGN_NICKNAME)/gf14_1rf_lg6_w80_bit_nn_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/$(PLATFORM)/lib/$(DESIGN_NICKNAME)/gf14_1rf_lg8_w128_all_nn_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/$(PLATFORM)/lib/$(DESIGN_NICKNAME)/gf14_2rf_lg6_w44_bit_nn_nominal_0p80v_0p80v_25c.lib \
+                         ./platforms/$(PLATFORM)/lib/$(DESIGN_NICKNAME)/gf14_2rf_lg8_w64_bit_nn_nominal_0p80v_0p80v_25c.lib
+
+export ADDITIONAL_GDS  = ./platforms/$(PLATFORM)/gds/$(DESIGN_NICKNAME)/gf14_1rf_lg6_w80_bit.gds2 \
+                         ./platforms/$(PLATFORM)/gds/$(DESIGN_NICKNAME)/gf14_1rf_lg8_w128_all.gds2 \
+                         ./platforms/$(PLATFORM)/gds/$(DESIGN_NICKNAME)/gf14_2rf_lg6_w44_bit.gds2 \
+                         ./platforms/$(PLATFORM)/gds/$(DESIGN_NICKNAME)/gf14_2rf_lg8_w64_bit.gds2
+
+# These values must be multiples of placement site
+export DIE_AREA    = 0 0 1100.008 1099.84
+export CORE_AREA   = 19.992 20.16 1080.016 1080.16
+
+export ABC_CLOCK_PERIOD_IN_PS = 1250
+
+export PLACE_DENSITY = 0.20
+
+export MACRO_WRAPPERS = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/wrappers.tcl
+
+# TODO: replace this with max(CHANNEL_WIDTH_[HV]) from IP_global.cfg
+export MACRO_BLOCKAGE_HALO = 25
