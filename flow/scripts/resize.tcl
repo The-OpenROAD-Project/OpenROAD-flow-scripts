f9414d661c3dacd0f3d94e5e9d46c153fe142fc7
Tom Spyrou <tspyrou@dfm.ucsd.edu>
2020-06-10 07:27:28 -0700

    update script to work if wire res/cap not set

--- flow/scripts/resize.tcl
+++ flow/scripts/resize.tcl
@@ -28,6 +28,8 @@ proc print_banner {header} {
 # Set res and cap
 if {[info exists ::env(WIRE_RC_RES)] && [info exists ::env(WIRE_RC_CAP)]} {
   set_wire_rc -res $::env(WIRE_RC_RES) -cap $::env(WIRE_RC_CAP)
+} else {
+  set_wire_rc -layer $::env(WIRE_RC_LAYER)
 }
 
 # pre report
