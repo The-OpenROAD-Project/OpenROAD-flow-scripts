789e25aac591e0a9672c7a7d4e31e7fab6ba3ada
Matt Liberty <matt.liberty@gmail.com>
2020-07-07 15:09:50 -0700

    use full_clock_expanded format and add nets & fanout fields to report_checks

--- flow/scripts/final_report.tcl
+++ flow/scripts/final_report.tcl
@@ -34,17 +34,17 @@ log_begin $::env(REPORTS_DIR)/6_final_report.rpt
 puts "\n=========================================================================="
 puts "report_checks -path_delay min"
 puts "--------------------------------------------------------------------------"
-report_checks -path_delay min -fields {slew cap input}
+report_checks -path_delay min -fields {slew cap input nets fanout} -format full_clock_expanded
 
 puts "\n=========================================================================="
 puts "report_checks -path_delay max"
 puts "--------------------------------------------------------------------------"
-report_checks -path_delay max -fields {slew cap input}
+report_checks -path_delay max -fields {slew cap input nets fanout} -format full_clock_expanded
 
 puts "\n=========================================================================="
 puts "report_checks -unconstrained"
 puts "--------------------------------------------------------------------------"
-report_checks -unconstrained
+report_checks -unconstrained -fields {slew cap input nets fanout} -format full_clock_expanded
 
 puts "\n=========================================================================="
 puts "report_tns"
