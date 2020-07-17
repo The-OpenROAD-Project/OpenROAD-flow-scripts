72308a378c9884a42c62acaf7dacc050e7b3a68c
eder-matheus <emrmonteiro@inf.ufrgs.br>
2020-07-17 17:15:06 -0300

    Update resize.tcl

--- flow/scripts/resize.tcl
+++ flow/scripts/resize.tcl
@@ -15,7 +15,12 @@ if {![info exists standalone] || $standalone} {
   }
 
   # Read def and sdc
-  read_def $::env(RESULTS_DIR)/3_1_place_iop.def
+  if {[info exist ::env(NON_RANDOM_IO_PLACEMENT)]} {
+    read_def $::env(RESULTS_DIR)/3_1_place_iop.def
+  } else {
+    read_def $::env(RESULTS_DIR)/3_1_place_gp.def
+  }
+
   read_sdc $::env(RESULTS_DIR)/2_floorplan.sdc
   if [file exists platforms/$::env(PLATFORM)/derate.tcl] {
     source platforms/$::env(PLATFORM)/derate.tcl
