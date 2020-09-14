0f96a65cf002e78f9e4bae7249bc8d9f0191891b
Mehdi Saligane <mehdi@vlsipool-j02.eecs.umich.edu>
2020-09-13 20:30:17 -0400

    FR updates and adding fixes for diode cell

--- flow/platforms/sky130/fastroute.tcl
+++ flow/platforms/sky130/fastroute.tcl
@@ -1,7 +1,7 @@
 fastroute -output_file $::env(RESULTS_DIR)/route.guide \
           -max_routing_layer $::env(MAX_ROUTING_LAYER) \
           -unidirectional_routing true \
-          -layers_adjustments {{2 0} {3 0.15} {4 0.15} {5 0.15} {6 0.15}}  \
+          -layers_adjustments {{2 0.5} {3 0.5} {4 0.5} {5 0.5} {6 0.5}}  \
           -overflow_iterations 100 \
 	  -layers_pitches {{2 0.37} {3 0.48} {4 0.74} {5 0.96} {6 3.33}} \
 	  -verbose 2 \
