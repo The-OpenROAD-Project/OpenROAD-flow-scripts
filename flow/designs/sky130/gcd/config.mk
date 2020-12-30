193ea9bc8c8432f46c6834081f22ecc95dc5bb07
Wenbo Duan <wbduan@vlsipool-j11.eecs.umich.edu>
2020-12-30 16:09:24 -0500

    update sky130 libs

--- flow/designs/sky130/gcd/config.mk
+++ flow/designs/sky130/gcd/config.mk
@@ -5,5 +5,5 @@ export VERILOG_FILES = ./designs/src/$(DESIGN_NICKNAME)/gcd.v
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
 # These values must be multiples of placement site
-export DIE_AREA    = 0 0 299.96 300.128
-export CORE_AREA   = 9.996 10.08 289.964 290.048
+export DIE_AREA    = 0 0 279.96 280.128
+export CORE_AREA   = 9.996 10.08 269.964 270.048
