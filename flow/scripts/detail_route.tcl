ac2320a91bf8aefd284b3e42413962f30007e7b4
Matt Liberty <matt.liberty@gmail.com>
2021-01-06 11:27:25 -0800

    load the lef/def in detail_route.tcl as we are now writing back to the database in drt

--- flow/scripts/detail_route.tcl
+++ flow/scripts/detail_route.tcl
@@ -1,5 +1,16 @@
-# Currently there is no need to populate opendb but that is
-# expected to change in the future.
+if {![info exists standalone] || $standalone} {
+  # Read lef
+  read_lef $::env(TECH_LEF)
+  read_lef $::env(SC_LEF)
+  if {[info exist ::env(ADDITIONAL_LEFS)]} {
+    foreach lef $::env(ADDITIONAL_LEFS) {
+      read_lef $lef
+    }
+  }
+
+  # Read design files
+  read_def $::env(RESULTS_DIR)/4_cts.def
+}
 
 detailed_route -param $::env(OBJECTS_DIR)/TritonRoute.param
 
