efe526f3bdedc121d3e07e0ee77dbcefbe6a9a26
Mehdi Saligane <mehdi@vlsipool-j10.eecs.umich.edu>
2021-01-21 17:43:11 -0500

    Splitting sky130 platform into 2 seperate SKY130HD and SKY130HS platforms

--- flow/platforms/sky130/config.mk
+++
@@ -1,9 +0,0 @@
-# Process node
-export PROCESS = 130
-export LIBRARY ?= hs
-
-
-# Rules for metal fill
-export FILL_CONFIG = ./platforms/$(PLATFORM)/fill.json
-
-include ./platforms/$(PLATFORM)/config_$(LIBRARY).mk 
