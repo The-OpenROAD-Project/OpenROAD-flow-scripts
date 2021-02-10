351e34846a81812ca8b11f5a15fa56da19566f0a
Matt Liberty <mliberty@eng.ucsd.edu>
2021-02-10 12:07:54 -0800

    use TRITON_ROUTE_LEF in the detailed routing step (prep for using opendb to populate TR)

--- flow/scripts/detail_route.tcl
+++ flow/scripts/detail_route.tcl
@@ -1,12 +1,6 @@
 if {![info exists standalone] || $standalone} {
   # Read lef
-  read_lef $::env(TECH_LEF)
-  read_lef $::env(SC_LEF)
-  if {[info exist ::env(ADDITIONAL_LEFS)]} {
-    foreach lef $::env(ADDITIONAL_LEFS) {
-      read_lef $lef
-    }
-  }
+  read_lef $::env(TRITON_ROUTE_LEF)
 
   # Read design files
   read_def $::env(RESULTS_DIR)/4_cts.def
