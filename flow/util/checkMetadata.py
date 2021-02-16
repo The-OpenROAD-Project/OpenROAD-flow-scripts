c79d63030b8210b006fc030ee8ae89bf3408ef18
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-02-16 21:34:13 +0000

    Add gold reference metadata

--- flow/util/checkMetadata.py
+++ flow/util/checkMetadata.py
@@ -34,10 +34,15 @@ parser.add_argument('--metadata', '-m', required=True,
                     help='The metadata file')
 parser.add_argument('--rules', '-r', required=True,
                     help='The rules file')
+parser.add_argument('--goldMetadata', '-g', required=True,
+                    help='The gold/reference metadata file')
 args = parser.parse_args()
 
 with open(args.metadata) as metadataFile:
     metadata = json.load(metadataFile)
+
+with open(args.goldMetadata) as goldMetadataFile:
+    referenceMetadata = json.load(goldMetadataFile)
 
 with open(args.rules) as rulesFile:
     rules = json.load(rulesFile)['rules']
@@ -55,6 +60,7 @@ ops = { "<" : operator.lt,
         ">=": operator.ge,
         "==": operator.eq,
         "!=": operator.ne,
+        "%" : "delta",
       }
 
 errors = False
@@ -66,14 +72,26 @@ for rule in rules:
     op = ops[compare]
     check_value = try_number(metadata[field])
 
+    if op == "delta":
+        reference_value = try_number(referenceMetadata[field])
+        if isinstance(check_value, float) != isinstance(reference_value, float):
+            errors = True
+            continue
+        percentage = (check_value - reference_value) / reference_value
+        print("check_value = {}, reference_value = {}, diff_percentage = {}%".format(
+            check_value, reference_value, percentage*100))
+        check_value = abs(percentage)
+        op = operator.le
+        compare = "(module value) should be less or equal than"
+
     if (isinstance(rule_value, float) != isinstance(check_value, float)
         or not op(check_value, rule_value)):
         errors = True
-        print 'Error: field {} fails rule {} {} {}'.format(field, check_value, compare, rule_value)
+        print('Error: field {} fails rule {} {} {}'.format(field, check_value, compare, rule_value))
     else:
-        print 'Passed: field {} passed rule {} {} {}'.format(field, check_value, compare, rule_value)
+        print('Passed: field {} passed rule {} {} {}'.format(field, check_value, compare, rule_value))
 
 if not errors:
-    print 'All metadata rules passed ({} rules)'.format(len(rules))
+    print('All metadata rules passed ({} rules)'.format(len(rules)))
 
 sys.exit(1 if errors else 0)
