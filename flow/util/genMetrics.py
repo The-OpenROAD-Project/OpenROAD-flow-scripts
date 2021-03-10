0a659ff7a4724c0c13934be43d69cdbfde687cf5
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-03-10 18:11:16 +0000

    metrics: correct file merge, always cast to float or string

--- flow/util/genMetrics.py
+++ flow/util/genMetrics.py
@@ -99,7 +99,7 @@ def extractGnuTime(prefix, file, jsonFile):
                      file)
   extractTagFromFile(prefix + "__mem__peak", jsonFile,
                      "^\S+elapsed \S+CPU (\S+)memKB",
-                     file, t=int)
+                     file)
 
 
 #
@@ -135,7 +135,7 @@ def extract_metrics(cwd, platform, design, output):
     logPath = os.path.join(cwd, "logs", platform, design)
     rptPath = os.path.join(cwd, "reports", platform, design)
     resultPath = os.path.join(cwd, "results", platform, design)
-    
+
     metrics_dict = {}
     metrics_dict["run__flow__generate__date"] = now.strftime("%Y-%m-%d %H:%M")
     cmdOutput = subprocess.check_output(['openroad', '-version'])
@@ -152,15 +152,15 @@ def extract_metrics(cwd, platform, design, output):
 
 # Synthesis
 # ==============================================================================
-# yosys
-#
+
     extractTagFromFile("synth__area__stdcell__count", metrics_dict,
                        "Number of cells: +(\S+)",
-                       rptPath+"/synth_stat.txt", t=int)
+                       rptPath+"/synth_stat.txt")
+
     extractTagFromFile("synth__area__stdcell__area", metrics_dict,
                        "Chip area for module.*: +(\S+)",
-                       rptPath+"/synth_stat.txt", t=float)
-#
+                       rptPath+"/synth_stat.txt")
+
 # Clocks
 #===============================================================================
 
@@ -170,31 +170,38 @@ def extract_metrics(cwd, platform, design, output):
 
 # Floorplan
 # ==============================================================================
+
     extractTagFromFile("floorplan__timing__tns_total", metrics_dict,
                        "^tns (\S+)",
-                       logPath+"/2_1_floorplan.log", t=float)
+                       logPath+"/2_1_floorplan.log")
+
     extractTagFromFile("floorplan__timing__wns__worst", metrics_dict,
                        "^wns (\S+)",
-                       logPath+"/2_1_floorplan.log", t=float)
+                       logPath+"/2_1_floorplan.log")
+
     extractTagFromFile("floorplan__area__stdcell__count", metrics_dict,
                        "^Design area (\S+) u\^2",
-                       logPath+"/2_1_floorplan.log", t=int)
+                       logPath+"/2_1_floorplan.log")
+
     extractTagFromFile("floorplan__area__instance__util", metrics_dict,
                        "^Design area.* (\S+%) utilization",
                        logPath+"/2_1_floorplan.log")
+
     extractTagFromFile("floorplan__area__IO__count", metrics_dict,
                        "Num of I/O +(\d+)",
-                       logPath+"/3_2_place_iop.log", t=int)
+                       logPath+"/3_2_place_iop.log")
+
     extractTagFromFile("floorplan__area__macros__count", metrics_dict,
                        "Extracted # Macros: (\S+)",
-                       logPath+"/2_4_mplace.log", -1, 0, t=int)
+                       logPath+"/2_4_mplace.log", defaultNotFound=0)
 
 # Place
 # ==============================================================================
-# global place
+
     extractTagFromFile("globalplace__area__density__target", metrics_dict,
                        "TargetDensity: (\S+)",
                        logPath+"/3_1_place_gp.log")
+
     extractTagFromFile("globalplace__area__wirelength__estimate", metrics_dict,
                        "Total wirelength: (\S+)",
                        logPath+"/3_1_place_gp.log")
@@ -203,22 +210,28 @@ def extract_metrics(cwd, platform, design, output):
 #
     extractTagFromFile("placeopt__area__inbuffer__count", metrics_dict,
                        "Inserted (\d+) input buffers",
-                       logPath+"/3_3_resizer.log", t=int)
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__area__outbuffer__count", metrics_dict,
                        "Inserted (\d+) output buffers",
-                       logPath+"/3_3_resizer.log", t=int)
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__area__resize__count", metrics_dict,
                        "Resized (\d+) instances",
-                       logPath+"/3_3_resizer.log", t=int)
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__timing__tns__total", metrics_dict,
                        "^tns (\S+)",
-                       logPath+"/3_3_resizer.log", occurrence=-1, t=float)
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__timing__wns__worst", metrics_dict,
                        "^wns (\S+)",
-                       logPath+"/3_3_resizer.log", occurrence=-1, t=float)
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__area__instance__area", metrics_dict,
-                       "^Design area (\S+ \S+)",
-                       logPath+"/3_3_resizer.log", occurrence=-1)
+                       "^Design area (\S+) u\^2",
+                       logPath+"/3_3_resizer.log")
+
     extractTagFromFile("placeopt__area__instance__util", metrics_dict,
                        "^Design area.* (\S+%) utilization",
                        logPath+"/3_3_resizer.log")
@@ -226,28 +239,34 @@ def extract_metrics(cwd, platform, design, output):
 # Detail place
     extractTagFromFile("detailedplace__inst__displacement__total", metrics_dict,
                        "total displacement +(\d*\.?\d*)",
-                       logPath+"/3_4_opendp.log", t=float)
+                       logPath+"/3_4_opendp.log")
+
     extractTagFromFile("detailedplace__inst__displacement__average", metrics_dict,
                        "average displacement +(\d*\.?\d*)",
-                       logPath+"/3_4_opendp.log", t=float)
+                       logPath+"/3_4_opendp.log")
+
     extractTagFromFile("detailedplace__inst__displacement__max", metrics_dict,
                        "max displacement +(\d*\.?\d*)",
-                       logPath+"/3_4_opendp.log", t=float)
+                       logPath+"/3_4_opendp.log")
+
     extractTagFromFile("detailedplace__wirelength__initial__estimate", metrics_dict,
                        "original HPWL +(\d*\.?\d*)",
-                       logPath+"/3_4_opendp.log", t=float)
+                       logPath+"/3_4_opendp.log")
+
     extractTagFromFile("detailedplace__wirelength__final__estimate", metrics_dict,
                        "legalized HPWL +(\d*\.?\d*)",
-                       logPath+"/3_4_opendp.log", t=float)
+                       logPath+"/3_4_opendp.log")
+
 # CTS
 # ==============================================================================
+
     extractTagFromFile("cts__timing__slack__tns", metrics_dict,
                        "^tns (\S+)",
-                       logPath+"/4_1_cts.log", t=float)
+                       logPath+"/4_1_cts.log")
 
     extractTagFromFile("cts__timing__slack__wns", metrics_dict,
                        "^wns (\S+)",
-                       logPath+"/4_1_cts.log", t=float)
+                       logPath+"/4_1_cts.log")
 
 # Route
 # ==============================================================================
@@ -255,10 +274,12 @@ def extract_metrics(cwd, platform, design, output):
 
     extractTagFromFile("detailedroute__wirelength", metrics_dict,
                        "total wire length = +(\S+) um",
-                       logPath+"/5_2_TritonRoute.log", t=int)
+                       logPath+"/5_2_TritonRoute.log")
+
     extractTagFromFile("detailedroute__via__count", metrics_dict,
                        "total number of vias = +(\S+)",
-                       logPath+"/5_2_TritonRoute.log", t=int)
+                       logPath+"/5_2_TritonRoute.log")
+
     extractTagFromFile("detailedroute__errors__count", metrics_dict,
                        "(?i)error:",
                        logPath+"/5_2_TritonRoute.log",
@@ -274,23 +295,24 @@ def extract_metrics(cwd, platform, design, output):
 
     extractTagFromFile("finish__power__internal__total", metrics_dict,
                        "Total +(\S+) +\S+ +\S+ +\S+ +\S+",
-                       logPath+"/6_report.log", t=float)
+                       logPath+"/6_report.log")
 
     extractTagFromFile("finish__power__switch__total", metrics_dict,
                        "Total +\S+ +(\S+) +\S+ +\S+ +\S+",
-                       logPath+"/6_report.log", t=float)
+                       logPath+"/6_report.log")
 
     extractTagFromFile("finish__power__leak__total", metrics_dict,
                        "Total +\S+ +\S+ +(\S+) +\S+ +\S+",
-                       logPath+"/6_report.log", t=float)
+                       logPath+"/6_report.log")
 
     extractTagFromFile("finish__power__total", metrics_dict,
                        "Total +\S+ +\S+ +\S+ +(\S+) +\S+",
-                       logPath+"/6_report.log", t=float)
+                       logPath+"/6_report.log")
 
     extractTagFromFile("finish__area", metrics_dict,
                        "^Design area (\S+ \S+)",
                        logPath+"/6_report.log")
+
     extractTagFromFile("finish__util", metrics_dict,
                        "^Design area.* (\S+%) utilization",
                        logPath+"/6_report.log")
@@ -336,8 +358,6 @@ def extract_metrics(cwd, platform, design, output):
     return metrics_dict, metrics_df
 
 
-
-
 args = parse_args()
 now = datetime.datetime.now()
 
@@ -373,4 +393,3 @@ if args.design == "all_designs":
     metrics_html_file.close()
 else:
     metrics_dict, metrics_df = extract_metrics(args.flowPath, args.platform, args.design, args.output)
-
