9729ba09e7a1c2a6009bc1f6e688cbc813e40874
louiic <pchaiyakul@dfm.ucsd.edu>
2021-03-10 18:10:07 -0800

    Add more files to complete bring up

---
+++ flow/platforms/asap7/make_tracks.tcl
@@ -0,0 +1,19 @@
+make_tracks Pad -x_offset 0.116 -x_pitch 0.08  -y_offset 0.116 -y_pitch 0.08
+make_tracks M9  -x_offset 0.116 -x_pitch 0.08  -y_offset 0.116 -y_pitch 0.08
+make_tracks M8  -x_offset 0.116 -x_pitch 0.08  -y_offset 0.116 -y_pitch 0.08
+make_tracks M7  -x_offset 0.036 -x_pitch 0.064 -y_offset 0.036 -y_pitch 0.064
+make_tracks M6  -x_offset 0.048 -x_pitch 0.048 -y_offset 0.036 -y_pitch 0.064
+make_tracks M5  -x_offset 0.048 -x_pitch 0.048 -y_offset 0.027 -y_pitch 0.048
+make_tracks M4  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.027 -y_pitch 0.048
+make_tracks M3  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.036 -y_pitch 0.036
+
+puts "Creating M2 track"
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.315 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.351 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.387 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.423 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.459 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.495 -y_pitch 0.27
+make_tracks M2  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.27  -y_pitch 0.27
+
+make_tracks M1  -x_offset 0.036 -x_pitch 0.036 -y_offset 0.036 -y_pitch 0.036
