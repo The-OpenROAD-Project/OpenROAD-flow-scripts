904f0d9718d30d3e6a5cea75cd9b6806491b40c7
Geraldo Pradipta <pradi001@umn.edu>
2021-03-12 14:12:35 -0800

    add note -order_wires is required for RCX

--- flow/scripts/final_report.tcl
+++ flow/scripts/final_report.tcl
@@ -16,6 +16,7 @@ if {![info exists standalone] || $standalone} {
   # Read def and sdc
   # Use -order_wires to build wire graph
   # for antenna checker read_def -order_wires $::env(RESULTS_DIR)/6_1_fill.def
+  # -order_wires flag is REQUIRED to run RCX
   read_def $::env(RESULTS_DIR)/6_1_fill.def
   read_sdc $::env(RESULTS_DIR)/6_1_fill.sdc
 } else {
