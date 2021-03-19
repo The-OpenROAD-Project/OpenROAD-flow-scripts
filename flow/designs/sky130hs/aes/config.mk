d4db7e1235432628c1d5a292093b8fab98e1f6e2
VidyaChhabria <chhab011@umn.edu>
2021-03-19 13:42:36 -0700

    Defining power net and voltage

--- flow/designs/sky130hs/aes/config.mk
+++ flow/designs/sky130hs/aes/config.mk
@@ -9,3 +9,9 @@ export RCX_RULES     = $(PLATFORM_DIR)/rcx_patterns.rules
 # These values must be multiples of placement site
 export DIE_AREA    = 0 0 1120 1020.8
 export CORE_AREA   = 10 12 1110 1011.2
+
+# IR drop estimation supply net name to be analyzed and supply voltage variable
+export VDD_NET_NAME  = VDD
+export VSS_NET_NAME  = VSS
+export VDD_VOLTAGE   = 1.8
+export VSS_VOLTAGE   = 0.0
--- flow/designs/sky130hs/gcd/config.mk
+++ flow/designs/sky130hs/gcd/config.mk
@@ -8,3 +8,9 @@ export RCX_RULES     = $(PLATFORM_DIR)/rcx_patterns.rules
 # These values must be multiples of placement site
 export DIE_AREA    = 0 0 279.96 280.128
 export CORE_AREA   = 9.996 10.08 269.964 270.048
+
+# IR drop estimation supply net name to be analyzed and supply voltage variable
+export VDD_NET_NAME  = VDD
+export VSS_NET_NAME  = VSS
+export VDD_VOLTAGE   = 1.8
+export VSS_VOLTAGE   = 0.0
