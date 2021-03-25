2c06b8a2f0f42b5bad2bb2be3a5180e8638bfc52
louiic <pchaiyakul@dfm.ucsd.edu>
2021-03-25 07:59:12 -0700

    Recapture ASAP7 platform   - reorgainze data   - extract only necessary files from ASAP7 repository for OpenRoad pacakage

---
+++ flow/platforms/asap7/yoSys/blackbox_map.tcl
@@ -0,0 +1,13 @@
+hierarchy -generate asap7_* o:Q o:QA o:QB \
+                               i:CLK i:CLKA i:CLKB \
+                               i:CEN i:CENA i:CENB \
+                               i:GWEN \
+                               i:A i:AA i:AB \
+                               i:D i:DB \
+                               i:WEN i:WENA i:WENB\
+                               i:STOV \
+                               i:EMA i:EMAA i:EMAB \
+                               i:EMAW i:EMAS i:RET1N \
+                               i:SE i:TEN o:CENY o:WENY o:AY \
+                               o:SO i:SI i:TCEN i:TWEN i:TA i:TD \
+                               i:DFTRAMBYP i:PGEN i:KEN i:BEN i:TQ
