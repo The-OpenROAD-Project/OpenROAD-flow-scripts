de2f65c6b8a90a5bb2b6b6f27e5970b7ec5137cc
Peter Gadfort <peter.gadfort.civ@mail.mil>
2021-04-14 12:01:09 -0400

    correcting typo in klayout.tcl

--- flow/scripts/klayout.tcl
+++ flow/scripts/klayout.tcl
@@ -14,7 +14,7 @@ exec klayout -zz -rd design_name=$::env(DESIGN_NAME) \
            -rd in_def=$::env(RESULTS_DIR)/6_final.def \
            -rd in_files="$::env(GDSOAS_FILES) $::env(WRAPPED_GDSOAS)" \
            -rd config_file=$fill_config \
-           -rd sealf_file=$seal_gds \
+           -rd seal_file=$seal_gds \
            -rd out_file=$::env(RESULTS_DIR)/6_final.$::env(STREAM_SYSTEM_EXT) \
            -rd tech_file=$::env(OBJECTS_DIR)/klayout.lyt \
            -rm $::env(UTILS_DIR)/def2stream.py
