463afd8e4f26426c2909982b31e4095b341ae98e
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2021-04-22 12:33:27 -0700

    PPA changes to tune fast route layer settings for sky130 platform

--- flow/designs/sky130hs/ibex/config_ppa.mk
+++ flow/designs/sky130hs/ibex/config_ppa.mk
@@ -1,7 +1,9 @@
 include $(dir $(DESIGN_CONFIG))/config.mk
 
-export FLOW_VARIANT = ppa
+export FLOW_VARIANT = ppa_pr
 
 export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 2
 export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2
+
+export FASTROUTE_TCL = $(PLATFORM_DIR)/fastroute_ppa.tcl
 
