00de6791fdcda368a536ec9f92a6d98632fd45a3
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2021-04-22 12:57:01 -0700

    Typo change flow variant name back to ppa

--- flow/designs/sky130hd/aes/config_ppa.mk
+++ flow/designs/sky130hd/aes/config_ppa.mk
@@ -1,6 +1,6 @@
 include $(dir $(DESIGN_CONFIG))/config.mk
 
-export FLOW_VARIANT = ppa_pr
+export FLOW_VARIANT = ppa
 
 export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 3
 export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2
