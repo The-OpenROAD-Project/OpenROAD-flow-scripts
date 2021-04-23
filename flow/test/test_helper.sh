3f3e229ad1cd7e428aa756ee121eaeb0c9eef956
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-04-22 23:12:43 -0700

    test: allow script to be called from anywhere

--- flow/test/test_helper.sh
+++ flow/test/test_helper.sh
@@ -4,23 +4,23 @@
 DESIGN_NAME=${1:-gcd}
 PLATFORM=${2:-nangate45}
 CONFIG_MK=${3:-config.mk}
+DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK
 
-# TODO: Currently assumes this script must be called from the flowdir.
-TEST_DIR=${TEST_DIR:-test}
+cd "$(dirname $(readlink -f $0))/../"
 
 if [ -z "$DESIGN_NICKNAME" ]
 then
-  LOG_FILE=$TEST_DIR/logs/$PLATFORM/$DESIGN_NAME.log
-  GOLD_LOG_FILE=$TEST_DIR/gold_logs/$PLATFORM/$DESIGN_NAME.log
+  LOG_FILE=./logs/$PLATFORM/$DESIGN_NAME.log
+  GOLD_LOG_FILE=./gold_logs/$PLATFORM/$DESIGN_NAME.log
 else
-  LOG_FILE=$TEST_DIR/logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
-  GOLD_LOG_FILE=$TEST_DIR/gold_logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
+  LOG_FILE=./logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
+  GOLD_LOG_FILE=./gold_logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
 fi
 
 
 set -o pipefail
 
-mkdir -p $TEST_DIR/logs/$PLATFORM
+mkdir -p ./logs/$PLATFORM
 
 if [ -f "../../private_tool_scripts/util/utils.mk" ]; then
   TARGETS="finish metadata drc_calibre"
@@ -28,12 +28,12 @@ else
   TARGETS="finish metadata"
 fi
 
-make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK clean_all clean_metadata
+make DESIGN_CONFIG=$DESIGN_CONFIG clean_all clean_metadata
 
 # turn off abort on error so we can always capture the result
 set +e
 
-make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK $TARGETS 2>&1 | tee $LOG_FILE
+make DESIGN_CONFIG=$DESIGN_CONFIG $TARGETS 2>&1 | tee $LOG_FILE
 
 # Save the return code to return as the overall status after we package
 # the results
@@ -41,7 +41,7 @@ ret=$?
 set -e
 
 if [ ! -z ${MAKE_ISSUE+x} ]; then
-  make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK final_report_issue
+  make DESIGN_CONFIG=$DESIGN_CONFIG final_report_issue
 fi
 
 exit $ret
