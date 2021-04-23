69b61d2d48df5095ceb85bab9b33edc08c05c248
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-04-22 23:10:57 -0700

    test: rename DESIGN_CONFIG -> DESIGN_NAME

--- flow/test/test_helper.sh
+++ flow/test/test_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 # Setting args (and setting default values for testing)
-DESIGN_CONFIG=${1:-gcd}
+DESIGN_NAME=${1:-gcd}
 PLATFORM=${2:-nangate45}
 CONFIG_MK=${3:-config.mk}
 
@@ -10,8 +10,8 @@ TEST_DIR=${TEST_DIR:-test}
 
 if [ -z "$DESIGN_NICKNAME" ]
 then
-  LOG_FILE=$TEST_DIR/logs/$PLATFORM/$DESIGN_CONFIG.log
-  GOLD_LOG_FILE=$TEST_DIR/gold_logs/$PLATFORM/$DESIGN_CONFIG.log
+  LOG_FILE=$TEST_DIR/logs/$PLATFORM/$DESIGN_NAME.log
+  GOLD_LOG_FILE=$TEST_DIR/gold_logs/$PLATFORM/$DESIGN_NAME.log
 else
   LOG_FILE=$TEST_DIR/logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
   GOLD_LOG_FILE=$TEST_DIR/gold_logs/$PLATFORM/$DESIGN/$DESIGN_NICKNAME.log
@@ -28,12 +28,12 @@ else
   TARGETS="finish metadata"
 fi
 
-make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_CONFIG/$CONFIG_MK clean_all clean_metadata
+make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK clean_all clean_metadata
 
 # turn off abort on error so we can always capture the result
 set +e
 
-make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_CONFIG/$CONFIG_MK $TARGETS 2>&1 | tee $LOG_FILE
+make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK $TARGETS 2>&1 | tee $LOG_FILE
 
 # Save the return code to return as the overall status after we package
 # the results
@@ -41,7 +41,7 @@ ret=$?
 set -e
 
 if [ ! -z ${MAKE_ISSUE+x} ]; then
-  make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_CONFIG/$CONFIG_MK final_report_issue
+  make -C $TEST_DIR/.. DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK final_report_issue
 fi
 
 exit $ret
