4d27430eacdb8d6e4e0d06c4b9e189470d477690
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-04-24 10:13:01 -0700

    test: use FLOW_VARIANT

--- flow/test/test_helper.sh
+++ flow/test/test_helper.sh
@@ -4,6 +4,7 @@
 DESIGN_NAME=${1:-gcd}
 PLATFORM=${2:-nangate45}
 CONFIG_MK=${3:-config.mk}
+FLOW_VARIANT=${4:-base}
 DESIGN_CONFIG=./designs/$PLATFORM/$DESIGN_NAME/$CONFIG_MK
 
 cd "$(dirname $(readlink -f $0))/../"
@@ -28,12 +29,12 @@ else
   TARGETS="finish metadata"
 fi
 
-make DESIGN_CONFIG=$DESIGN_CONFIG clean_all clean_metadata
+make DESIGN_CONFIG=$DESIGN_CONFIG FLOW_VARIANT=$FLOW_VARIANT clean_all clean_metadata
 
 # turn off abort on error so we can always capture the result
 set +e
 
-make DESIGN_CONFIG=$DESIGN_CONFIG $TARGETS 2>&1 | tee $LOG_FILE
+make DESIGN_CONFIG=$DESIGN_CONFIG FLOW_VARIANT=$FLOW_VARIANT $TARGETS 2>&1 | tee $LOG_FILE
 
 # Save the return code to return as the overall status after we package
 # the results
@@ -41,7 +42,7 @@ ret=$?
 set -e
 
 if [ ! -z ${MAKE_ISSUE+x} ]; then
-  make DESIGN_CONFIG=$DESIGN_CONFIG final_report_issue
+  make DESIGN_CONFIG=$DESIGN_CONFIG FLOW_VARIANT=$FLOW_VARIANT final_report_issue
 fi
 
 exit $ret
