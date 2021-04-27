3f727ec10a422c68332ba71b39ca89a3ddb1a787
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2021-04-27 10:09:31 -0700

    Tigted utilization for the sky 130 designs

--- flow/designs/sky130hd/aes/config_ppa.mk
+++ flow/designs/sky130hd/aes/config_ppa.mk
@@ -1,9 +1,13 @@
 include $(dir $(DESIGN_CONFIG))/config.mk
 
 export FLOW_VARIANT = ppa
+#
+# These values must be multiples of placement site  0.46 x 2.72
+export DIE_AREA    = 0 0  760 760 
+export CORE_AREA   = 10 10 750.14 752.56
 
-export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 3
-export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2
+export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 3 
+export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2 
 
 export FASTROUTE_TCL = $(PLATFORM_DIR)/fastroute_ppa.tcl
 
--- flow/designs/sky130hd/jpeg/config_ppa.mk
+++ flow/designs/sky130hd/jpeg/config_ppa.mk
@@ -2,7 +2,11 @@ include $(dir $(DESIGN_CONFIG))/config.mk
 
 export FLOW_VARIANT = ppa
 
-export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 3
+# These values must be multiples of placement site 0.46 x 2.72
+export DIE_AREA    = 0 0 1400 1400
+export CORE_AREA   = 10. 10 1390 1391.76
+
+export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT = 4
 export CELL_PAD_IN_SITES_DETAIL_PLACEMENT = 2
 
 export FASTROUTE_TCL = $(PLATFORM_DIR)/fastroute_ppa.tcl
