605d0748e82c020eda92fe694075309ebd33ec8c
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-05-04 15:00:29 -0700

    metrics: remove var

--- flow/util/checkMetadata.py
+++ flow/util/checkMetadata.py
@@ -94,8 +94,7 @@ for _, rule in rules.items():
         errors += 1
         continue
 
-    hasError = op(build_value, rule_value)
-    if hasError:
+    if op(build_value, rule_value)
         pre = '[INFO]'
         check = 'pass'
     else:
