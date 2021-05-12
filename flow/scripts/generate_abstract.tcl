7d2d20035f1024851c1343d167d8750e17aedbb5
ibrahimkhairy <ibrahimkhairy@ymail.com>
2021-05-12 14:45:15 +0000

    updating hierarchical flow targets

--- flow/scripts/generate_abstract.tcl
+++ flow/scripts/generate_abstract.tcl
@@ -18,21 +18,16 @@ if {![info exists standalone] || $standalone} {
   # for antenna checker read_def -order_wires $::env(RESULTS_DIR)/6_1_fill.def
   read_def $::env(RESULTS_DIR)/6_1_fill.def
   puts "Starting genration of abstract views"
-# echo "ADDITIONAL_LEFS += $::env(RESULTS_DIR)/$::env(DESIGN_NICKNAME).lef" > $::env(RESULTS_DIR)/ADDITIONAL_VIEWS.mk
-# echo "ADDITIONAL_LIBS += $::env(RESULTS_DIR)/$::env(DESIGN_NICKNAME).lib" >> $::env(RESULTS_DIR)/ADDITIONAL_VIEWS.mk
     
  source scripts/generate_lef.tcl 
 } else {
   puts "Starting genration of abstract views"
-  
- # echo "ADDITIONAL_LEFS += $::env(RESULTS_DIR)/$::env(DESIGN_NICKNAME).lef" > $::env(RESULTS_DIR)/ADDITIONAL_VIEWS.mk
- # echo "ADDITIONAL_LIBS += $::env(RESULTS_DIR)/$::env(DESIGN_NICKNAME).lib" >> $::env(RESULTS_DIR)/ADDITIONAL_VIEWS.mk
 }
 
 
 source scripts/generate_lef.tcl
 #source scripts/generate_libs.tcl
-#
-#if {![info exists standalone] || $standalone} {
-#  exit
-#}
+
+if {![info exists standalone] || $standalone} {
+  exit
+}
