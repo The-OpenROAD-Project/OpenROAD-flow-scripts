a433812933ce90d49a4667fdc5ffc948d359f5c9
Peter Gadfort <peter.gadfort.civ@mail.mil>
2021-05-14 12:59:43 -0400

    removing adder flow from GCD designs

--- flow/designs/sky130hd/gcd/config.mk
+++ flow/designs/sky130hd/gcd/config.mk
@@ -5,6 +5,9 @@ export VERILOG_FILES = ./designs/src/$(DESIGN_NICKNAME)/gcd.v
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 export RCX_RULES     = $(PLATFORM_DIR)/rcx_patterns.rules
 
+# Adders degrade GCD
+export ADDER_MAP_FILE :=
+
 # These values must be multiples of placement site
 export DIE_AREA    = 0 0 279.96 280.128
 export CORE_AREA   = 9.996 10.08 269.964 270.048
