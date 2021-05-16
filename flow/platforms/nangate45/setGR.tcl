4cfd705aba4784cea1afaf434256d522c58f1993
mgwoo <mwoo@eng.ucsd.edu>
2021-05-15 21:21:52 -0700

    add setGR.tcl for sharing the GR's config
    1. change OpenROAD commit pointer to merged master (GPL-RD is
    integrated)
    2. add setGR.tcl for ng45, sky130.
    3. source setGR.tcl in global_place.tcl

---
+++ flow/platforms/nangate45/setGR.tcl
@@ -0,0 +1,6 @@
+set_global_routing_layer_adjustment metal2 0.8
+set_global_routing_layer_adjustment metal3 0.7
+set_global_routing_layer_adjustment metal4-$::env(MAX_ROUTING_LAYER) 0.4
+
+set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
+set_macro_extension 2
--- flow/scripts/global_place.tcl
+++ flow/scripts/global_place.tcl
@@ -28,6 +28,11 @@ if {![info exists standalone] || $standalone} {
 source $::env(PLATFORM_DIR)/setRC.tcl
 set_dont_use $::env(DONT_USE_CELLS)
 
+# set global router for -routability_driven
+if [file exists $::env(PLATFORM_DIR)/setGR.tcl] {
+  source $::env(PLATFORM_DIR)/setGR.tcl
+}
+
 if { 0 != [llength [array get ::env GLOBAL_PLACEMENT_ARGS]] } {
 global_placement -routability_driven -density $::env(PLACE_DENSITY) \
     -pad_left $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
