a8d79b14ced4b30c7414ce519be57155c7e482be
mgwoo <mwoo@eng.ucsd.edu>
2021-05-22 22:45:56 -0700

    add else case (global_place.tcl, global_route.tcl)

--- flow/scripts/global_place.tcl
+++ flow/scripts/global_place.tcl
@@ -31,6 +31,10 @@ set_dont_use $::env(DONT_USE_CELLS)
 # set fastroute layer reduction
 if {[info exist env(FASTROUTE_TCL)]} {
   source $env(FASTROUTE_TCL)
+} else {
+  set_global_routing_layer_adjustment $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER) 0.5
+  set_routing_layers -signal $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER)
+  set_macro_extension 2
 }
 
 if { 0 != [llength [array get ::env GLOBAL_PLACEMENT_ARGS]] } {
