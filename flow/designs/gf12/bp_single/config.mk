6349f76350873b027c013019e4b9e46bee32c590
louiic <pchaiyakul@dfm.ucsd.edu>
2021-05-30 06:57:37 -0700

    Add Calibre setting to designs' DESIGN_TYPE

--- flow/designs/gf12/bp_single/config.mk
+++ flow/designs/gf12/bp_single/config.mk
@@ -63,3 +63,5 @@ export PDN_CFG ?= $(PLATFORM_DIR)/cfg/pdn_grid_strategy_13m_9T.top.cfg
 
 # TODO: replace this with max(CHANNEL_WIDTH_[HV]) from IP_global.cfg
 export MACRO_BLOCKAGE_HALO = 25
+
+export DESIGN_TYPE = CHIP
