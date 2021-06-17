5f5b921c1d16bea0414e3f6c3450d4f3bc78a26a
Ashton Snelgrove <ashton.snelgrove@utah.edu>
2021-06-17 10:35:27 -0600

    Update to current version of LSOracle

--- build_openroad.sh
+++ build_openroad.sh
@@ -140,7 +140,7 @@ elif [ "$build_method" == "LOCAL" ]; then
   (cd tools/build/OpenROAD && cmake ../../OpenROAD && $NICE make -j$PROC)
 
   mkdir -p tools/build/LSOracle
-  cmake -B tools/build/LSOracle tools/LSOracle -D CMAKE_BUILD_TYPE=RELEASE -D YOSYS_SHARE_DIR=$(pwd)/tools/yosys -D YOSYS_PLUGIN=ON
+  cmake -B tools/build/LSOracle tools/LSOracle -D CMAKE_BUILD_TYPE=RELEASE -D YOSYS_INCLUDE_DIR=$(pwd)/tools/yosys -D YOSYS_PLUGIN=ON
   cmake --build tools/build/LSOracle -j$PROC
   mkdir -p tools/build/yosys/share/yosys/plugins
   cp tools/build/LSOracle/yosys-plugin/oracle.so tools/build/yosys/share/yosys/plugins/
