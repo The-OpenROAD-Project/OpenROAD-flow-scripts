6c1bbf89038767f7bdb0fda81f3654aca2ec3b77
Austin Rovinski <rovinski@umich.edu>
2021-10-19 18:46:15 -0400

    [asap7] New setRC values based on cross-platform calibration
    Signed-off-by: Austin Rovinski <rovinski@umich.edu>

--- flow/platforms/asap7/setRC.tcl
+++ flow/platforms/asap7/setRC.tcl
@@ -1,11 +1,13 @@
 # Liberty units are fF,kOhm
-set_layer_rc -layer M1 -capacitance 1.1368e-01 -resistance 1.3889e-01
-set_layer_rc -layer M2 -capacitance 1.3426e-01 -resistance 2.4222e-02
-set_layer_rc -layer M3 -capacitance 1.2918e-01 -resistance 2.4222e-02
-set_layer_rc -layer M4 -capacitance 1.1396e-01 -resistance 1.6778e-02
-set_layer_rc -layer M5 -capacitance 1.3323e-01 -resistance 1.4677e-02
-set_layer_rc -layer M6 -capacitance 1.1575e-01 -resistance 1.0371e-02
-set_layer_rc -layer M7 -capacitance 1.3293e-01 -resistance 9.6720e-03
+set_layer_rc -layer M1 -capacitance 6.33956E-02 -resistance 1.3889e-01
+set_layer_rc -layer M2 -capacitance 8.02750E-02 -resistance 2.4222e-02
+set_layer_rc -layer M3 -capacitance 1.23273E-01 -resistance 2.4222e-02
+set_layer_rc -layer M4 -capacitance 1.29902E-01 -resistance 1.6778e-02
+set_layer_rc -layer M5 -capacitance 1.19552E-01 -resistance 1.4677e-02
+set_layer_rc -layer M6 -capacitance 1.23114E-01 -resistance 1.0371e-02
+set_layer_rc -layer M7 -capacitance 1.05408E-01 -resistance 9.6720e-03
+
+# No correlation data is available for layers M8 and M9
 set_layer_rc -layer M8 -capacitance 1.1822e-01 -resistance 7.4310e-03
 set_layer_rc -layer M9 -capacitance 1.3497e-01 -resistance 6.8740e-03
 
