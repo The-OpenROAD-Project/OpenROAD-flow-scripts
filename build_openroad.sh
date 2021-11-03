6b02ceea71c786e113929cf22a6bac83ad27401b
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-11-03 14:45:58 -0300

    Move steps into functions
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- build_openroad.sh
+++ build_openroad.sh
@@ -92,45 +92,8 @@ while (( "$#" )); do
   esac
 done
 
-# Choose install method
-if [ -z ${BUILD_METHOD+x} ] && command -v docker &> /dev/null; then
-  echo "[INFO FLW-0000] Using docker build method. This will create a docker image tagged '${DOCKER_TAG}'."
-  build_method="DOCKER"
-else
-  echo "[INFO FLW-0000] Using local build method. This will create binaries at 'tools/install/..'."
-  build_method="LOCAL"
-fi
-
-# Clone repositories
-if [ -z ${OPENROAD_FLOW_NO_GIT_INIT+x} ]; then
-  git submodule update --init --recursive
-fi
-
-
-if [ ! -z ${OR_REPO+x} ]; then
-  base_url=$(dirname $OR_REPO)
-  [[ ${base_url##*/} = $base_url ]] && CURRENT_REMOTE=${base_url##*:} || CURRENT_REMOTE=${base_url##*/}
-  remotes=$(git --git-dir tools/OpenROAD/.git remote)
-  SAVEIFS=$IFS
-  IFS=$'\n'
-  remotes=($remotes)
-  IFS=$SAVEIFS
-  if [[ ! " ${remotes[@]} " =~ " ${CURRENT_REMOTE} " ]]; then
-    git --git-dir tools/OpenROAD/.git remote add $CURRENT_REMOTE $OR_REPO
-  fi
-fi
-
-
-if [ ! -z ${UPDATE_OR+x} ]; then
-  echo "[INFO FLW-0005] Updating OpenROAD tool to the latest."
-  (cd tools/OpenROAD && git fetch $CURRENT_REMOTE $OR_BRANCH \
-    && git checkout $CURRENT_REMOTE/$OR_BRANCH \
-    && git pull $CURRENT_REMOTE $OR_BRANCH \
-    && git submodule update --init --recursive)
-fi
-
-# Docker build
-if [ "$build_method" == "DOCKER" ]; then
+__docker_build()
+{
   docker build --tag openroad/yosys --file tools/yosys_util/Dockerfile --target builder tools/yosys
   docker build --tag openroad/lsoracle --file tools/LSOracle/Dockerfile.openroad tools
   ./tools/OpenROAD/etc/DockerHelper.sh create -target=builder -threads=${PROC}
@@ -142,22 +105,61 @@ if [ "$build_method" == "DOCKER" ]; then
   if [ "$COPY_PLATFORMS" == "YES" ]; then
     mv .dockerignore{.bak,}
   fi
+}
 
-# Local build
-elif [ "$build_method" == "LOCAL" ]; then
+__local_build()
+{
   $NICE make install -C tools/yosys -j${PROC} PREFIX=$(pwd)/tools/install/yosys CONFIG=gcc ABCREV=bafd2a7 ABCURL=https://github.com/berkeley-abc/abc
 
   cmake -B tools/OpenROAD/build tools/OpenROAD -DCMAKE_INSTALL_PREFIX=tools/install/OpenROAD
   $NICE cmake --build tools/OpenROAD/build --target install -j${PROC}
 
   cmake -B tools/LSOracle/build tools/LSOracle \
-        -D CMAKE_BUILD_TYPE=RELEASE \
-        -D YOSYS_INCLUDE_DIR=$(pwd)/tools/yosys \
-        -D YOSYS_PLUGIN=ON \
-        -D YOSYS_SHARE_DIR=$(pwd)/tools/install/yosys/share/yosys \
-        -D CMAKE_INSTALL_PREFIX=$(pwd)/tools/install/LSOracle
+    -D CMAKE_BUILD_TYPE=RELEASE \
+    -D YOSYS_INCLUDE_DIR=$(pwd)/tools/yosys \
+    -D YOSYS_PLUGIN=ON \
+    -D YOSYS_SHARE_DIR=$(pwd)/tools/install/yosys/share/yosys \
+    -D CMAKE_INSTALL_PREFIX=$(pwd)/tools/install/LSOracle
+
   $NICE cmake --build tools/LSOracle/build -j${PROC} --target install
+}
+
+__common_setup()
+{
+  # Clone repositories
+  if [ -z ${OPENROAD_FLOW_NO_GIT_INIT+x} ]; then
+    git submodule update --init --recursive
+  fi
+
+  if [ ! -z ${OR_REPO+x} ]; then
+    base_url=$(dirname $OR_REPO)
+    [[ ${base_url##*/} = $base_url ]] && CURRENT_REMOTE=${base_url##*:} || CURRENT_REMOTE=${base_url##*/}
+    remotes=$(git --git-dir tools/OpenROAD/.git remote)
+    SAVEIFS=$IFS
+    IFS=$'\n'
+    remotes=($remotes)
+    IFS=$SAVEIFS
+    if [[ ! " ${remotes[@]} " =~ " ${CURRENT_REMOTE} " ]]; then
+      git --git-dir tools/OpenROAD/.git remote add $CURRENT_REMOTE $OR_REPO
+    fi
+  fi
+
+
+  if [ ! -z ${UPDATE_OR+x} ]; then
+    echo "[INFO FLW-0005] Updating OpenROAD tool to the latest."
+    (cd tools/OpenROAD && git fetch $CURRENT_REMOTE $OR_BRANCH \
+      && git checkout $CURRENT_REMOTE/$OR_BRANCH \
+      && git pull $CURRENT_REMOTE $OR_BRANCH \
+      && git submodule update --init --recursive)
+  fi
+}
+
+__common_setup
+# Choose install method
+if [ -z ${BUILD_METHOD+x} ] && command -v docker &> /dev/null; then
+  echo "[INFO FLW-0000] Using docker build method. This will create a docker image tagged '${DOCKER_TAG}'."
+  __docker_build
 else
-  echo "[ERROR FLW-0003] No valid build method found."
-  exit 1
+  echo "[INFO FLW-0000] Using local build method. This will create binaries at 'tools/install/..'."
+  __local_build
 fi
