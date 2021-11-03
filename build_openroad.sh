3dc775437e51ad158c962df4a9661dd949a408ce
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-11-03 15:47:32 -0300

    Protect variables
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- build_openroad.sh
+++ build_openroad.sh
@@ -39,7 +39,7 @@ PROC=$(nproc --all)
 COPY_PLATFORMS="NO"
 DOCKER_TAG="openroad/flow-scripts"
 CURRENT_REMOTE="origin"
-OR_BRANCH="master"
+OPENROAD_BRANCH="master"
 
 INSTALL_PATH="$(pwd)/tools/install"
 
@@ -51,14 +51,14 @@ YOSYS_OVERWIRTE_ARGS="NO"
 YOSYS_USER_ARGS=""
 YOSYS_ARGS="\
 CONFIG=gcc \
-PREFIX=$INSTALL_PATH/yosys \
+PREFIX=${INSTALL_PATH}/yosys \
 ABCREV=bafd2a7 ABCURL=https://github.com/berkeley-abc/abc \
 "
 
 OPENROAD_OVERWIRTE_ARGS="NO"
 OPENROAD_USER_ARGS=""
 OPENROAD_ARGS="\
--D CMAKE_INSTALL_PREFIX=$INSTALL_PATH/OpenROAD \
+-D CMAKE_INSTALL_PREFIX=${INSTALL_PATH}/OpenROAD \
 "
 
 LSORACLE_OVERWIRTE_ARGS="NO"
@@ -67,8 +67,8 @@ LSORACLE_ARGS="\
 -D CMAKE_BUILD_TYPE=RELEASE \
 -D YOSYS_INCLUDE_DIR=$(pwd)/tools/yosys \
 -D YOSYS_PLUGIN=ON \
--D YOSYS_SHARE_DIR=$INSTALL_PATH/yosys/share/yosys \
--D CMAKE_INSTALL_PREFIX=$INSTALL_PATH/LSOracle \
+-D YOSYS_SHARE_DIR=${INSTALL_PATH}/yosys/share/yosys \
+-D CMAKE_INSTALL_PREFIX=${INSTALL_PATH}/LSOracle \
 "
 
 # Parse arguments
@@ -83,11 +83,11 @@ while (( "$#" )); do
                         shift
                         ;;
                 --or_repo)
-                        OR_REPO="$2"
+                        OPENROAD_GIT_URL="$2"
                         shift
                         ;;
                 -l|--latest)
-                        UPDATE_OR=1
+                        USE_OPENROAD_MASTER=1
                         ;;
                 -t|--threads)
                         PROC="$2"
@@ -168,33 +168,33 @@ fi
 
 __docker_build()
 {
-        $NICE docker build \
+        ${NICE} docker build \
                 "${DOCKER_ARGS}" \
                 --tag openroad/yosys \
                 --file tools/yosys_util/Dockerfile \
                 --target builder \
                 tools/yosys
 
-        $NICE docker build \
+        ${NICE} docker build \
                 "${DOCKER_ARGS}" \
                 --tag openroad/lsoracle \
                 --file tools/LSOracle/Dockerfile.openroad \
                 tools
 
-        $NICE ./tools/OpenROAD/etc/DockerHelper.sh create \
+        ${NICE} ./tools/OpenROAD/etc/DockerHelper.sh create \
                 -target=builder \
                 -threads=${PROC}
 
-        if [ "$COPY_PLATFORMS" == "YES" ]; then
+        if [ "${COPY_PLATFORMS}" == "YES" ]; then
                 cp .dockerignore{,.bak}
                 sed -i '/flow\/platforms/d' .dockerignore
         fi
-        $NICE docker build \
+        ${NICE} docker build \
                 "${DOCKER_ARGS}" \
-                --tag ${DOCKER_TAG} \
+                --tag "${DOCKER_TAG}" \
                 --file Dockerfile \
                 .
-        if [ "$COPY_PLATFORMS" == "YES" ]; then
+        if [ "${COPY_PLATFORMS}" == "YES" ]; then
                 mv .dockerignore{.bak,}
         fi
 }
@@ -202,20 +202,20 @@ __docker_build()
 __local_build()
 {
         # build yosys
-        $NICE make install -C tools/yosys -j ${PROC} "${YOSYS_ARGS}"
+        ${NICE} make install -C tools/yosys -j "${PROC}" ${YOSYS_ARGS}
 
         # build openroad
-        $NICE cmake tools/OpenROAD -B tools/OpenROAD/build "${OPENROAD_ARGS}"
-        $NICE cmake --build tools/OpenROAD/build --target install -j ${PROC}
+        ${NICE} cmake tools/OpenROAD -B tools/OpenROAD/build ${OPENROAD_ARGS}
+        ${NICE} cmake --build tools/OpenROAD/build --target install -j "${PROC}"
 
         # build lsoracle
-        $NICE cmake tools/LSOracle -B tools/LSOracle/build "${LSORACLE_ARGS}"
-        $NICE cmake --build tools/LSOracle/build --target install -j ${PROC}
+        ${NICE} cmake tools/LSOracle -B tools/LSOracle/build ${LSORACLE_ARGS}
+        ${NICE} cmake --build tools/LSOracle/build --target install -j "${PROC}"
 }
 
 __change_or_repo()
 (
-        base_url=$(dirname $OR_REPO)
+        base_url=$(dirname "${OPENROAD_GIT_URL}")
         [[ ${base_url##*/} = $base_url ]] && CURRENT_REMOTE=${base_url##*:} || CURRENT_REMOTE=${base_url##*/}
         cd tools/OpenROAD
         remotes=$(git remote)
@@ -224,31 +224,31 @@ __change_or_repo()
         remotes=($remotes)
         IFS=$SAVEIFS
         if [[ ! " ${remotes[@]} " =~ " ${CURRENT_REMOTE} " ]]; then
-                git remote add $CURRENT_REMOTE $OR_REPO
+                git remote add "${CURRENT_REMOTE}" "${OPENROAD_GIT_URL}"
         fi
 )
 
 __update_or_latest()
 (
         cd tools/OpenROAD
-        git fetch $CURRENT_REMOTE $OR_BRANCH
-        git checkout $CURRENT_REMOTE/$OR_BRANCH
-        git pull $CURRENT_REMOTE $OR_BRANCH
+        git fetch "${CURRENT_REMOTE}" "${OPENROAD_BRANCH}"
+        git checkout "${CURRENT_REMOTE}/${OPENROAD_BRANCH}"
+        git pull "${CURRENT_REMOTE}" "${OPENROAD_BRANCH}"
         git submodule update --init --recursive
 )
 
 __common_setup()
 {
         # Clone repositories
-        if [ -z ${OPENROAD_FLOW_NO_GIT_INIT+x} ]; then
+        if [ -z "${OPENROAD_FLOW_NO_GIT_INIT+x}" ]; then
                 git submodule update --init --recursive
         fi
 
-        if [ ! -z ${OR_REPO+x} ]; then
+        if [ ! -z "${OPENROAD_GIT_URL+x}" ]; then
                 __change_or_repo
         fi
 
-        if [ ! -z ${UPDATE_OR+x} ]; then
+        if [ ! -z "${USE_OPENROAD_MASTER+x}" ]; then
                 echo "[INFO FLW-0005] Updating OpenROAD tool to the latest."
                 __update_or_latest
         fi
@@ -256,7 +256,7 @@ __common_setup()
 
 __common_setup
 # Choose install method
-if [ -z ${BUILD_METHOD+x} ] && command -v docker &> /dev/null; then
+if [ -z "${BUILD_METHOD+x}" ] && command -v docker &> /dev/null; then
         echo -n "[INFO FLW-0000] Using docker build method."
         echo " This will create a docker image tagged '${DOCKER_TAG}'."
         __docker_build
