2fa3e0615a85ed90c3207671da1a3b9888594d1b
Peter Gadfort <peter.gadfort.civ@army.mil>
2021-11-09 11:28:15 -0500

    asap7: adder mapping use only full adders
    Signed-off-by: Peter Gadfort <peter.gadfort.civ@army.mil>

--- flow/platforms/asap7/yoSys/cells_adders.v
+++ flow/platforms/asap7/yoSys/cells_adders.v
@@ -17,24 +17,27 @@ module _tech_fa (A, B, C, X, Y);
   generate for (i = 0; i < WIDTH; i = i + 1) begin
       if (_TECHMAP_CONSTVAL_A_[i] === 1'b0 || _TECHMAP_CONSTVAL_B_[i] === 1'b0 || _TECHMAP_CONSTVAL_C_[i] === 1'b0) begin
         if (_TECHMAP_CONSTVAL_C_[i] === 1'b0) begin
-          HAxp5_ASAP7_75t_R halfadder_Cconst (
+          FAx1_ASAP7_75t_R halfadder_Cconst (
               .A(A[i]),
               .B(B[i]),
+              .CI(1'b0),
               .CON(NX[i]), .SN(NY[i])
             );
         end 
         else begin
           if (_TECHMAP_CONSTVAL_B_[i] === 1'b0) begin
-            HAxp5_ASAP7_75t_R halfadder_Bconst (
+            FAx1_ASAP7_75t_R halfadder_Bconst (
                 .A(A[i]),
                 .B(C[i]),
+                .CI(1'b0),
                 .CON(NX[i]), .SN(NY[i])
               );
           end
           else begin
-            HAxp5_ASAP7_75t_R halfadder_Aconst (
+            FAx1_ASAP7_75t_R halfadder_Aconst (
                 .A(B[i]),
                 .B(C[i]),
+                .CI(1'b0),
                 .CON(NX[i]), .SN(NY[i])
               );
           end
