a36d3da42a4fca26592b1bcc76b5afc39a2f5287
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2021-11-23 10:04:15 -0800

    Update custom fastroute.tcl for ca53_cpu
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/gf12/ca53/fastroute.tcl
+++ flow/designs/gf12/ca53/fastroute.tcl
@@ -1,8 +1,6 @@
 set_global_routing_layer_adjustment M2 0.5
 set_global_routing_layer_adjustment M3 0.5
 set_global_routing_layer_adjustment C4-K4 0.5
-set_global_routing_layer_adjustment C5 0.5
-set_global_routing_layer_adjustment K1-K4 0.5
 #set_global_routing_layer_adjustment H1-H2 0.5
 
 set_routing_layers -signal M2-$::env(MAX_ROUTING_LAYER) -clock  K1-$::env(MAX_ROUTING_LAYER)
