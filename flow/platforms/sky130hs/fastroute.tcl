e7a4b7dda49e632d344b0f3a34473da9783d3e4b
luis201420 <tauro_luisito_20@hotmail.com>
2021-11-26 13:38:55 -0500

    changing 1/0 padding and 30% reduction for sky130hd and sky130hs designs
    Signed-off-by: luis201420 <tauro_luisito_20@hotmail.com>

--- flow/platforms/sky130hs/fastroute.tcl
+++ flow/platforms/sky130hs/fastroute.tcl
@@ -1,8 +1,8 @@
-#set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.5
-set_global_routing_layer_adjustment met5 0.5
-set_global_routing_layer_adjustment met4 0.4
-set_global_routing_layer_adjustment met3 0.4
-set_global_routing_layer_adjustment met2 0.4
-set_global_routing_layer_adjustment met1 0.4
+set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.3
+#set_global_routing_layer_adjustment met5 0.5
+#set_global_routing_layer_adjustment met4 0.4
+#set_global_routing_layer_adjustment met3 0.4
+#set_global_routing_layer_adjustment met2 0.4
+#set_global_routing_layer_adjustment met1 0.4
 
 set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
