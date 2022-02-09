29ab173b257d13aa9ae15546ed3ba4fc81d690c7
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-02-09 05:20:56 -0800

    New set of tests for hier TMP flow (for comparison purpose with RTLMP)
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

---
+++ flow/designs/gf12/ariane/config_hier_tmp.mk
@@ -0,0 +1,26 @@
+include $(dir $(DESIGN_CONFIG))/config.mk
+
+
+export FLOW_VARIANT = hier_tmp
+export SYNTH_HIERARCHICAL = 1
+export MAX_UNGROUP_SIZE = 100
+
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NAME)/constraint_hier.sdc
+
+export FLOORPLAN_DEF = ./results/$(PLATFORM)/$(DESIGN_NAME)/$(FLOW_VARIANT)/2_2_floorplan_io.def
+#
+# RTL_MP Settings
+export RTLMP_MAX_INST = 5000
+export RTLMP_MIN_INST = 1000
+export RTLMP_MAX_MACRO = 12
+export RTLMP_MIN_MACRO = 4
+
+# These values must be multiples of placement site
+export DIE_AREA    = 0 0 700 700
+export CORE_AREA   = 10 10 690 690
+export PLACE_PINS_ARGS = -exclude left:0-300 -exclude left:400-700 -exclude right:* -exclude top:* -exclude bottom:*
+
+export MACRO_PLACE_HALO = 5 5
+export MACRO_PLACE_CHANNEL = 10 10
+
+export PLACE_DENSITY = 0.55
---
+++ flow/designs/tsmc65lp/ariane/config_hier_tmp.mk
@@ -0,0 +1,23 @@
+include $(dir $(DESIGN_CONFIG))/config.mk
+
+export FLOW_VARIANT = hier_tmp
+export SYNTH_HIERARCHICAL = 1
+export MAX_UNGROUP_SIZE = 100
+
+export FLOORPLAN_DEF = ./results/$(PLATFORM)/$(DESIGN_NAME)/$(FLOW_VARIANT)/2_2_floorplan_io.def
+#
+# RTL_MP Settings
+export RTLMP_MAX_INST = 5000
+export RTLMP_MIN_INST = 1000
+export RTLMP_MAX_MACRO = 12
+export RTLMP_MIN_MACRO = 4
+
+# These values must be multiples of placement site
+export DIE_AREA    = 0 0 1950 1560
+export CORE_AREA   = 10 12 1940 1550
+export PLACE_PINS_ARGS = -exclude left:0-400 -exclude left:900-1560 -exclude right:* -exclude top:* -exclude bottom:*
+
+export MACRO_PLACE_HALO = 10 10
+export MACRO_PLACE_CHANNEL = 20 20
+
+export PLACE_DENSITY = .70
