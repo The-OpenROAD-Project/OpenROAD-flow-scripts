bdfccb3e7564b76b304182414330acb60101c0b3
James Cherry <cherry@parallaxsw.com>
2022-03-04 10:37:17 -0800

    sky130hd correlateRC layer caps
    Signed-off-by: James Cherry <cherry@parallaxsw.com>

--- flow/platforms/sky130hd/setRC.tcl
+++ flow/platforms/sky130hd/setRC.tcl
@@ -1,11 +1,12 @@
-# sync from openroad repo test/sky130hd/sky130hd.rc
+# correlateRC.py gcd,ibex,aes,jpeg,chameleon,riscv32i,chameleon_hier
+# cap units pf/um
 set_layer_rc -layer li1 -capacitance 1.499e-04 -resistance 7.176e-02
-set_layer_rc -layer met1 -capacitance 1.449e-04 -resistance 8.929e-04
-set_layer_rc -layer met2 -capacitance 1.331e-04 -resistance 8.929e-04
-set_layer_rc -layer met3 -capacitance 1.464e-04 -resistance 1.567e-04
-set_layer_rc -layer met4 -capacitance 1.297e-04 -resistance 1.567e-04
-set_layer_rc -layer met5 -capacitance 1.501e-04 -resistance 1.781e-05
-# end sync
+set_layer_rc -layer met1 -capacitance 1.72375E-04 -resistance 8.929e-04
+set_layer_rc -layer met2 -capacitance 1.36233E-04 -resistance 8.929e-04
+set_layer_rc -layer met3 -capacitance 2.14962E-04 -resistance 1.567e-04
+set_layer_rc -layer met4 -capacitance 1.48128E-04 -resistance 1.567e-04
+set_layer_rc -layer met5 -capacitance 1.54087E-04 -resistance 1.781e-05
+# end correlate
 
 set_layer_rc -via mcon -resistance 9.249146E-3
 set_layer_rc -via via -resistance 4.5E-3
