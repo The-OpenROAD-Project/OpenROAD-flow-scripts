021c71ccefc2df875ee97392d6cc7871632f50ca
Vitor Bandeira <vitor.vbandeira@gmail.com>
2022-04-27 15:24:11 -0300

    Refactor rules to include new options
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- flow/util/genRuleFile.py
+++ flow/util/genRuleFile.py
@@ -1,28 +1,69 @@
 #!/usr/bin/env python3
 
-import sys
+from math import ceil, isinf
 from os import chdir
 from os.path import isfile, abspath
 from re import sub
+import argparse
 import json
-from math import ceil, isinf
+import operator
+import sys
 
-if len(sys.argv) != 3:
-    print('usage:', sys.argv[0], '<DIR> <VARIANT>')
+parser = argparse.ArgumentParser(
+    description='Generates or updates rules file for CI.')
+parser.add_argument('dir', help='Path to the design directory.')
+parser.add_argument(
+    '-v',
+    '--variant',
+    default='base',
+    help='Flow variant [default="base"].')
+parser.add_argument(
+    '-u',
+    '--update',
+    action='store_true',
+    default=False,
+    help='Update all rules independent of previous values.')
+parser.add_argument(
+    '-t',
+    '--tighten',
+    action='store_true',
+    default=False,
+    help='Update passing rules if they became tighter')
+parser.add_argument(
+    '-f',
+    '--failing',
+    action='store_true',
+    default=False,
+    help='Update failing rules.')
+args = parser.parse_args()
+
+if not args.update and not args.tighten and not args.failing:
+    print('[ERROR] Please select at least one of '
+          '-u/--update, -t/--tighten, -f/--failing.')
+    parser.print_help()
     sys.exit(1)
 
-chdir(sys.argv[1])
-variant = sys.argv[2]
-ok_file = f'metadata-{variant}-ok.json'
-out_file = f'rules-{variant}.json'
+chdir(args.dir)
+metrics_file = f"metadata-{args.variant}-ok.json"
+old_rules_file = f"rules-{args.variant}.json"
+new_rules_file = f"rules-{args.variant}.json"
 rules = list()
 
-if isfile(ok_file):
-    with open(ok_file, 'r') as f:
-        data = json.load(f)
+if isfile(metrics_file):
+    with open(metrics_file, 'r') as f:
+        metrics = json.load(f)
 else:
-    print('Not found', abspath(ok_file))
+    print(f"[ERROR] File not found {abspath(metrics_file)}")
     sys.exit(1)
+
+if isfile(old_rules_file):
+    with open(old_rules_file, 'r') as f:
+        old_rules = dict()
+        for rule in json.load(f)['rules']:
+            field = rule['field']
+            old_rules[field] = rule
+else:
+    print(f"[WARNING] File not found {abspath(old_rules_file)}")
 
 # dict format
 # 'metric_name': {
@@ -32,7 +73,7 @@ else:
 #     'fixed': <float>, sum this number instead of using % padding
 #     'round_value': <bool>, use the rounded value for the rule
 # },
-metrics = {
+rules_dict = {
     # synth
     'synth__design__instance__area__stdcell': {
         'use_period': False,
@@ -165,56 +206,97 @@ metrics = {
     },
 }
 
+ops = {
+    '<': operator.lt,
+    '>': operator.gt,
+    '<=': operator.le,
+    '>=': operator.ge,
+    '==': operator.eq,
+    '!=': operator.ne,
+}
+
 period_list = list()
-for entry in data['constraints__clocks__details']:
+for entry in metrics['constraints__clocks__details']:
     period_list.append(float(sub(r'^.*: ', '', entry)))
 
 if len(period_list) != 1:
     print('[WARNING] Multiple clocks not supported. Will use first clock.')
 period = period_list[0]
 
-for field, option in metrics.items():
-    if field not in data.keys():
-        print(f'[ERROR] Metric {field} not found in metrics file: {ok_file}.')
+for field, option in rules_dict.items():
+    if field not in metrics.keys():
+        print(f"[ERROR] Metric {field} not found in "
+              f"metrics file: {metrics_file}.")
         sys.exit(1)
 
-    value = data[field]
-
-    if isinstance(value, str):
-        print('[WARNING] Skipping string field {} = {}'.format(field, value))
+    if isinstance(metrics[field], str):
+        print(f"[WARNING] Skipping string field {field} = {metrics[field]}")
         continue
 
     if len(period_list) != 1 and field == 'globalroute__timing__clock__slack':
         print('[WARNING] Skipping clock slack until multiple clocks support.')
         continue
 
-    if 'padding' in option.keys() and option['padding'] != 0:
-        if option['use_period']:
-            value -= period * option['padding'] / 100
-            value = min(value, 0)
-        elif 'padding_fixed' in option.keys():
-            temp_1 = value + option['padding_fixed']
-            temp_2 = value * (1 + option['padding'] / 100)
-            value = max(temp_1, temp_2)
-        else:
-            value += value * option['padding'] / 100
-    elif 'fixed' in option.keys():
-        value = min(option['fixed'], value + option['fixed'])
-
-    if field == 'cts__design__instance__count__hold_buffer' and value == 0:
-        value = ceil(data['placeopt__design__instance__count__stdcell'] * 0.1)
-
     new_rule = dict()
     new_rule['field'] = field
-    if option['round_value'] and not isinf(value):
-        new_rule['value'] = round(value)
+
+    if 'padding' in option.keys() and option['padding'] != 0:
+        if option['use_period']:
+            new_rule['value'] = metrics[field] - period * option['padding'] / 100
+            new_rule['value'] = min(new_rule['value'], 0)
+        elif 'padding_fixed' in option.keys():
+            temp_1 = metrics[field] + option['padding_fixed']
+            temp_2 = metrics[field] * (1 + option['padding'] / 100)
+            new_rule['value'] = max(temp_1, temp_2)
+        else:
+            new_rule['value'] = metrics[field] * (1 + option['padding'] / 100)
+    elif 'fixed' in option.keys():
+        new_rule['value'] = min(option['fixed'], metrics[field] + option['fixed'])
+    else:  # TODO: check below
+        new_rule['value'] = metrics[field]
+
+    if field == 'cts__design__instance__count__hold_buffer' \
+            and new_rule['value'] == 0:
+        new_rule['value'] = ceil(
+            metrics['placeopt__design__instance__count__stdcell'] * 0.1)
+
+    if option['round_value'] and not isinf(new_rule['value']):
+        new_rule['value'] = int(round(new_rule['value']))
     else:
-        new_rule['value'] = float('{:.2f}'.format(value))
+        new_rule['value'] = float(f"{new_rule['value']:.2f}")
+
+    compare = ops[option['compare']]
+    old_rule = old_rules[field]
+    if old_rule['compare'] != option['compare']:
+        print('[WARNING] Compare operator changed since last update.')
     new_rule['compare'] = option['compare']
+
+    UPDATE = False
+    if args.tighten \
+            and new_rule['value'] != old_rule['value'] \
+            and compare(new_rule['value'], old_rule['value']):
+        UPDATE = True
+        print(f"[INFO] Tightening rule {field} "
+              f"from {old_rule['value']} to {new_rule['value']}.")
+
+    if args.failing and \
+            not compare(metrics[field], old_rule['value']):
+        UPDATE = True
+        print(f"[INFO] Updating failing rule {field} "
+              f"from {old_rule['value']} to {new_rule['value']}.")
+
+    if args.update:
+        UPDATE = True
+        print(f"[INFO] Updating rule {field} "
+              f"from {old_rule['value']} to {new_rule['value']}.")
+
+    if not UPDATE:
+        new_rule['value'] = old_rule['value']
+
     rules.append(new_rule)
 
-final_rules = dict()
-final_rules['rules'] = rules
-with open(out_file, 'w') as f:
-    print('[INFO] writing', abspath(out_file))
-    json.dump(final_rules, f, indent=4)
+# final_rules = dict()
+# final_rules['rules'] = rules
+# with open(new_rules_file, 'w') as f:
+#     print('[INFO] writing', abspath(new_rules_file))
+#     json.dump(final_rules, f, indent=4)
