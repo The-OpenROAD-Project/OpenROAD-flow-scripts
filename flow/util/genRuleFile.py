0917e9db95f87a8db213785d66a1e11efbf6bc6f
Vitor Bandeira <vitor.vbandeira@gmail.com>
2022-04-27 15:17:26 -0300

    Style update
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- flow/util/genRuleFile.py
+++ flow/util/genRuleFile.py
@@ -13,169 +13,169 @@ if len(sys.argv) != 3:
 
 chdir(sys.argv[1])
 variant = sys.argv[2]
-okFile = f'metadata-{variant}-ok.json'
-outFile = f'rules-{variant}.json'
+ok_file = f'metadata-{variant}-ok.json'
+out_file = f'rules-{variant}.json'
 rules = list()
 
-if isfile(okFile):
-    with open(okFile, 'r') as f:
+if isfile(ok_file):
+    with open(ok_file, 'r') as f:
         data = json.load(f)
 else:
-    print('Not found', abspath(okFile))
+    print('Not found', abspath(ok_file))
     sys.exit(1)
 
 # dict format
-# 'metric__name': {
-#     'usePeriod': <bool>, use a percentage of the clock period as padding
+# 'metric_name': {
+#     'use_period': <bool>, use a percentage of the clock period as padding
 #     'padding': <float>, percentage of padding to use
 #     'padding_fixed': <float>, fixed margin for padding
 #     'fixed': <float>, sum this number instead of using % padding
-#     'roundValue': <bool>, use the rounded value for the rule
+#     'round_value': <bool>, use the rounded value for the rule
 # },
 metrics = {
     # synth
     'synth__design__instance__area__stdcell': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 15,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     # clock
     'constraints__clocks__count': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 0,
-        'roundValue': True,
+        'round_value': True,
         'compare': '==',
     },
     # floorplan
     # place
     'placeopt__design__instance__area': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 15,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     'placeopt__design__instance__count__stdcell': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 15,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     'detailedplace__design__violations': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 0,
-        'roundValue': True,
+        'round_value': True,
         'compare': '==',
     },
     # cts
     'cts__timing__setup__ws': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 25,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'cts__timing__setup__ws__pre_repair': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 25,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'cts__timing__setup__ws__post_repair': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 25,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'cts__design__instance__count__hold_buffer': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 10,
         'padding_fixed': 10,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     # route
     'globalroute__timing__clock__slack': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 5,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'globalroute__timing__setup__ws': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 5,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'detailedroute__route__wirelength': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 15,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     'detailedroute__route__drc_errors': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 0,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     # finish
     'finish__timing__setup__ws': {
-        'usePeriod': True,
+        'use_period': True,
         'padding': 5,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'finish__design__instance__area': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 15,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     'finish__timing__drv__max_slew_limit': {
-        'usePeriod': False,
+        'use_period': False,
         'fixed': -0.20,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'finish__timing__drv__max_fanout_limit': {
-        'usePeriod': False,
+        'use_period': False,
         'fixed': -0.20,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'finish__timing__drv__max_cap_limit': {
-        'usePeriod': False,
+        'use_period': False,
         'fixed': -0.20,
-        'roundValue': False,
+        'round_value': False,
         'compare': '>=',
     },
     'finish__timing__drv__setup_violation_count': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 20,
         'padding_fixed': 10,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
     'finish__timing__drv__hold_violation_count': {
-        'usePeriod': False,
+        'use_period': False,
         'padding': 20,
         'padding_fixed': 10,
-        'roundValue': True,
+        'round_value': True,
         'compare': '<=',
     },
 }
 
-periodList = list()
+period_list = list()
 for entry in data['constraints__clocks__details']:
-    periodList.append(float(sub(r'^.*: ', '', entry)))
+    period_list.append(float(sub(r'^.*: ', '', entry)))
 
-if len(periodList) != 1:
+if len(period_list) != 1:
     print('[WARNING] Multiple clocks not supported. Will use first clock.')
-period = periodList[0]
+period = period_list[0]
 
 for field, option in metrics.items():
     if field not in data.keys():
-        print(f'[ERROR] Metric {field} not found in metrics file: {okFile}.')
+        print(f'[ERROR] Metric {field} not found in metrics file: {ok_file}.')
         sys.exit(1)
 
     value = data[field]
@@ -184,12 +184,12 @@ for field, option in metrics.items():
         print('[WARNING] Skipping string field {} = {}'.format(field, value))
         continue
 
-    if len(periodList) != 1 and field == 'globalroute__timing__clock__slack':
+    if len(period_list) != 1 and field == 'globalroute__timing__clock__slack':
         print('[WARNING] Skipping clock slack until multiple clocks support.')
         continue
 
     if 'padding' in option.keys() and option['padding'] != 0:
-        if option['usePeriod']:
+        if option['use_period']:
             value -= period * option['padding'] / 100
             value = min(value, 0)
         elif 'padding_fixed' in option.keys():
@@ -204,17 +204,17 @@ for field, option in metrics.items():
     if field == 'cts__design__instance__count__hold_buffer' and value == 0:
         value = ceil(data['placeopt__design__instance__count__stdcell'] * 0.1)
 
-    newRule = dict()
-    newRule['field'] = field
-    if option['roundValue'] and not isinf(value):
-        newRule['value'] = round(value)
+    new_rule = dict()
+    new_rule['field'] = field
+    if option['round_value'] and not isinf(value):
+        new_rule['value'] = round(value)
     else:
-        newRule['value'] = float('{:.2f}'.format(value))
-    newRule['compare'] = option['compare']
-    rules.append(newRule)
+        new_rule['value'] = float('{:.2f}'.format(value))
+    new_rule['compare'] = option['compare']
+    rules.append(new_rule)
 
-finalRules = dict()
-finalRules['rules'] = rules
-with open(outFile, 'w') as f:
-    print('[INFO] writing', abspath(outFile))
-    json.dump(finalRules, f, indent=4)
+final_rules = dict()
+final_rules['rules'] = rules
+with open(out_file, 'w') as f:
+    print('[INFO] writing', abspath(out_file))
+    json.dump(final_rules, f, indent=4)
