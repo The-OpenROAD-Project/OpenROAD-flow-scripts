b475d569382169034f4c8b6f26deb6c561e852ed
sakundu <sakundu@ucsd.edu>
2022-05-05 00:04:50 -0700

    Updated based on Vitor's suggestion
    Signed-off-by: sakundu <sakundu@ucsd.edu>

--- flow/scripts/odb2files.tcl
+++ flow/scripts/odb2files.tcl
@@ -1,8 +1,6 @@
 read_db $::env(ODB_FILE)
 write_def $::env(DEF_FILE)
 
-if { [info exists ::env(VERILOG_FILE)] } {
-    write_verilog $::env(VERILOG_FILE)
-}
+write_verilog $::env(VERILOG_FILE)
 
 exit
