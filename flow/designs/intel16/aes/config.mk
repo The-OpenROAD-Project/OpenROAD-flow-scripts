c43e2f25e3eb6c2f8f9e9ec7f8a58487eb63ebb9
louiic <pchaiyakul@eng.ucsd.edu>
2022-05-31 21:26:50 -0700

    First revision of Intel16 design - AES
    Signed-off-by: louiic <pchaiyakul@eng.ucsd.edu>

---
+++ flow/designs/intel16/aes/config.mk
@@ -0,0 +1,20 @@
+$(info [INFO-FLOW] AES Design)
+DESIGN_DIR                   := $(realpath $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST)))))
+DESIGN_PDK_HOME              := $(realpath $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+export DESIGN_NICKNAME = aes
+export DESIGN_NAME     = aes_cipher_top
+export PLATFORM        = intel16
+
+export VERILOG_FILES = $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/*.v))
+export SDC_FILE      = $(DESIGN_DIR)/constraint.sdc
+
+# These values must be multiples of placement site
+# x=0.19 y=1.4
+export DIE_AREA    = 0 0 250 250
+export CORE_AREA   = 1.26 1.89 248 248
+
+
+export PLACE_DENSITY = uniform
+
+export ABC_CLOCK_PERIOD_IN_PS = 2600
