7021fe079b99b17073d46a85fd8b4e14b52a8082
Will <5217539+WRansohoff@users.noreply.github.com>
2022-06-14 09:50:18 -0400

    klayout.lyt: Fix minor quotation bug, add missing layer_map property
    Signed-off-by: Will <5217539+WRansohoff@users.noreply.github.com>

--- flow/scripts/klayout.tcl
+++ flow/scripts/klayout.tcl
@@ -10,11 +10,14 @@ if {[info exists ::env(SEAL_GDS)]} {
     set seal_gds ""
 }
 
+set GDSOAS_INPUT "$::env(GDSOAS_FILES) $::env(WRAPPED_GDSOAS)"
+
 exec klayout -zz -rd design_name=$::env(DESIGN_NAME) \
            -rd in_def=$::env(RESULTS_DIR)/6_final.def \
-           -rd in_files="$::env(GDSOAS_FILES) $::env(WRAPPED_GDSOAS)" \
+           -rd in_files=$GDSOAS_INPUT \
            -rd config_file=$fill_config \
            -rd seal_file=$seal_gds \
            -rd out_file=$::env(RESULTS_DIR)/6_final.$::env(STREAM_SYSTEM_EXT) \
            -rd tech_file=$::env(OBJECTS_DIR)/klayout.lyt \
+           -rd layer_map=$::env(GDS_LAYER_MAP) \
            -rm $::env(UTILS_DIR)/def2stream.py
