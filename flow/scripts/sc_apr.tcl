669a39071f3a14b6bba01dfc21d6fb9a3630efec
Noah Moroze <noah@zeroasic.com>
2022-06-17 16:16:36 -0400

    Comment sc_apr.tcl
    Signed-off-by: Noah Moroze <noah@zeroasic.com>

--- flow/scripts/sc_apr.tcl
+++ flow/scripts/sc_apr.tcl
@@ -8,20 +8,26 @@ set sc_refdir [dict get $sc_cfg tool $sc_tool refdir $sc_step $sc_index]
 set results_dir $::env(RESULTS_DIR)
 set inputs [list]
 
+# Copy from inputs/ into OpenROAD work directory
 file mkdir $results_dir
 foreach f [glob -directory inputs/ -tails -nocomplain *] {
     file copy -force inputs/$f $results_dir
     lappend inputs $f
 }
 
+# Determine OR script name based on step name
 if {$sc_step == "export"} {
+    # export is special case
     set script "klayout.tcl"
 } else {
+    # strip or_ prefix
     set script [string replace $sc_step 0 2 ""].tcl
 }
 
+# Run script
 source $sc_refdir/$script
 
+# Copy anything from work dir that was not an input into outputs/
 foreach f [glob -directory $results_dir -tails -nocomplain *] {
     if {[lsearch $inputs $f] == -1} {
         file copy -force $results_dir/$f outputs/
