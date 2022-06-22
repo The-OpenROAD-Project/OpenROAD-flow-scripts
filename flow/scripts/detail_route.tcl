e89ebb9e651375b5b870147a4e683dee1912c2b0
osamahammad21 <osama21@aucegypt.edu>
2022-06-22 17:41:32 +0200

    flow: add 5_1_grt.odb
    Signed-off-by: osamahammad21 <osama21@aucegypt.edu>

--- flow/scripts/detail_route.tcl
+++ flow/scripts/detail_route.tcl
@@ -1,14 +1,15 @@
 source $::env(SCRIPTS_DIR)/load.tcl
-load_design 4_cts.odb 4_cts.sdc "Starting detailed routing"
-
+if { [info exists ::env(USE_WXL)]} {
+  set db_file 4_cts.odb
+} else {
+  set db_file 5_1_grt.odb
+}
+load_design $db_file 4_cts.sdc "Starting detailed routing"
 set_propagated_clock [all_clocks]
 
 set_thread_count $::env(NUM_CORES)
 
 set additional_args ""
-if { ![info exists ::env(USE_WXL)]} {
-  read_guides $::env(RESULTS_DIR)/route.guide
-}
 if { [info exists ::env(dbProcessNode)]} {
   append additional_args " -db_process_node $::env(dbProcessNode)"
 }
@@ -47,5 +48,5 @@ if { [info exists ::env(POST_DETAIL_ROUTE_TCL)] } {
 }
 
 if {![info exists save_checkpoint] || $save_checkpoint} {
-  write_db $::env(RESULTS_DIR)/5_route.odb
+  write_db $::env(RESULTS_DIR)/5_2_route.odb
 }
