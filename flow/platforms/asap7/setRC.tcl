395cdca4a55b5b751bfb6c7a995a4cd5dd7d4e48
Anton Blanchard <anton@linux.ibm.com>
2022-07-19 11:43:05 +1000

    Update ASAP7 RC layer estimates
    These were generated by running util/correlateRC.py across all ASAP7
    designs.
    
    Signed-off-by: Anton Blanchard <anton@linux.ibm.com>

--- flow/platforms/asap7/setRC.tcl
+++ flow/platforms/asap7/setRC.tcl
@@ -1,11 +1,13 @@
+# correlateRC.py aes,ethmac,gcd,ibex,jpeg,sha3,uart
 # Liberty units are fF,kOhm
+set_layer_rc -layer M2 -resistance 4.44843E-02 -capacitance 8.08251E-02
+set_layer_rc -layer M3 -resistance 2.56040E-02 -capacitance 1.26829E-01
+set_layer_rc -layer M4 -resistance 1.63749E-02 -capacitance 1.38907E-01
+set_layer_rc -layer M5 -resistance 1.36984E-02 -capacitance 1.16547E-01
+set_layer_rc -layer M6 -resistance 8.88899E-03 -capacitance 1.24308E-01
+set_layer_rc -layer M7 -resistance 1.00718E-02 -capacitance 1.16830E-01
+# Designs only use M2-M7, so we don't have correlation data for these layers
 set_layer_rc -layer M1 -capacitance 1.1368e-01 -resistance 1.3889e-01
-set_layer_rc -layer M2 -capacitance 1.3426e-01 -resistance 2.4222e-02
-set_layer_rc -layer M3 -capacitance 1.2918e-01 -resistance 2.4222e-02
-set_layer_rc -layer M4 -capacitance 1.1396e-01 -resistance 1.6778e-02
-set_layer_rc -layer M5 -capacitance 1.3323e-01 -resistance 1.4677e-02
-set_layer_rc -layer M6 -capacitance 1.1575e-01 -resistance 1.0371e-02
-set_layer_rc -layer M7 -capacitance 1.3293e-01 -resistance 9.6720e-03
 set_layer_rc -layer M8 -capacitance 1.1822e-01 -resistance 7.4310e-03
 set_layer_rc -layer M9 -capacitance 1.3497e-01 -resistance 6.8740e-03
 
@@ -19,4 +21,4 @@ set_layer_rc -via V7 -resistance 1.00E-02
 set_layer_rc -via V8 -resistance 1.00E-02
 set_layer_rc -via V9 -resistance 1.00E-02
 
-set_wire_rc -layer M3
+set_wire_rc -resistance 2.07476E-02 -capacitance 1.24551E-01
