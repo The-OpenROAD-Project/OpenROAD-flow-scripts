ece4c99f28df49160c35811a66bf1470da23d2f4
Vitor Bandeira <vitor.vbandeira@gmail.com>
2022-07-21 12:42:44 -0300

    ci: refactor report summary
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- flow/util/genReport.py
+++ flow/util/genReport.py
@@ -6,29 +6,29 @@ import re
 # make sure the working dir is flow/
 os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
-reportFilename = 'report.log'
-singleReportFilename = f"reports/{reportFilename}"
-summaryFilename = 'reports/report-summary.log'
-drcFilename = '5_route_drc.rpt'
-lastExpectedLog = '6_report.log'
-metricsLogFmt = 'gen-metrics-{}-check.log'
-metricsCheckFmt = 'metadata-{}-check.log'
-regexError = re.compile(r"^\[err", re.IGNORECASE)
-regexWarning = re.compile(r"^\[warn", re.IGNORECASE)
+REPORT_FILENAME = 'report.log'
+SINGLE_REPORT_FILENAME = "reports/report.log"
+SUMMARY_FILENAME = 'reports/report-summary.log'
+DRC_FILENAME = '5_route_drc.rpt'
+LAST_EXPECTED_LOG = '6_report.log'
+METRICS_LOG_FMT = 'gen-metrics-{}-check.log'
+METRICS_CHECK_FMT = 'metadata-{}-check.log'
+REGEX_ERROR = re.compile(r"^\[err", re.IGNORECASE)
+REGEX_WARNING = re.compile(r"^\[warn", re.IGNORECASE)
 
-helpText = '''
+HELP_TEXT = '''
 Scans "./logs" and "./reports" folders for errors and warnings.
 '''
-parser = argparse.ArgumentParser(description=helpText)
+parser = argparse.ArgumentParser(description=HELP_TEXT)
 
-helpText = '''
+HELP_TEXT = '''
 Level 0: only report fail/pass (default).
 Level 1: report count per file.
 Level 2: report full messages.
 '''
 parser.add_argument('--verbose', '-v',
                     required=False, default=0, action='count',
-                    help=helpText)
+                    help=HELP_TEXT)
 
 parser.add_argument('--quiet', '-q',
                     required=False, default=False, action='store_true',
@@ -41,16 +41,19 @@ parser.add_argument('--summary', '-s',
 args = parser.parse_args()
 
 
-def parseMessages(filename):
+def parse_messages(filename):
+    '''
+    TODO: docs
+    '''
     errors = list()
     warnings = list()
     try:
-        with open(filename, 'r') as f:
-            lines = f.readlines()
+        with open(filename, 'r') as file:
+            lines = file.readlines()
         for line in lines:
-            if re.search(regexError, line):
+            if re.search(REGEX_ERROR, line):
                 errors.append(line.strip())
-            elif re.search(regexWarning, line):
+            elif re.search(REGEX_WARNING, line):
                 warnings.append(line.strip())
     except Exception as e:
         print(f"Failed to open {filename}. Check to see if design finished.")
@@ -58,6 +61,9 @@ def parseMessages(filename):
 
 
 def gen_report(name, data):
+    '''
+    TODO: docs
+    '''
     if args.verbose or data['drcList'] or data['status'] is not 'green':
         output = f"{name}\n"
     else:
@@ -69,103 +75,114 @@ def gen_report(name, data):
         else:
             output += f"  Last log file {data['lastLog']}\n"
 
-    if len(data['logErrors']) == 0:
-        if args.verbose:
-            output += '  No error found in the log files.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['logErrors'])} error(s) in the log files.\n"
-        if args.verbose >= 1:
-            for error in data['logErrors']:
-                if args.verbose >= 2:
-                    output += f"      {error}\n"
+    if args.verbose:
+        output += f"  Found {len(data['logErrors'])} error(s) in the log files.\n"
+        for error in data['logErrors']:
+            output += f"      {error}\n"
 
-    if len(data['logWarnings']) == 0:
-        if args.verbose:
-            output += '  No warning found in the log files.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['logWarnings'])} warning(s) in the log files.\n"
-        if args.verbose >= 1:
-            for warning in data['logWarnings']:
-                if args.verbose >= 2:
-                    output += f"      {warning}\n"
+    if args.verbose:
+        output += f"  Found {len(data['logWarnings'])} warning(s) in the log files.\n"
+    if args.verbose >= 2:
+        for warning in data['logWarnings']:
+            output += f"      {warning}\n"
 
-    if len(data['metricsLogsErrors']) == 0:
-        if args.verbose:
-            output += '  No error found while generating the metrics file.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['metricsLogsErrors'])} error(s) while generating the metrics file.\n"
-        for e in data['metricsLogsErrors']:
-            output += f"    {e}\n"
+    if args.verbose:
+        output += f"  Found {len(data['metricsLogsErrors'])} error(s) while generating the metrics file.\n"
+    for error in data['metricsLogsErrors']:
+        output += f"    {error}\n"
 
-    if len(data['metricsLogsWarnings']) == 0:
-        if args.verbose:
-            output += '  No warning found while generating the metrics file.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['metricsLogsWarnings'])} warning(s) while generating the metrics file.\n"
-        if args.verbose >= 2:
-            for e in data['metricsLogsWarnings']:
-                output += f"    {e}\n"
+    if args.verbose:
+        output += f"  Found {len(data['metricsLogsWarnings'])} warning(s) while generating the metrics file.\n"
+    if args.verbose >= 2:
+        for warning in data['metricsLogsWarnings']:
+            output += f"    {warning}\n"
 
-    if len(data['metricsErrors']) == 0:
-        if args.verbose:
-            output += '  No metrics check failed.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['metricsErrors'])} failures during metrics check.\n"
-        for e in data['metricsErrors']:
-            output += f"    {e}\n"
+    if args.verbose:
+        output += f"  Found {len(data['metricsErrors'])} failures during metrics check.\n"
+    for error in data['metricsErrors']:
+        output += f"    {error}\n"
 
-    if len(data['metricsWarnings']) == 0:
-        if args.verbose:
-            output += '  No warning found during metrics check.\n'
-    else:
-        if args.verbose:
-            output += f"  Found {len(data['metricsWarnings'])} warning(s) during metrics check.\n"
-        if args.verbose >= 2:
-            for e in data['metricsWarnings']:
-                output += f"    {e}\n"
+    if args.verbose:
+        output += f"  Found {len(data['metricsWarnings'])} warning(s) during metrics check.\n"
+    if args.verbose >= 2:
+        for warning in data['metricsWarnings']:
+            output += f"    {warning}\n"
 
-    if len(data['drcList']) == 0:
-        if args.verbose:
-            output += '  No violations found.'
+    if data['status'] == 'green':
+        output += '  Design has the following violations under the allowed limit: '
     else:
-        if data['status'] == 'green':
-            output += '  Design has the following violations under the allowed limit: '
-        else:
-            output += '  Design has the following violations over the allowed limit: '
-        for drc, count in data['drcList'].items():
-            output += f"{drc} ({count}) "
-        output += '\n'
+        output += '  Design has the following violations over the allowed limit: '
+    for drc, count in data['drcList'].items():
+        output += f"{drc} ({count}) "
+    output += '\n'
 
     return output.strip()
 
 
 def write_report(path, content):
+    '''
+    TODO: docs
+    '''
     content = content.strip()
-    with open(path, 'w') as f:
-        f.write(content)
+    with open(path, 'w') as file:
+        file.write(content)
     if args.verbose:
         content += f"\nGenerated report file: {path}\n"
-    with open(singleReportFilename, 'a') as f:
-        f.write(content)
-        f.write('\n')
+    with open(SINGLE_REPORT_FILENAME, 'a') as file:
+        file.write(content)
+        file.write('\n')
     if not args.quiet and content != '':
         print(content)
 
 
-if os.path.isfile(singleReportFilename):
-    if not args.quiet:
-        print(f"Overwriting report {singleReportFilename}.")
-    os.remove(singleReportFilename)
+def get_summary(status, text):
+    '''
+    TODO: docs
+    '''
+    text += '-' * 30 + '\n'
+    text += ' ' * 7 + f"{status} designs\n"
+    text += '-' * 30 + '\n'
+    for name, data in designList.items():
+        if data['status'] == status:
+            content = gen_report(name, data)
+            if content != '':
+                text += content + '\n'
+                if args.verbose:  # add empty line for readability
+                    text += '\n'
+    return text
 
-if os.path.isfile(summaryFilename):
+
+def write_summary():
+    '''
+    Write summary and individual report files with data on designList
+    '''
+    summary = '=' * 60 + '\n'
+    summary += ' ' * 26 + 'SUMMARY\n'
+    summary += '=' * 60 + '\n'
+    summary += f"\nNumber of designs: {len(designList.keys())}\n\n"
+
+    summary = get_summary('green', summary)
+    summary = get_summary('red', summary)
+
+    if summary != '':
+        if not args.quiet:
+            print(summary)
+        with open(SUMMARY_FILENAME, 'a') as summaryFile:
+            summaryFile.write(summary)
+        if not args.quiet and args.verbose:
+            print('Generated report file:', SINGLE_REPORT_FILENAME)
+            print('Generated report summary file:', SINGLE_REPORT_FILENAME)
+
+
+if os.path.isfile(SINGLE_REPORT_FILENAME):
     if not args.quiet:
-        print(f"Overwriting report summary {summaryFilename}.")
-    os.remove(summaryFilename)
+        print(f"Overwriting report {SINGLE_REPORT_FILENAME}.")
+    os.remove(SINGLE_REPORT_FILENAME)
+
+if os.path.isfile(SUMMARY_FILENAME):
+    if not args.quiet:
+        print(f"Overwriting report summary {SUMMARY_FILENAME}.")
+    os.remove(SUMMARY_FILENAME)
 
 designList = dict()
 
@@ -178,42 +195,42 @@ for logDir, dirs, files in sorted(os.walk('logs', topdown=False)):
     # basic info about current design
     platform, design, variant = dirList[1:]
     d = dict()
-    d['outputFile'] = os.path.join(reportDir, reportFilename)
+    d['outputFile'] = os.path.join(reportDir, REPORT_FILENAME)
 
     # check if design ran to completion without errors or warnings
     for name in sorted(files):
         filename = os.path.join(logDir, name)
-        d['logErrors'], d['logWarnings'] = parseMessages(filename)
+        d['logErrors'], d['logWarnings'] = parse_messages(filename)
         if name.endswith('.log'):
             d['lastLog'] = name
-    d['finished'] = (d['lastLog'] == lastExpectedLog)
+    d['finished'] = (d['lastLog'] == LAST_EXPECTED_LOG)
 
     # check if metrics generation had issues
-    metricsLogFile = os.path.join(reportDir, metricsLogFmt.format(variant))
-    d['metricsLogsErrors'], d['metricsLogsWarnings'] = parseMessages(metricsLogFile)
+    metricsLogFile = os.path.join(reportDir, METRICS_LOG_FMT.format(variant))
+    d['metricsLogsErrors'], d['metricsLogsWarnings'] = parse_messages(metricsLogFile)
 
     # check if metrics passed
-    metricsCheckFile = os.path.join(reportDir, metricsCheckFmt.format(variant))
-    d['metricsErrors'], d['metricsWarnings'] = parseMessages(metricsCheckFile)
+    metricsCheckFile = os.path.join(reportDir, METRICS_CHECK_FMT.format(variant))
+    d['metricsErrors'], d['metricsWarnings'] = parse_messages(metricsCheckFile)
 
     # check if calibre was run and if drc check passed
     calibreCheckFile = os.path.join(logDir, 'calibre/save-to-drc-db.log')
-    d['calibreErrors'], d['calibreWarnings'] = parseMessages(calibreCheckFile)
+    d['calibreErrors'], d['calibreWarnings'] = parse_messages(calibreCheckFile)
 
     # check if there were drc violations
-    drcReportFile = os.path.join(reportDir, drcFilename)
+    drcReportFile = os.path.join(reportDir, DRC_FILENAME)
     d['drcList'] = dict()
     try:
-        with open(drcReportFile, 'r') as f:
-            for l in f.readlines():
-                if 'violation type:' in l:
-                    type_ = l.strip('violation type:').strip()
+        with open(drcReportFile, 'r') as file:
+            for line in file.readlines():
+                if 'violation type:' in line:
+                    type_ = line.strip('violation type:').strip()
                     if type_ in d['drcList'].keys():
                         d['drcList'][type_] += 1
                     else:
                         d['drcList'][type_] = 1
     except Exception as e:
-        print(f"Failed to open {drcFilename}. DRT probably did not finished.")
+        print(f"Failed to open {DRC_FILENAME}. DRT probably did not finished.")
 
     if d['metricsErrors'] or d['calibreErrors']:
         d['status'] = 'red'
@@ -222,44 +239,8 @@ for logDir, dirs, files in sorted(os.walk('logs', topdown=False)):
 
     designList[f"{platform} {design} ({variant})"] = d
 
-# write summary and individual report files with data on designList
-
-summary = '=' * 60 + '\n'
-summary += ' ' * 26 + 'SUMMARY\n'
-summary += '=' * 60 + '\n'
-summary += f"\nNumber of designs: {len(designList.keys())}\n\n"
-summary += '-' * 30 + '\n'
-summary += ' ' * 7 + 'Passing designs\n'
-summary += '-' * 30 + '\n'
-
-for name, data in designList.items():
-    if data['status'] == 'green':
-        content = gen_report(name, data)
-        if content != '':
-            summary += content + '\n'
-        if args.verbose:  # add empty line for readability
-            summary += '\n'
-        if not args.summary:
-            write_report(data['outputFile'], content)
-
-summary += '\n' + '-' * 30 + '\n'
-summary += ' ' * 7 + 'Failing designs\n'
-summary += '-' * 30 + '\n\n'
-
-for name, data in designList.items():
-    if data['status'] == 'red':
-        content = gen_report(name, data)
-        summary += content + '\n'
-        if args.verbose:  # add empty line for readability
-            summary += '\n'
-        write_report(data['outputFile'], content)
-
-if not args.quiet and summary != '':
-    print(summary)
-
-with open(summaryFilename, 'a') as summaryFile:
-    summaryFile.write(summary)
-
-if not args.quiet and args.verbose:
-    print('Generated report file:', singleReportFilename)
-    print('Generated report summary file:', singleReportFilename)
+if args.summary:
+    write_summary()
+else:
+    for name, data in designList.items():
+        write_report(data['outputFile'], gen_report(name, data))
