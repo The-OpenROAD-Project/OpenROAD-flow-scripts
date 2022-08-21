84cac546bc8973bc84cf475e8e3dbf47d5ae4a13
Anton Blanchard <anton@linux.ibm.com>
2022-08-21 21:13:19 +1000

    asap7: Add MIN_CLOCK_ROUTING_LAYER
    Use higher metal layers for clock routing.
    
    Signed-off-by: Anton Blanchard <anton@linux.ibm.com>

--- flow/platforms/asap7/config.mk
+++ flow/platforms/asap7/config.mk
@@ -111,7 +111,11 @@ export SET_RC_TCL              = $(PLATFORM_DIR)/setRC.tcl
 
 # Route options
 export MIN_ROUTING_LAYER       = M2
+export MIN_CLOCK_ROUTING_LAYER = M4
 export MAX_ROUTING_LAYER       = M7
+
+# Define fastRoute tcl
+export FASTROUTE_TCL ?= $(PLATFORM_DIR)/fastroute.tcl
 
 # KLayout technology file
 export KLAYOUT_TECH_FILE       = $(PLATFORM_DIR)/KLayout/asap7.lyt
