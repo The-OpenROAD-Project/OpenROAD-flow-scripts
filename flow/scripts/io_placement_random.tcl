956048c95cb67b58f25a06580889ff07730a9cba
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-08-29 15:54:39 -0700

    Support FLOORPLAN_DEF read in to initialize floorplan. If floorplan def is used to initialize floorplan we do not have to perform io placement. Used in top down flows
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/scripts/io_placement_random.tcl
+++ flow/scripts/io_placement_random.tcl
@@ -1,13 +1,17 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 2_1_floorplan.odb 1_synth.sdc "Starting random IO placement"
 
-if {[info exists ::env(IO_CONSTRAINTS)]} {
-  source $::env(IO_CONSTRAINTS)
-}
-place_pins -hor_layer $::env(IO_PLACER_H) \
+if {[info exists ::env(FLOORPLAN_DEF)]} {
+    puts "Use DEF in to initialization floorplan, Skip io placement."
+} else {
+  if {[info exists ::env(IO_CONSTRAINTS)]} {
+    source $::env(IO_CONSTRAINTS)
+  }
+  place_pins -hor_layer $::env(IO_PLACER_H) \
            -ver_layer $::env(IO_PLACER_V) \
            -random \
            {*}$::env(PLACE_PINS_ARGS)
+}
 
 if {![info exists save_checkpoint] || $save_checkpoint} {
   write_db $::env(RESULTS_DIR)/2_2_floorplan_io.odb
