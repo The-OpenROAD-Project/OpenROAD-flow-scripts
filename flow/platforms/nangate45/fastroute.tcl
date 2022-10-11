afe1d1b63c38942c8c9efa8651787ae469ec78c5
Eder Monteiro <emrmonteiro@inf.ufrgs.br>
2022-10-11 16:42:51 -0300

    update adjustments on metal2 and metal3 to 50%
    Signed-off-by: Eder Monteiro <emrmonteiro@inf.ufrgs.br>

--- flow/platforms/nangate45/fastroute.tcl
+++ flow/platforms/nangate45/fastroute.tcl
@@ -1,5 +1,4 @@
-set_global_routing_layer_adjustment metal2 0.8
-set_global_routing_layer_adjustment metal3 0.7
+set_global_routing_layer_adjustment metal2-metal3 0.5
 set_global_routing_layer_adjustment metal4-$::env(MAX_ROUTING_LAYER) 0.25
 
 set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
