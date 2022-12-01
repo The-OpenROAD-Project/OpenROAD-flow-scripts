5cf579fd0ff83fd7604949c7493830da65084eec
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-12-01 14:47:23 -0800

    Update nangate45 configs
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/nangate45/bp_multi_top/config_hier.mk
+++ flow/designs/nangate45/bp_multi_top/config_hier.mk
@@ -11,11 +11,11 @@ export RTLMP_MIN_INST = 5000
 export RTLMP_MAX_MACRO = 12
 export RTLMP_MIN_MACRO = 4 
 
-export DIE_AREA    = 0 0 1200 900 
-export CORE_AREA   = 10.07 9.8 1189.9 889.6
+export DIE_AREA    = 0 0 1200 1100 
+export CORE_AREA   = 10.07 9.8 1189.9 1089.6
 export PLACE_PINS_ARGS = -exclude left:* -exclude right:*
 
 export MACRO_PLACE_HALO = 5 5
 export MACRO_PLACE_CHANNEL = 10 10
 
-export PLACE_DENSITY_LB_ADDON = 0.10
+export PLACE_DENSITY_LB_ADDON = 0.02
--- flow/designs/nangate45/swerv_wrapper/config_hier.mk
+++ flow/designs/nangate45/swerv_wrapper/config_hier.mk
@@ -11,9 +11,9 @@ export RTLMP_MIN_INST = 5000
 export RTLMP_MAX_MACRO = 12
 export RTLMP_MIN_MACRO = 4
 
-export DIE_AREA    = 0 0 1200 1342.6
-export CORE_AREA   = 10.07 11.2 1190 1332.8
-export PLACE_PINS_ARGS = -exclude left:* -exclude right:* -exclude top:*
+export DIE_AREA    = 0 0 1400 1342.6
+export CORE_AREA   = 10.07 11.2 1390 1332.8
+export PLACE_PINS_ARGS = -exclude left:500-1350 -exclude right:500-1350 -exclude top:*
 
 export MACRO_PLACE_HALO = 5 5
 export MACRO_PLACE_CHANNEL = 10 10
