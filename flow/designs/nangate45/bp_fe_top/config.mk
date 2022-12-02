3abcfff31fef19d61133e69f72fc559cb2908252
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-12-02 03:42:12 -0800

    Update NG45  config.mk for macro placement designs to use hier_rtlmp as default
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/nangate45/bp_fe_top/config.mk
+++ flow/designs/nangate45/bp_fe_top/config.mk
@@ -2,6 +2,16 @@ export DESIGN_NICKNAME = bp_fe
 export DESIGN_NAME = bp_fe_top
 export PLATFORM    = nangate45
 
+export SYNTH_HIERARCHICAL = 1
+export MAX_UNGROUP_SIZE = 100000
+export RTLMP_FLOW = True
+#
+# RTL_MP Settings
+export RTLMP_MAX_INST = 20000
+export RTLMP_MIN_INST = 5000
+export RTLMP_MAX_MACRO = 12
+export RTLMP_MIN_MACRO = 4 
+
 export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/pickled.v \
                        ./designs/$(PLATFORM)/$(DESIGN_NAME)/macros.v
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
@@ -12,8 +22,11 @@ export ADDITIONAL_LIBS = $(sort $(wildcard ./designs/$(PLATFORM)/$(DESIGN_NAME)/
 
 # These values must be multiples of placement site
 # x=0.19 y=1.4
-export DIE_AREA    = 0 0 999.97 799.4
-export CORE_AREA   = 10.07 9.8 989.9 789.6
+export DIE_AREA    = 0 0 899.97 699.4
+export CORE_AREA   = 10.07 9.8 889.9 689.6
+
+export MACRO_PLACE_HALO = 5 5
+export MACRO_PLACE_CHANNEL = 10 10
 
 
 export PLACE_DENSITY = 0.14
