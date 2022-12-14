430607f8d90a159874e2b0fc4ba64fa32e34b872
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2022-12-14 06:51:04 -0800

    reverted back bp_be_top and swerv_wrapper due to CI failures
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/nangate45/swerv_wrapper/config.mk
+++ flow/designs/nangate45/swerv_wrapper/config.mk
@@ -1,15 +1,6 @@
 export DESIGN_NAME = swerv_wrapper
 export PLATFORM    = nangate45
 
-export SYNTH_HIERARCHICAL = 1
-export RTLMP_FLOW = True
-#
-# RTL_MP Settings
-export RTLMP_MAX_INST = 25000
-export RTLMP_MIN_INST = 5000
-export RTLMP_MAX_MACRO = 12
-export RTLMP_MIN_MACRO = 4
-
 export VERILOG_FILES = ./designs/src/swerv/swerv_wrapper.sv2v.v \
                        ./designs/$(PLATFORM)/swerv/macros.v
 export SDC_FILE      = ./designs/$(PLATFORM)/swerv/constraint.sdc
@@ -20,9 +11,7 @@ export ADDITIONAL_LIBS = $(sort $(wildcard ./designs/$(PLATFORM)/swerv/*.lib))
 
 # These values must be multiples of placement site
 # x=0.19 y=1.4
-export DIE_AREA    = 0 0 1400 1342.6
-export CORE_AREA   = 10.07 11.2 1390 1332.8
-export PLACE_PINS_ARGS = -exclude left:500-1350 -exclude right:500-1350 -exclude top:*
+export DIE_AREA    = 0 0 1550.02 1342.6
+export CORE_AREA   = 10.07 11.2 1540.14 1332.8
 
-export MACRO_PLACE_HALO = 5 5
-export MACRO_PLACE_CHANNEL = 10 10
+export MACRO_PLACE_HALO ?= 24.4 17.12
