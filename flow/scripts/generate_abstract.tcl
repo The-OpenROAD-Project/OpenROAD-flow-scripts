3e323dbcddffcd4523bd1661ccf8e92bd4c4cabe
Øyvind Harboe <oyvind.harboe@zylin.com>
2022-12-16 22:32:25 +0100

    write liberty file alongside .lef files for macros
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/generate_abstract.tcl
+++ flow/scripts/generate_abstract.tcl
@@ -2,6 +2,7 @@ source $::env(SCRIPTS_DIR)/load.tcl
 load_design 6_1_fill.odb 6_1_fill.sdc "Starting generation of abstract views"
  
 puts "Starting generation of abstract views"
+write_timing_model $::env(RESULTS_DIR)/$::env(DESIGN_NAME).lib
 write_abstract_lef $::env(RESULTS_DIR)/$::env(DESIGN_NAME).lef
 
 if {[info exist ::env(CDL_FILES)]} {
