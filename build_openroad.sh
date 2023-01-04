cfe52834148e1ba05b6ffd4fe02166c13fc9dc16
habibayassin <habibayassin@aucegypt.edu>
2023-01-04 00:30:33 +0000

    add support for centos in build, add platforms to docker img
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- build_openroad.sh
+++ build_openroad.sh
@@ -294,6 +294,15 @@ __local_build()
           export PATH="$(brew --prefix bison)/bin:$(brew --prefix flex)/bin:$(brew --prefix tcl-tk)/bin:$PATH"
           export CMAKE_PREFIX_PATH=$(brew --prefix or-tools)
         fi
+        if [[ -f "/opt/rh/devtoolset-8/enable" ]]; then
+            # the scl script has unbound variables
+            set +u
+            source /opt/rh/devtoolset-8/enable
+            set -u
+        fi
+        export CC="$(command -v gcc)"
+        export CXX="$(command -v g++)"
+        
         echo "[INFO FLW-0017] Compiling Yosys."
         ${NICE} make install -C tools/yosys -j "${PROC}" ${YOSYS_ARGS}
 
