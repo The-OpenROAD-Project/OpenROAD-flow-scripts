5e3c4dc6e3cf69935d5bcf0f72c248274fb1662f
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2023-01-10 04:15:40 -0800

    Update config and rules based on updated hier_rlmp (tune_mpl2 -- code of OR in PR review)
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/nangate45/bp_be_top/config.mk
+++ flow/designs/nangate45/bp_be_top/config.mk
@@ -3,13 +3,13 @@ export DESIGN_NAME = bp_be_top
 export PLATFORM    = nangate45
 
 export SYNTH_HIERARCHICAL = 1
+export RTLMP_FLOW = True
 #
 # RTL_MP Settings
 export RTLMP_MAX_INST = 30000
 export RTLMP_MIN_INST = 5000
 export RTLMP_MAX_MACRO = 12
 export RTLMP_MIN_MACRO = 4 
-
 
 export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/pickled.v \
                        ./designs/$(PLATFORM)/$(DESIGN_NAME)/macros.v
@@ -19,14 +19,12 @@ export ADDITIONAL_LEFS = $(sort $(wildcard ./designs/$(PLATFORM)/$(DESIGN_NAME)/
 export ADDITIONAL_LIBS = $(sort $(wildcard ./designs/$(PLATFORM)/$(DESIGN_NAME)/*.lib))
 
 
-# These values must be multiples of placement site
-# x=0.19 y=1.4
 export DIE_AREA    = 0 0 900 800 
 export CORE_AREA   = 10.07 11.2 890 790 
 
 export PLACE_PINS_ARGS = -exclude left:500-800 -exclude right:500-800 -exclude top:*
 
-export MACRO_PLACE_HALO = 7 7
-export MACRO_PLACE_CHANNEL = 14 14
+export MACRO_PLACE_HALO = 10 10
+export MACRO_PLACE_CHANNEL = 20 20
 
 export PLACE_DENSITY_LB_ADDON = 0.10
