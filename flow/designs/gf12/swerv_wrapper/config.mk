a127706cfba0dde6a078fff5ffb4a380423f4221
Ravi Varadarajan <rvaradarajan@ucsd.edu>
2023-01-14 06:05:18 -0800

    Update priv config and metatadata for HierRTLMP
    Signed-off-by: Ravi Varadarajan <rvaradarajan@ucsd.edu>

--- flow/designs/gf12/swerv_wrapper/config.mk
+++ flow/designs/gf12/swerv_wrapper/config.mk
@@ -1,16 +1,6 @@
 export DESIGN_NAME = swerv_wrapper
 export PLATFORM    = gf12
 
-export SYNTH_HIERARCHICAL = 1
-export MAX_UNGROUP_SIZE ?= 1000
-export RTLMP_FLOW = True
-
-# RTL_MP Settings
-export RTLMP_MAX_INST = 25000
-export RTLMP_MIN_INST = 5000
-export RTLMP_MAX_MACRO = 12
-export RTLMP_MIN_MACRO = 4
-
 export VERILOG_FILES = ./designs/src/swerv/swerv_wrapper.sv2v.v \
                        ./designs/$(PLATFORM)/$(DESIGN_NAME)/macros.v
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
@@ -27,18 +17,13 @@ export ADDITIONAL_GDS = $(PLATFORM_DIR)/gds/gf12_1rf_lg11_w40_all.gds2 \
                         $(PLATFORM_DIR)/gds/gf12_1rf_lg6_w22_all.gds2 \
                         $(PLATFORM_DIR)/gds/gf12_1rf_lg8_w34_all.gds2
 
-export MACRO_WRAPPERS = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/wrappers.tcl
-
-export DIE_AREA    = 0 0 650 550
-export CORE_AREA   = 10 10 640 540
-export PLACE_PINS_ARGS = -exclude left:* -exclude right:* -exclude top:* -exclude bottom:0-50 -exclude bottom:600-650
+export DIE_AREA    = 0 0 702 542 
+export CORE_AREA   = 2 2 700 540 
 export HAS_IO_CONSTRAINTS = 1
-#
-export MACRO_PLACE_HALO = 5 5
-export MACRO_PLACE_CHANNEL = 10 10
+export PLACE_PINS_ARGS = -exclude left:* -exclude right:* -exclude top:* -exclude bottom:0-10 -exclude bottom:400-700
 
-#export PLACE_DENSITY = 0.50
-export PLACE_DENSITY_LB_ADDON = 0.10 
+export PLACE_DENSITY = 0.40
+export MACRO_WRAPPERS = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/wrappers.tcl
 
 ifneq ($(USE_FILL),)
 export DESIGN_TYPE = CELL
--- flow/designs/tsmc65lp/ariane/config.mk
+++ flow/designs/tsmc65lp/ariane/config.mk
@@ -1,16 +1,6 @@
 export DESIGN_NAME = ariane
 export PLATFORM    = tsmc65lp
 
-export SYNTH_HIERARCHICAL = 1
-export MAX_UNGROUP_SIZE ?= 10000
-export RTLMP_FLOW = True
-#
-# RTL_MP Settings
-export RTLMP_MAX_INST = 20000
-export RTLMP_MIN_INST = 5000
-export RTLMP_MAX_MACRO = 12
-export RTLMP_MIN_MACRO = 4
-
 export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/ariane.sv2v.v \
                        ./designs/$(PLATFORM)/$(DESIGN_NAME)/macros.v
 
@@ -20,12 +10,9 @@ export ADDITIONAL_LEFS = $(PLATFORM_DIR)/lef/tsmc65lp_1rf_lg8_w64_byte.lef
 export ADDITIONAL_LIBS = $(PLATFORM_DIR)/lib/tsmc65lp_1rf_lg8_w64_byte_ss_1p08v_1p08v_125c.lib
 export ADDITIONAL_GDS  = $(PLATFORM_DIR)/gds/tsmc65lp_1rf_lg8_w64_byte.gds2
 
-export DIE_AREA    = 0 0 1950 1560
-export CORE_AREA   = 10 12 1940 1550
-export PLACE_PINS_ARGS = -exclude left:0-400 -exclude left:900-1560 -exclude right:* -exclude top:* -exclude bottom:*
+# These values must be multiples of placement site
+export DIE_AREA    = 0 0 2200 1560
+export CORE_AREA   = 10 12 2190 1550.4
+#export PLACE_PINS_ARGS = -exclude left:0-600 -exclude left:800-1560 -exclude right:* -exclude top:* -exclude bottom:*
 
-export MACRO_PLACE_HALO = 10 10
-export MACRO_PLACE_CHANNEL = 30 30
-
-#export PLACE_DENSITY = .70
-export PLACE_DENSITY_LB_ADDON = 0.10 
+export PLACE_DENSITY = 0.55
