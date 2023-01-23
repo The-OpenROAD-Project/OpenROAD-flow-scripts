1aa08a39f2ca4d9d72519be60bace2f04933c74e
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-01-23 08:01:16 +0100

    designs/asap7/mock-very-large-array: align to manufacturing grid
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/asap7/mock-very-large-array/placement.py
+++ flow/designs/asap7/mock-very-large-array/placement.py
@@ -9,6 +9,12 @@ margin = 2
 rows = 16
 cols = 16
 
+# from asap7_tech_1x_201209.lef MANUFACTURINGGRID 0.001
+mf = 0.001
+
+def manufacturinggrid(number):
+    return mf * round(number / mf)
+
 for row in range(rows):
     for col in range(cols):
-        print(f'ces_{row}_{col} R0 {col * x_pitch + offset_x} {row * y_pitch + offset_y}')
+        print(f'ces_{row}_{col} R0 {col * x_pitch + offset_x:.3f} {row * y_pitch + offset_y:.3f}')
