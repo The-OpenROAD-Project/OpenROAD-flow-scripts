bdc4c0740db03fd122a17eefe0c7f23f300e054e
Matt Liberty <mliberty@eng.ucsd.edu>
2023-02-08 04:05:38 -0800

    asap7: remove old multipler code from make_tracks & tapcell
    Signed-off-by: Matt Liberty <mliberty@eng.ucsd.edu>

--- flow/platforms/asap7/openRoad/make_tracks.tcl
+++ flow/platforms/asap7/openRoad/make_tracks.tcl
@@ -1,21 +1,18 @@
+make_tracks Pad -x_offset 0.116 -x_pitch 0.080 -y_offset 0.116 -y_pitch 0.080
+make_tracks M9  -x_offset 0.116 -x_pitch 0.080 -y_offset 0.116 -y_pitch 0.080
+make_tracks M8  -x_offset 0.116 -x_pitch 0.080 -y_offset 0.116 -y_pitch 0.080
+make_tracks M7  -x_offset 0.016 -x_pitch 0.064 -y_offset 0.016 -y_pitch 0.064
+make_tracks M6  -x_offset 0.012 -x_pitch 0.048 -y_offset 0.016 -y_pitch 0.064
+make_tracks M5  -x_offset 0.012 -x_pitch 0.048 -y_offset 0.012 -y_pitch 0.048
+make_tracks M4  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.012 -y_pitch 0.048
+make_tracks M3  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.009 -y_pitch 0.036
 
-set multiplier 1
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.045 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.081 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.117 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.153 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.189 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.225 -y_pitch 0.270
+make_tracks M2  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.270 -y_pitch 0.270
 
-make_tracks Pad -x_offset [expr 0.116 * $multiplier] -x_pitch [expr 0.08  * $multiplier] -y_offset [expr 0.116 * $multiplier] -y_pitch [expr 0.08 * $multiplier]
-make_tracks M9  -x_offset [expr 0.116 * $multiplier] -x_pitch [expr 0.08  * $multiplier] -y_offset [expr 0.116 * $multiplier] -y_pitch [expr 0.08 * $multiplier]
-make_tracks M8  -x_offset [expr 0.116 * $multiplier] -x_pitch [expr 0.08  * $multiplier] -y_offset [expr 0.116 * $multiplier] -y_pitch [expr 0.08 * $multiplier]
-make_tracks M7  -x_offset [expr 0.016 * $multiplier] -x_pitch [expr 0.064 * $multiplier] -y_offset [expr 0.016 * $multiplier] -y_pitch [expr 0.064 * $multiplier]
-make_tracks M6  -x_offset [expr 0.012 * $multiplier] -x_pitch [expr 0.048 * $multiplier] -y_offset [expr 0.016 * $multiplier] -y_pitch [expr 0.064 * $multiplier]
-make_tracks M5  -x_offset [expr 0.012 * $multiplier] -x_pitch [expr 0.048 * $multiplier] -y_offset [expr 0.012 * $multiplier] -y_pitch [expr 0.048 * $multiplier]
-make_tracks M4  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr 0.012 * $multiplier] -y_pitch [expr 0.048 * $multiplier]
-make_tracks M3  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr 0.009 * $multiplier] -y_pitch [expr 0.036 * $multiplier]
-
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.045 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.081 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.117 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.153 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.189 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.225 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-make_tracks M2  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr (0.270 - 0.000) * $multiplier] -y_pitch [expr 0.270 * $multiplier]
-
-make_tracks M1  -x_offset [expr 0.009 * $multiplier] -x_pitch [expr 0.036 * $multiplier] -y_offset [expr 0.009 * $multiplier] -y_pitch [expr 0.036 * $multiplier]
+make_tracks M1  -x_offset 0.009 -x_pitch 0.036 -y_offset 0.009 -y_pitch 0.036
