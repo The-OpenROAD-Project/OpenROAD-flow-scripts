4404dd45ee8053562587f340365e993d5a7d6662
Matt Liberty <mliberty@precisioninno.com>
2023-02-28 14:30:30 -0800

    Add MACRO_EXTENSION option to global_route.tcl
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/scripts/global_route.tcl
+++ flow/scripts/global_route.tcl
@@ -11,6 +11,9 @@ if {[info exist env(FASTROUTE_TCL)]} {
 } else {
   set_global_routing_layer_adjustment $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER) 0.5
   set_routing_layers -signal $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER)
+  if {[info exist env(MACRO_EXTENSION)]} {
+    set_macro_extension $env(MACRO_EXTENSION)
+  }
 }
 
 global_route -guide_file $env(RESULTS_DIR)/route.guide \
