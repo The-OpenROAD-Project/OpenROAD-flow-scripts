69b7c7159fb25745b09e32a91b219834d23a298c
jaswanth <jaswanthbudy@gmail.com>
2023-03-21 15:40:36 +0530

    modified the load script
    Signed-off-by: jaswanth <jaswanthbudy@gmail.com>

--- flow/scripts/gui.tcl
+++ flow/scripts/gui.tcl
@@ -2,56 +2,42 @@
 source $::env(SCRIPTS_DIR)/read_liberty.tcl
 
 # Read def
-if {[info exist ::env(DEF_FILE)]} {
-    # Read lef
-    read_lef $::env(TECH_LEF)
-    read_lef $::env(SC_LEF)
-    if {[info exist ::env(ADDITIONAL_LEFS)]} {
-      foreach lef $::env(ADDITIONAL_LEFS) {
-        read_lef $lef
-      }
-    }
-    set input_file $::env(DEF_FILE)
-    read_def $input_file
-} else {
+if {[info exists ::env(ODB_FILE)]} {
     set input_file $::env(ODB_FILE)
-    read_db $input_file
+} else {
+    error "ODB_FILE environment variable is not set."
 }
 
-if {![info exist ::env(GUI_NO_TIMING)]} {
-  # Determine design stage (1 ... 6)
-  set design_stage [lindex [split [file tail $input_file] "_"] 0]
-  
-  # Read SDC, first try to find the most recent SDC file for the stage
-  set sdc_file ""
-  for {set s $design_stage} {$s > 0} {incr s -1} {
-    set sdc_file [glob -nocomplain -directory $::env(RESULTS_DIR) -types f "${s}_\[A-Za-z\]*\.sdc"]
-    if {$sdc_file != ""} {
-      break
+if {![info exists ::env(GUI_NO_TIMING)]} {
+    # Determine design stage (1 ... 6) from input file name
+    set design_stage [regexp -inline {^(\d+)_} [file tail $input_file]]
+    
+    # Find the most recent SDC file for the stage
+    set sdc_file ""
+    for {set s $design_stage} {$s > 0 && $sdc_file == ""} {
+        set sdc_file [file exists [file join $::env(RESULTS_DIR) "${s}_*.sdc"]]
     }
-  }
-  if {$sdc_file == ""} {
-    set sdc_file $::env(SDC_FILE)
-  }
-  read_sdc $sdc_file
-  if [file exists $::env(PLATFORM_DIR)/derate.tcl] {
-    source $::env(PLATFORM_DIR)/derate.tcl
-  }
-  
-  source $::env(PLATFORM_DIR)/setRC.tcl
-  if {$design_stage >= 4} {
-    # CTS has run, so propagate clocks
-    set_propagated_clock [all_clocks]
-  }
-  
-  if {$design_stage >= 6 && [file exist $::env(RESULTS_DIR)/6_final.spef]} {
-    puts "Loading spef"
-    read_spef $::env(RESULTS_DIR)/6_final.spef
-  } elseif {$design_stage >= 3} {
-    puts "Estimating parasitics"
-    estimate_parasitics -placement
-  }
-  
-  # Cleanup temporary variables
-  unset sdc_file s design_stage
+    if {$sdc_file == ""} {
+        set sdc_file $::env(SDC_FILE)
+    }
+    read_sdc $sdc_file
+    
+    if {[file exists $::env(PLATFORM_DIR)/derate.tcl]} {
+        source $::env(PLATFORM_DIR)/derate.tcl
+    }
+    source $::env(PLATFORM_DIR)/setRC.tcl
+    
+    if {$design_stage >= 4} {
+        # CTS has run, so propagate clocks
+        set_propagated_clock [all_clocks]
+        
+        if {$design_stage >= 6 && [file exists [file join $::env(RESULTS_DIR) "6_final.spef"]]} {
+            puts "Loading spef"
+            read_spef [file join $::env(RESULTS_DIR) "6_final.spef"]
+        } elseif {$design_stage >= 3} {
+            puts "Estimating parasitics"
+            estimate_parasitics -placement
+        }
+    }
 }
+
