31b508c2079330e2648a0510aeb2aba70d4199b1
Jake Taylor <jake@ascenium.com>
2023-03-30 10:35:22 +0200

    Fix io alignment for mock-array-big
    Also clean up some of the config/setup stuff to be a bit more straightforward.
    
    Signed-off-by: Jake Taylor <jake@ascenium.com>

--- flow/designs/asap7/mock-array-big/macro-placement.tcl
+++ flow/designs/asap7/mock-array-big/macro-placement.tcl
@@ -1,6 +1,6 @@
 # fish out values from single source of truth: config.py
-set vals [regexp -all -inline {\S+} [exec sh -c {cd designs/asap7/mock-array-big && python3 -c "import config;print(f'{config.rows} {config.cols} {config.x_pitch} {config.offset_x} {config.y_pitch} {config.offset_y}')"}]]
-lassign $vals rows cols x_pitch offset_x y_pitch offset_y
+set vals [regexp -all -inline {\S+} [exec sh -c {cd designs/asap7/mock-array-big && python3 -c "import config;print(f'{config.rows} {config.cols} {config.pitch_and_margin} {config.die_offset_x} {config.die_offset_y}')"}]]
+lassign $vals rows cols pitch_and_margin die_offset_x die_offset_y
 
 set block [ord::get_db_block]
 set units [$block getDefUnits]
@@ -9,10 +9,10 @@ for {set row 0} {$row < $rows} {incr row} {
   for {set col 0} {$col < $cols} {incr col} {
     set inst [$block findInst [format "ces_%d_%d" $row $col]]
     $inst setOrient R0
-    set x [expr round([expr {$offset_x + $col * $x_pitch}] * $units)]
-    set y [expr round([expr {$offset_y + $row * $y_pitch}] * $units)]
+    set x [expr round([expr {$die_offset_x + $col * $pitch_and_margin}] * $units)]
+    set y [expr round([expr {$die_offset_y + $row * $pitch_and_margin}] * $units)]
 
-    $inst setOrigin $x $y 
+    $inst setOrigin $x $y
     $inst setPlacementStatus FIRM
   }
 }
