3b7030bbe41e0b74694ecfcfac2e48cfb0b482f8
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-04-10 10:33:59 +0200

    DO NOT MERGE more pins tests

--- flow/designs/src/mock-array-big/Element.v
+++ flow/designs/src/mock-array-big/Element.v
@@ -1,24 +1,24 @@
 module Element(
-  input        clock,
-  input  [7:0] io_ins_0,
-  input  [7:0] io_ins_1,
-  input  [7:0] io_ins_2,
-  input  [7:0] io_ins_3,
-  output [7:0] io_outs_0,
-  output [7:0] io_outs_1,
-  output [7:0] io_outs_2,
-  output [7:0] io_outs_3
+  input           clock,
+  input  [1023:0] io_ins_0,
+  input  [1023:0] io_ins_1,
+  input  [1023:0] io_ins_2,
+  input  [1023:0] io_ins_3,
+  output [1023:0] io_outs_0,
+  output [1023:0] io_outs_1,
+  output [1023:0] io_outs_2,
+  output [1023:0] io_outs_3
 );
 `ifdef RANDOMIZE_REG_INIT
-  reg [31:0] _RAND_0;
-  reg [31:0] _RAND_1;
-  reg [31:0] _RAND_2;
-  reg [31:0] _RAND_3;
+  reg [1023:0] _RAND_0;
+  reg [1023:0] _RAND_1;
+  reg [1023:0] _RAND_2;
+  reg [1023:0] _RAND_3;
 `endif // RANDOMIZE_REG_INIT
-  reg [7:0] REG; // @[MockArray.scala 33:42]
-  reg [7:0] REG_1; // @[MockArray.scala 33:42]
-  reg [7:0] REG_2; // @[MockArray.scala 33:42]
-  reg [7:0] REG_3; // @[MockArray.scala 33:42]
+  reg [1023:0] REG; // @[MockArray.scala 33:42]
+  reg [1023:0] REG_1; // @[MockArray.scala 33:42]
+  reg [1023:0] REG_2; // @[MockArray.scala 33:42]
+  reg [1023:0] REG_3; // @[MockArray.scala 33:42]
   assign io_outs_0 = REG; // @[MockArray.scala 33:13]
   assign io_outs_1 = REG_1; // @[MockArray.scala 33:13]
   assign io_outs_2 = REG_2; // @[MockArray.scala 33:13]
@@ -65,14 +65,14 @@ initial begin
       `endif
     `endif
 `ifdef RANDOMIZE_REG_INIT
-  _RAND_0 = {1{`RANDOM}};
-  REG = _RAND_0[7:0];
-  _RAND_1 = {1{`RANDOM}};
-  REG_1 = _RAND_1[7:0];
-  _RAND_2 = {1{`RANDOM}};
-  REG_2 = _RAND_2[7:0];
-  _RAND_3 = {1{`RANDOM}};
-  REG_3 = _RAND_3[7:0];
+  _RAND_0 = {32{`RANDOM}};
+  REG = _RAND_0[1023:0];
+  _RAND_1 = {32{`RANDOM}};
+  REG_1 = _RAND_1[1023:0];
+  _RAND_2 = {32{`RANDOM}};
+  REG_2 = _RAND_2[1023:0];
+  _RAND_3 = {32{`RANDOM}};
+  REG_3 = _RAND_3[1023:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
