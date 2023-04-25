62a84f506f9fc931ee3fcf5dead4dd9fa6958746
Ravi Varadarajan <rvaradarajan@or-2.ucsd.edu>
2023-04-25 14:47:00 -0700

    bp_quad (block) updates for NG45 and gf12
    Signed-off-by: Ravi Varadarajan <rvaradarajan@or-2.ucsd.edu>

--- flow/designs/gf12/bp_quad/config.mk
+++ flow/designs/gf12/bp_quad/config.mk
@@ -1,15 +1,16 @@
 export DESIGN_NICKNAME = bp_quad
 export DESIGN_NAME = bsg_chip
 export PLATFORM    = gf12
-export FLOW_VARIANT = synth
+
+export RTLMP_FLOW = True
 export SYNTH_HIERARCHICAL = 1
-export MAX_UNGROUP_SIZE ?= 10000
+export MAX_UNGROUP_SIZE ?= 1000
 
-export CACHED_NETLIST = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_dual_core_v0/yosys/bp_dual_hier_yosys_netlist.v
-export VERILOG_FILES = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_quad_core_v0/bsg_chip.sv2v.v \
-                       $(PLATFORM_DIR)/bp/IN12LP_GPIO18_13M9S30P.blackbox.v
+export CACHED_NETLIST = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_quad_core_v0/bp_quad_block/yosys/bp_quad_yosys_netlist.v
+export VERILOG_FILES = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_quad_core_v0/bp_quad_block/rtl/bsg_chip_block.sv2v.v
 
-export SDC_FILE      = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_quad_core_v0/bsg_chip.elab.v.sdc
+
+export SDC_FILE      = $(PLATFORM_DIR)/bp/bsg_ac_black_parrot_quad_core_v0/bp_quad_block/sdc/bsg_chip.sdc
 
 export WRAP_LEFS       = $(PLATFORM_DIR)/lef/gf12_1r1w_d32_w64_m1.lef \
                          $(PLATFORM_DIR)/lef/gf12_1rw_d128_w116_m2_bit.lef \
@@ -18,9 +19,6 @@ export WRAP_LEFS       = $(PLATFORM_DIR)/lef/gf12_1r1w_d32_w64_m1.lef \
                          $(PLATFORM_DIR)/lef/gf12_1rw_d64_w124_m2_bit.lef \
                          $(PLATFORM_DIR)/lef/gf12_1rw_d64_w62_m2_bit.lef
 
-export ADDITIONAL_LEFS = $(PLATFORM_DIR)/lef/IN12LP_GPIO18_13M9S30P.lef \
-                         $(PLATFORM_DIR)/lef/CDMM_13M_3Mx_2Cx_4Kx_2Hx_2Gx_LB.lef
-
 export WRAP_LIBS       = $(PLATFORM_DIR)/lib/gf12_1r1w_d32_w64_m1_ffpg_sigcmin_0p88v_0p88v_m40c.lib \
                          $(PLATFORM_DIR)/lib/gf12_1rw_d128_w116_m2_bit_ffpg_sigcmin_0p88v_0p88v_m40c.lib \
                          $(PLATFORM_DIR)/lib/gf12_1rw_d256_w48_m2_ffpg_sigcmin_0p88v_0p88v_m40c.lib \
@@ -28,7 +26,6 @@ export WRAP_LIBS       = $(PLATFORM_DIR)/lib/gf12_1r1w_d32_w64_m1_ffpg_sigcmin_0
                          $(PLATFORM_DIR)/lib/gf12_1rw_d64_w124_m2_bit_ffpg_sigcmin_0p88v_0p88v_m40c.lib \
                          $(PLATFORM_DIR)/lib/gf12_1rw_d64_w62_m2_bit_ffpg_sigcmin_0p88v_0p88v_m40c.lib
 
-export ADDITIONAL_LIBS = $(PLATFORM_DIR)/lib/IN12LP_GPIO18_13M9S30P_TT_0P8_1P8_25.lib
 
 export ADDITIONAL_GDS  = $(PLATFORM_DIR)/gds/gf12_1r1w_d32_w64_m1.gds2 \
                          $(PLATFORM_DIR)/gds/gf12_1rw_d128_w116_m2_bit.gds2 \
@@ -41,18 +38,18 @@ export ADDITIONAL_GDS  = $(PLATFORM_DIR)/gds/gf12_1r1w_d32_w64_m1.gds2 \
 
 export SEAL_GDS        = $(PLATFORM_DIR)/gds/crackstop_3x3.gds
 
+export DIE_AREA    = 0 0 1800 1800 
+export CORE_AREA   = 5 5 1795 1795
+export PLACE_PINS_ARGS = -exclude left:* -exclude right:* -exclude top:* -exclude bottom:0-800 -exclude bottom:1200-1800
+export HAS_IO_CONSTRAINTS = 1
 
-export FOOTPRINT    = $(PLATFORM_DIR)/bp/bsg_bp_quad.package.strategy
-export SIG_MAP_FILE = $(PLATFORM_DIR)/bp/soc_bsg_black_parrot.sigmap
 
-export ABC_CLOCK_PERIOD_IN_PS = 1250
-
-export PLACE_DENSITY = 0.40
+export PLACE_DENSITY_LB_ADDON = 0.02
 
 export MACRO_WRAPPERS = $(PLATFORM_DIR)/bp/wrappers/wrappers.tcl
 
 export PDN_TCL = $(PLATFORM_DIR)/cfg/pdn_grid_strategy_13m_9T.top.tcl
 
 # Define macro halo and channel spacings
-export MACRO_PLACE_HALO = 0 0
-export MACRO_PLACE_CHANNEL = 30.24 30.24
+export MACRO_PLACE_HALO = 5 5 
+export MACRO_PLACE_CHANNEL = 10 10
--- flow/designs/nangate45/bp_quad/config.mk
+++ flow/designs/nangate45/bp_quad/config.mk
@@ -6,10 +6,10 @@ export SYNTH_HIERARCHICAL = 1
 export RTLMP_FLOW = True
 
 # RTL_MP Settings
-export RTLMP_MAX_INST = 30000
-export RTLMP_MIN_INST = 5000
-export RTLMP_MAX_MACRO = 16
-export RTLMP_MIN_MACRO = 4
+#export RTLMP_MAX_INST = 30000
+#export RTLMP_MIN_INST = 5000
+#export RTLMP_MAX_MACRO = 16
+#export RTLMP_MIN_MACRO = 4
 
 export VERILOG_FILES = ./designs/src/$(DESIGN_NICKNAME)/bsg_chip_block.sv2v.v \
                        ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macros.v
@@ -30,10 +30,10 @@ export ADDITIONAL_LIBS = $(PLATFORM_DIR)/lib/fakeram45_256x48.lib \
                          $(PLATFORM_DIR)/lib/fakeram45_64x62.lib \
                          $(PLATFORM_DIR)/lib/fakeram45_128x116.lib
 
-export DIE_AREA    = 0 0 1500 1500
-export CORE_AREA   = 10 12 1448 1448
+export DIE_AREA    = 0 0 3600 3600
+export CORE_AREA   = 10 12 3590 3590 
 
-export PLACE_PINS_ARGS = -exclude left:0-500 -exclude left:1000-1500: -exclude right:* -exclude top:* -exclude bottom:*
+export PLACE_PINS_ARGS = -exclude left:* -exclude right:* -exclude top:* -exclude bottom:0-1000 -exclude bottom:2400-3600
 
 export MACRO_PLACE_HALO = 10 10
 export MACRO_PLACE_CHANNEL = 20 20
