d46682c81dd67f4cc3134d629bc0c69869d4de65
Harsh Vardhan <openroadie@chez-vardhan.com>
2023-04-28 17:27:41 -0700

    fix: Add asap7 lvt flow test

---
+++ flow/designs/asap7/jpeg_lvt/config.mk
@@ -0,0 +1,18 @@
+export PLATFORM               = asap7
+
+export DESIGN_NAME            = jpeg_encoder
+export DESIGN_NICKNAME        = jpeg_lvt
+
+export VERILOG_FILES          = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
+export VERILOG_INCLUDE_DIRS   = ./designs/src/$(DESIGN_NICKNAME)/include
+export SDC_FILE               = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/jpeg_encoder15_7nm.sdc
+export ABC_AREA               = 1
+
+export CORE_UTILIZATION       = 30
+export CORE_ASPECT_RATIO      = 1
+export CORE_MARGIN            = 2
+export PLACE_DENSITY          = 0.60
+
+export DFF_LIB_FILE           = $($(CORNER)_DFF_LIB_FILE)
+export ASAP7_USELVT           = 1
+export TNS_END_PERCENT       ?= 100
