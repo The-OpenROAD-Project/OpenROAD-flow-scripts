ef96cf28ead89443de5d857e92fa3a365c6fa56f
vijayank88 <paruthi143@gmail.com>
2023-05-08 08:58:09 +0000

    yosys mapping files added for gf180
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

---
+++ flow/platforms/gf180/cells_latch.v
@@ -0,0 +1,15 @@
+module $_DLATCH_P_(input E, input D, output Q);
+    gf180mcu_fd_sc_mcu9t5v0__latq_1 _TECHMAP_REPLACE_ (
+        .D(D),
+        .E(E),
+        .Q(Q)
+        );
+endmodule
+
+module $_DLATCH_N_(input E, input D, output Q);
+    gf180mcu_fd_sc_mcu9t5v0__latsnq_1 _TECHMAP_REPLACE_ (
+        .D(D),
+        .E(E),
+        .Q(Q)
+        );
+endmodule
