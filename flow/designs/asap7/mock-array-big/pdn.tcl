cfe643db3c7286d507d2664bf43cab35a9dea4a3
louiic <pchaiyakul@ucsd.edu>
2023-05-12 07:47:03 -0700

    Add    - ring to top level Modify - stripe frequency for M5 and M6
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/pdn.tcl
+++ flow/designs/asap7/mock-array-big/pdn.tcl
@@ -3,19 +3,23 @@
 ####################################
 add_global_connection -net {VDD} -inst_pattern {.*} -pin_pattern {^VDD$} -power
 add_global_connection -net {VSS} -inst_pattern {.*} -pin_pattern {^VSS$} -ground
+
 ####################################
 # voltage domains
 ####################################
 set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
+
 ####################################
 # standard cell grid
 ####################################
 define_pdn_grid -name {top} -voltage_domains {CORE}
 add_pdn_stripe -grid {top} -layer {M1} -width {0.018} -pitch {0.54} -offset {0} -followpins
 add_pdn_stripe -grid {top} -layer {M2} -width {0.018} -pitch {0.54} -offset {0} -followpins
-# The M5 and M6 pitches are reduced to ensure all macros have straps over them
-add_pdn_stripe -grid {top} -layer {M5} -width {0.12} -spacing {0.072} -pitch {6} -offset {0.300}
-add_pdn_stripe -grid {top} -layer {M6} -width {0.288} -spacing {0.096} -pitch {6} -offset {0.513}
+add_pdn_ring   -grid {top} -layers {M5 M6} -widths {0.504 0.544} -spacings {0.096} -core_offset {0.504}
+
+add_pdn_stripe -grid {top} -layer {M5} -width {0.12}  -spacing {0.072} -pitch {2.16} -offset {1.50} -extend_to_core_ring
+add_pdn_stripe -grid {top} -layer {M6} -width {0.288} -spacing {0.096} -pitch {4.32} -offset {1.504} -extend_to_core_ring
+
 add_pdn_connect -grid {top} -layers {M1 M2}
 add_pdn_connect -grid {top} -layers {M2 M5}
 add_pdn_connect -grid {top} -layers {M5 M6}
