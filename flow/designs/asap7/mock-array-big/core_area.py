0dcee0949b2f38ce4826cd6e7e57ebe955965bf0
louiic <pchaiyakul@ucsd.edu>
2023-05-12 07:45:43 -0700

    Modify - obtain top level core area from config.py
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/core_area.py
+++ flow/designs/asap7/mock-array-big/core_area.py
@@ -1,6 +1,3 @@
 import config
 
-x = config.core_offset_x
-y = config.core_offset_y
-
-print(f'{x} {y} {x+config.core_width} {y+config.core_height}')
+print(f'{config.margin_x} {config.margin_y} {config.core_width + config.margin_x} {config.core_height + config.margin_y}')
