104650b7935296562d14873ae06e883c7aa51d36
Ravi Varadarajan <rvaradarajan@or-2.ucsd.edu>
2023-05-19 14:44:48 -0700

    update config/rules for riscv32i
    Signed-off-by: Ravi Varadarajan <rvaradarajan@or-2.ucsd.edu>

--- flow/designs/asap7/riscv32i/config.mk
+++ flow/designs/asap7/riscv32i/config.mk
@@ -2,23 +2,23 @@ export DESIGN_NICKNAME = riscv32i
 export DESIGN_NAME = riscv_top
 export PLATFORM    = asap7
 
+export SYNTH_HIERARCHICAL = 1
+export RTLMP_FLOW = True
+export MAX_UNGROUP_SIZE ?= 1000
+
 export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 export ADDITIONAL_LEFS = ./platforms/$(PLATFORM)/lef/fakeram7_256x32.lef
 export ADDITIONAL_LIBS = ./platforms/$(PLATFORM)/lib/fakeram7_256x32.lib
 
-export DIE_AREA = 0 0 120 120
-export CORE_AREA = 5 5 110 110
+export DIE_AREA = 0 0 80 90
+export CORE_AREA = 5 5 75 85 
 
-export PLACE_DENSITY = 0.75
+export PLACE_DENSITY_LB_ADDON = 0.10
 
-export SYNTH_HIERARCHICAL = 1
-export RTLMP_FLOW = True
-# RTL_MP Settings
-export RTLMP_MAX_INST = 10000
-export RTLMP_MIN_INST = 5000
-export RTLMP_MAX_MACRO = 4
-export RTLMP_MIN_MACRO = 1
 export HAS_IO_CONSTRAINTS = 1
 export PLACE_PINS_ARGS    = -exclude left:* -exclude right:* -exclude top:*
+export MACRO_PLACE_HALO    = 3 3
+export MACRO_PLACE_CHANNEL = 6 6
+#
 export TNS_END_PERCENT   ?= 100
