6d6b36190a3fa8b0f8626df0a10b50f7eec66993
vijayank88 <paruthi143@gmail.com>
2023-06-16 07:50:31 +0000

    uart-blocks update for gf180
    Signed-off-by: vijayank88 <paruthi143@gmail.com>

--- flow/designs/gf180/uart-blocks/BLOCKS_grid_strategy.tcl
+++ flow/designs/gf180/uart-blocks/BLOCKS_grid_strategy.tcl
@@ -21,13 +21,9 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 define_pdn_grid -name {block} -voltage_domains {CORE} 
 add_pdn_stripe -grid {block} -layer {Metal1} -width {0.900} -pitch {5.040} -offset {0} -followpins
 add_pdn_stripe -grid {block} -layer {Metal4} -width {4.480} -spacing {0.56} -pitch {44.8} -offset {22.4}
-if { [info exists ::env(BLOCKS)] && [expr {$::env(BLOCKS) == $::env(DESIGN_NAME)}] } {
-   add_pdn_connect -grid {block} -layers {Metal1 Metal4}
-} else {
 add_pdn_stripe -grid {block} -layer {Metal5} -width {4.480} -pitch {89.6} -offset {44.8}
 add_pdn_connect -grid {block} -layers {Metal1 Metal4}
 add_pdn_connect -grid {block} -layers {Metal4 Metal5}
-}
 ####################################
 # Block grids
 ####################################
