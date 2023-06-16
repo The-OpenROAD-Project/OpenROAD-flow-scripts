139d7ef8b2990a4d5b1cdcda9c551e271df9adf6
Harsh Vardhan <openroad@chez-vardhan.com>
2023-06-16 10:21:46 -0700

    change skip_gate_cloning (always on) to enable_gate_cloning (always off)
    Signed-off-by: Harsh Vardhan <openroad@chez-vardhan.com>

--- flow/scripts/cts.tcl
+++ flow/scripts/cts.tcl
@@ -80,7 +80,12 @@ if { [info exists ::env(SKIP_PIN_SWAP)] } {
   append additional_args " -skip_pin_swap"
 }
 
-repair_timing -skip_gate_cloning {*}$additional_args
+if { [info exists ::env(ENABLE_GATE_CLONING)] } {
+  puts "Enable gate cloning during optimization"
+  append additional_args " -enable_gate_cloning"
+}
+
+repair_timing {*}$additional_args
 
 detailed_placement
 check_placement -verbose
