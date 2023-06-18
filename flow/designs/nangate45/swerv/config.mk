f8f73fec2b7353d794a079ae75e7dfb258853bf3
Matt Liberty <mliberty@precisioninno.com>
2023-06-17 22:50:15 -0700

    bump ng45/swerv PLACE_DENSITY_LB_ADDON to resolve grt congestion
    metrics update:
    finish__timing__drv__max_cap_limit from -0.68 to -1.4.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- flow/designs/nangate45/swerv/config.mk
+++ flow/designs/nangate45/swerv/config.mk
@@ -8,6 +8,6 @@ export CORE_UTILIZATION = 40
 export CORE_ASPECT_RATIO = 1
 export CORE_MARGIN = 5
 
-export PLACE_DENSITY_LB_ADDON = 0.20
+export PLACE_DENSITY_LB_ADDON = 0.25
 export TNS_END_PERCENT        = 100
 
