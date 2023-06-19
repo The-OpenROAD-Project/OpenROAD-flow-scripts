f3a64ede4cf9b97c4d97b98ba9d5e45d3b1ce5fd
louiic <pchaiyakul@ucsd.edu>
2023-06-19 14:43:40 -0700

    Modify - change pgrid_? to placment_grid_?
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/macro-placement.tcl
+++ flow/designs/asap7/mock-array-big/macro-placement.tcl
@@ -1,6 +1,6 @@
 # fish out values from single source of truth: config.py
-set vals [regexp -all -inline {\S+} [exec sh -c {cd designs/asap7/mock-array-big && python3 -c "import config; print(f'{config.rows} {config.cols} {config.pitch_x} {config.pitch_y} {config.margin_x} {config.margin_y} {config.pgrid_x} {config.pgrid_y}')"}]]
-lassign $vals rows cols pitch_x pitch_y margin_x margin_y pgrid_x pgrid_y
+set vals [regexp -all -inline {\S+} [exec sh -c {cd designs/asap7/mock-array-big && python3 -c "import config; print(f'{config.rows} {config.cols} {config.pitch_x} {config.pitch_y} {config.margin_x} {config.margin_y} {config.placement_grid_x} {config.placement_grid_y}')"}]]
+lassign $vals rows cols pitch_x pitch_y margin_x margin_y placement_grid_x placement_grid_y
 
 set block [ord::get_db_block]
 set units [expr [$block getDefUnits] * 1.0]
@@ -13,8 +13,8 @@ for {set row 0} {$row < $rows} {incr row} {
     set w [$bbox getDX]
     set h [$bbox getDY]
 
-    set x [expr int((($margin_x + (($pgrid_x * $pitch_x) * ($col + 1.0))) * $units) + ($w * $col))]
-    set y [expr int((($margin_y + (($pgrid_y * $pitch_y) * ($row + 1.0))) * $units) + ($h * $row))]
+    set x [expr int((($margin_x + (($placement_grid_x * $pitch_x) * ($col + 1.0))) * $units) + ($w * $col))]
+    set y [expr int((($margin_y + (($placement_grid_y * $pitch_y) * ($row + 1.0))) * $units) + ($h * $row))]
 
     $inst setOrient R0
     $inst setOrigin $x $y
