83988ba9e1c9b29c5264c2e2536fd7d20a01d91d
louiic <pchaiyakul@ucsd.edu>
2023-06-19 14:42:47 -0700

    Modify - change user configurable variable to MOCK_ARRAY_TABLE          to include rows, cols, width, height, pitch_x and pitch_y
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/defaults.mk
+++ flow/designs/asap7/mock-array-big/defaults.mk
@@ -2,14 +2,8 @@
 # each number is a unit
 # current unit is configured as 2.16 which is on the routing grid for M5
 
-# Element size (width height)
-export MOCK_ARRAY_ELEMENT_SIZE  ?= 4 4
-
-# Element placement pitch (x y)
-export MOCK_ARRAY_ELEMENT_PITCH ?= 6 6
-
-# number of Element (rows cols)
-export MOCK_ARRAY_TABLE         ?= 8 8
+# table of Elements - (rows cols width height pitch_x pitch_y)
+export MOCK_ARRAY_TABLE         ?= 8 8 4 4 6 6
 
 # Element'd data width
 export MOCK_ARRAY_DATAWIDTH     ?= 8
