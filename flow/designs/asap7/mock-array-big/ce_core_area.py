f55edbf0024d1991efe05faaafccef347037741e
louiic <pchaiyakul@ucsd.edu>
2023-06-19 14:40:58 -0700

    Modify - pgrid_? to placement_grid_?
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/ce_core_area.py
+++ flow/designs/asap7/mock-array-big/ce_core_area.py
@@ -1,6 +1,6 @@
 import config
 
-margin_x = config.pgrid_x * 0.5
-margin_y = config.pgrid_y * 0.5
+margin_x = config.placement_grid_x * 0.5
+margin_y = config.placement_grid_y * 0.5
 
 print(f'{margin_x} {margin_y} {config.ce_width - margin_x} {config.ce_height - margin_y}')
