674e8ec275687d3702eb09e87f16fecb23213dc2
louiic <pchaiyakul@ucsd.edu>
2023-06-19 14:42:06 -0700

    Modify - usage of MOCK_ARRAY_TABLE          now it includs row, cols, width, height, pitch_x and pitch_y
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/config.py
+++ flow/designs/asap7/mock-array-big/config.py
@@ -3,32 +3,28 @@ import os
 # routing pitch for M4, M5 and M6 tied to placement grid at 2.16
 # therefore, the optimal placement and Element size should be multiple of 2.16
 # set grid x and y
-pgrid_x     = 2.16
-pgrid_y     = 2.16
+placement_grid_x     = 2.16
+placement_grid_y     = 2.16
 
-# number of Elements in row and column, can be control by user via environment variable MOCK_ARRAY_TABLE (rows, cols)
-# specification is in unit of placement grid
-rows, cols = [int(os.environ.get("MOCK_ARRAY_TABLE").split()[i]) for i in (0, 1)]
-
-# Element placement pitch, can be control by user via environment variable MOCK_ARRAY_ELEMENT_PITCH (x, y)
-# specification is in unit of placement grid
-pitch_x, pitch_y = [int(os.environ.get("MOCK_ARRAY_ELEMENT_PITCH").split()[i]) for i in (0, 1)]
-
-# Element size, can be control by user via environment variable MOCK_ARRAY_ELEMENT_SIZE (width, height)
-# specification is in unit of placement grid
-ce_x, ce_y = [int(os.environ.get("MOCK_ARRAY_ELEMENT_SIZE").split()[i]) for i in (0, 1)]
+# number of Elements in row and column, can be control by user via environment variable 
+# MOCK_ARRAY_TABLE (rows, cols, width, height, pitch_x, pitch_y)
+#  rows, cols       - number of Element in rows, cols
+#  width, height    - width and height of each Element
+#  pitch_x, pitch_y - placement pitch for each Element, in x and y direction
+# specification are in unit of placement grid
+rows, cols, ce_x, ce_y, pitch_x, pitch_y = map(int, os.environ.get("MOCK_ARRAY_TABLE").split())
 
 # Element size is set to multiple of placement grid above
-ce_width    = ce_x * pgrid_x
-ce_height   = ce_y * pgrid_y
+ce_width    = ce_x * placement_grid_x
+ce_height   = ce_y * placement_grid_y
 
 # top level core offset 
-margin_x    = pgrid_x
-margin_y    = pgrid_y
+margin_x    = placement_grid_x
+margin_y    = placement_grid_y
 
 # top level core and die size
-core_width  = ((pgrid_x * pitch_x) * (cols + 1)) + (ce_width  * cols)
-core_height = ((pgrid_y * pitch_y) * (rows + 1)) + (ce_height * rows)
+core_width  = ((placement_grid_x * pitch_x) * (cols + 1)) + (ce_width  * cols)
+core_height = ((placement_grid_y * pitch_y) * (rows + 1)) + (ce_height * rows)
 
 die_width  = core_width  + (margin_x * 2)
 die_height = core_height + (margin_y * 2)
