5e333ef914ea7b4dea495c3d2bbffd5318abe26b
louiic <pchaiyakul@ucsd.edu>
2023-06-19 14:39:49 -0700

    Modify - change usage of MOCK_ARRAY_TABLE
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array-big/Element/config.mk
+++ flow/designs/asap7/mock-array-big/Element/config.mk
@@ -12,8 +12,8 @@ export PLACE_DENSITY          = 0.50
 export GPL_TIMING_DRIVEN      = 0
 export GPL_ROUTABILITY_DRIVEN = 0
 
-export CORE_AREA = $(shell export MOCK_ARRAY_ELEMENT_SIZE="$(MOCK_ARRAY_ELEMENT_SIZE)" && export MOCK_ARRAY_ELEMENT_PITCH="$(MOCK_ARRAY_ELEMENT_PITCH)" && export MOCK_ARRAY_TABLE="$(MOCK_ARRAY_TABLE)" && python3 designs/asap7/mock-array-big/ce_core_area.py)
-export DIE_AREA  = $(shell export MOCK_ARRAY_ELEMENT_SIZE="$(MOCK_ARRAY_ELEMENT_SIZE)" && export MOCK_ARRAY_ELEMENT_PITCH="$(MOCK_ARRAY_ELEMENT_PITCH)" && export MOCK_ARRAY_TABLE="$(MOCK_ARRAY_TABLE)" && python3 designs/asap7/mock-array-big/ce_die_area.py)
+export CORE_AREA = $(shell export MOCK_ARRAY_TABLE="$(MOCK_ARRAY_TABLE)" && python3 designs/asap7/mock-array-big/ce_core_area.py)
+export DIE_AREA  = $(shell export MOCK_ARRAY_TABLE="$(MOCK_ARRAY_TABLE)" && python3 designs/asap7/mock-array-big/ce_die_area.py)
 
 export IO_CONSTRAINTS         = designs/asap7/mock-array-big/Element/io.tcl
 
