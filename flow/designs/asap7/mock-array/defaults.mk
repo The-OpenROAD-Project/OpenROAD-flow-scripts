416b1f4ab197e00a348154d689a47e01fb8eccca
louiic <pchaiyakul@ucsd.edu>
2023-06-28 00:25:34 -0700

    Modify - reduce Element placement pitch to 1 1 (2.16 2.16)
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array/defaults.mk
+++ flow/designs/asap7/mock-array/defaults.mk
@@ -3,7 +3,7 @@
 # current unit is configured as 2.16 which is on the routing grid for M5
 
 # table of Elements - (rows cols width height pitch_x pitch_y)
-export MOCK_ARRAY_TABLE         ?= 8 8 4 4 6 6
+export MOCK_ARRAY_TABLE         ?= 8 8 4 4 1 1
 
 # Element'd data width
 export MOCK_ARRAY_DATAWIDTH     ?= 8
