46417bb88f342f3a0f6e413a55936594e80e81a8
louiic <pchaiyakul@ucsd.edu>
2023-06-29 14:11:05 -0700

    Modify - minimum spacing between Element's placement
    Signed-off-by: louiic <pchaiyakul@ucsd.edu>

--- flow/designs/asap7/mock-array/defaults.mk
+++ flow/designs/asap7/mock-array/defaults.mk
@@ -2,8 +2,11 @@
 # each number is a unit
 # current unit is configured as 2.16 which is on the routing grid for M5
 
-# table of Elements - (rows cols width height pitch_x pitch_y)
-export MOCK_ARRAY_TABLE         ?= 8 8 4 4 1 1
+# table of Elements - (rows cols width height space_x space_y)
+# NOTE -> in order to get power between Element's placement, 
+#   space_x >= 1
+#   space_y >= 2
+export MOCK_ARRAY_TABLE         ?= 8 8 4 4 1 2
 
 # Element'd data width
 export MOCK_ARRAY_DATAWIDTH     ?= 8
