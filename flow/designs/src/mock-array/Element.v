efd4a9b08c0bfb67f7f2ba226bfcde6c2067848d
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-07-07 10:41:14 +0200

    mock-array: make it a 64 bit array
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/src/mock-array/Element.v
+++ flow/designs/src/mock-array/Element.v
@@ -1,33 +1,33 @@
 module Element(
-  input        clock,
-  input  [7:0] io_ins_down,
-  input  [7:0] io_ins_right,
-  input  [7:0] io_ins_up,
-  input  [7:0] io_ins_left,
-  output [7:0] io_outs_down,
-  output [7:0] io_outs_right,
-  output [7:0] io_outs_up,
-  output [7:0] io_outs_left,
-  input        io_lsbIns_1,
-  input        io_lsbIns_2,
-  input        io_lsbIns_3,
-  input        io_lsbIns_4,
-  input        io_lsbIns_5,
-  input        io_lsbIns_6,
-  input        io_lsbIns_7,
-  output       io_lsbOuts_0,
-  output       io_lsbOuts_1,
-  output       io_lsbOuts_2,
-  output       io_lsbOuts_3,
-  output       io_lsbOuts_4,
-  output       io_lsbOuts_5,
-  output       io_lsbOuts_6,
-  output       io_lsbOuts_7
+  input         clock,
+  input  [63:0] io_ins_down,
+  input  [63:0] io_ins_right,
+  input  [63:0] io_ins_up,
+  input  [63:0] io_ins_left,
+  output [63:0] io_outs_down,
+  output [63:0] io_outs_right,
+  output [63:0] io_outs_up,
+  output [63:0] io_outs_left,
+  input         io_lsbIns_1,
+  input         io_lsbIns_2,
+  input         io_lsbIns_3,
+  input         io_lsbIns_4,
+  input         io_lsbIns_5,
+  input         io_lsbIns_6,
+  input         io_lsbIns_7,
+  output        io_lsbOuts_0,
+  output        io_lsbOuts_1,
+  output        io_lsbOuts_2,
+  output        io_lsbOuts_3,
+  output        io_lsbOuts_4,
+  output        io_lsbOuts_5,
+  output        io_lsbOuts_6,
+  output        io_lsbOuts_7
 );
-  reg [7:0] REG;
-  reg [7:0] REG_1;
-  reg [7:0] REG_2;
-  reg [7:0] REG_3;
+  reg [63:0] REG;
+  reg [63:0] REG_1;
+  reg [63:0] REG_2;
+  reg [63:0] REG_3;
   assign io_outs_down = REG_3;
   assign io_outs_right = REG_2;
   assign io_outs_up = REG_1;
