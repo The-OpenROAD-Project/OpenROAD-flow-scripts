ce4e2cd725780e83914c201914d5fd91ee67c2dc
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-07-29 15:52:52 +0200

    mock-array: pipeline lsb does max four CEs before registering
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/src/mock-array/Element.v
+++ flow/designs/src/mock-array/Element.v
@@ -28,6 +28,7 @@ module Element(
   reg [63:0] REG_1;
   reg [63:0] REG_2;
   reg [63:0] REG_3;
+  reg  REG_4;
   assign io_outs_down = REG_3;
   assign io_outs_right = REG_2;
   assign io_outs_up = REG_1;
@@ -35,7 +36,7 @@ module Element(
   assign io_lsbOuts_0 = io_lsbIns_1;
   assign io_lsbOuts_1 = io_lsbIns_2;
   assign io_lsbOuts_2 = io_lsbIns_3;
-  assign io_lsbOuts_3 = io_lsbIns_4;
+  assign io_lsbOuts_3 = REG_4;
   assign io_lsbOuts_4 = io_lsbIns_5;
   assign io_lsbOuts_5 = io_lsbIns_6;
   assign io_lsbOuts_6 = io_lsbIns_7;
@@ -45,5 +46,6 @@ module Element(
     REG_1 <= io_ins_right;
     REG_2 <= io_ins_up;
     REG_3 <= io_ins_left;
+    REG_4 <= io_lsbIns_4;
   end
 endmodule
