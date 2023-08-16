799eadc02378c657febd09b79ce326b2c611df7f
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-08-16 11:36:43 +0200

    makefile: add preview_macro_placement target
    for manual placement io pin placement can be skipped, which
    simplifies macro placement debugging as io pin placement can
    fail if the floorplan is completely wrong.
    
    for automatic placement, the normal flow is run and then
    the gui shows the 2_4_floorplan_macro.odb.
    
    make clean_floorplan preview_macro_placement
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/floorplan_debug_macros.tcl
+++ flow/scripts/floorplan_debug_macros.tcl
@@ -1,8 +1,8 @@
 source $::env(SCRIPTS_DIR)/load.tcl
-load_design 2_3_floorplan_tdms.odb 1_synth.sdc "Starting macro placement"
+load_design 2_1_floorplan.odb 1_synth.sdc "Debug floorplan"
 
 source $::env(SCRIPTS_DIR)/macro_place_util.tcl
 
 if {![info exists save_checkpoint] || $save_checkpoint} {
-  write_db $::env(RESULTS_DIR)/2_4_floorplan_macro.odb
+  write_db $::env(RESULTS_DIR)/2_floorplan_debug_macros.odb
 }
