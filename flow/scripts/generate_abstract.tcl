c8a5e9cbed244a5b64f44d5f8e730443ce3f78e7
Matt Liberty <mliberty@precisioninno.com>
2023-09-05 07:23:45 -0700

    Fix generate_abstract.tcl to read_spef & propagate clocks
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/scripts/generate_abstract.tcl
+++ flow/scripts/generate_abstract.tcl
@@ -1,6 +1,9 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 6_final.odb 6_final.sdc "Starting generation of abstract views"
- 
+
+read_spef $::env(RESULTS_DIR)/6_final.spef
+set_propagated_clock [all_clocks]
+
 puts "Starting generation of abstract views"
 write_timing_model $::env(RESULTS_DIR)/$::env(DESIGN_NAME).lib
 write_abstract_lef -bloat_occupied_layers $::env(RESULTS_DIR)/$::env(DESIGN_NAME).lef
