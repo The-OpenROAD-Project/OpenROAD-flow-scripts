a03fe838cefe752d9a63807d9902b21bced72258
Harsh Vardhan <openroad@chez-vardhan.com>
2023-09-06 22:24:56 -0700

    add code to write out equivalence check config file and run the equivalence check post repair_timing
    Signed-off-by: Harsh Vardhan <openroad@chez-vardhan.com>

--- flow/scripts/load.tcl
+++ flow/scripts/load.tcl
@@ -33,3 +33,54 @@ proc load_design {design_file sdc_file msg} {
     puts $msg
   }
 }
+
+#===========================================================================================
+# Routines to run equivalence tests when they are enabled. 
+
+proc get_verilog_cells_for_design { } {
+    set dir "$::env(PLATFORM_DIR)/work_around_yosys/"
+    set cell_files [glob $dir/*]
+}
+
+proc write_eqy_golden_verilog {} {
+    write_verilog before.v
+}
+
+proc write_eqy_script { } {
+    set top_cell [current_design]
+    set cell_files [get_verilog_cells_for_design]
+    set outfile [open "eqy_test.eqy" w]
+    # Gold netlist
+    puts $outfile "\[gold]\nread_verilog -sv before.v $cell_files\n"
+    puts $outfile "prep -top $top_cell -flatten\nmemory_map\n\n"
+    # Modified netlist 
+    puts $outfile "\[gate]\nread_verilog -sv after.v $cell_files\n"
+    puts $outfile "prep -top $top_cell -flatten\nmemory_map\n\n"
+
+    # Recommendation from eqy team on how to speed up a design
+    puts $outfile "\[match *]\ngate-nomatch _*_.*\n\n"
+
+    # Equivalence check recipe 1
+    puts $outfile "\[strategy basic]\nuse sat\ndepth 10\n\n"
+    # Equivalence check recipe 2
+    puts $outfile "\[strategy sby]\nuse sby\ndepth 10\nengine smtbmc bitwuzla\n\n"
+
+    # puts "[strategy sby]\nuse sby\ndepth 2\nengine smtbmc bitwuzla\n\n"
+    # puts "[strategy sby2]\nuse sby\nengine abc pdr\ndepth 10\n\n"
+    close $outfile
+}
+
+proc run_equivalence_test {} {
+    write_verilog after.v 
+    write_eqy_script
+    
+    exec rm -rf eqy_test    
+    exec eqy eqy_test.eqy > output.log
+    set count [exec grep -c "Successfully proved designs equivalent" output.log]
+    if { $count == 0 } {
+	puts "Repair timing output failed equivalence test"
+    } else {
+	puts "Repair timing output passed equivalence test"
+    }
+}
+#===========================================================================================
