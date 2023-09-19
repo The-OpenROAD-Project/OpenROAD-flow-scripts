a23637371851756b1c2d02e9954bb53a3d817f95
Matt Liberty <mliberty@precisioninno.com>
2023-09-19 11:14:07 -0700

    Keep the routing blockages in the final .odb
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/scripts/final_report.tcl
+++ flow/scripts/final_report.tcl
@@ -7,11 +7,12 @@ set_propagated_clock [all_clocks]
 # Ensure all OR created (rsz/cts) instances are connected
 global_connect
 
+write_db $::env(RESULTS_DIR)/6_final.odb
+
 # Delete routing obstructions for final DEF
 source $::env(SCRIPTS_DIR)/deleteRoutingObstructions.tcl
 deleteRoutingObstructions
 
-write_db $::env(RESULTS_DIR)/6_final.odb
 write_def $::env(RESULTS_DIR)/6_final.def
 write_verilog $::env(RESULTS_DIR)/6_final.v
 
