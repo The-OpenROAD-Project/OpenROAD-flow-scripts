fa78adfc5b581b3cab2dbcee8667f50e1f86921c
Vitor Bandeira <vvbandeira@precisioninno.com>
2023-10-03 11:11:36 -0300

    env: update, PATH no longer needs to be set
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- env.sh
+++ env.sh
@@ -1,22 +1,8 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  modroot="$(dirname $(perl -e 'use Cwd "abs_path";print abs_path(shift)' "${BASH_SOURCE[0]}"))/tools"
-else
-  modroot="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))/tools"
-fi
-
-if [ ! -d "${modroot}" ]; then
-  echo "Module path does not exist: ${modroot}"
-  return 1
-fi
-
 if [ -f /opt/rh/rh-python38/enable ]; then
+  # required for Centos7 where the newest python available is 3.6
+  # eqy requires 3.8+
   source /opt/rh/rh-python38/enable
 fi
-
-export OPENROAD=${modroot}/OpenROAD
-echo "OPENROAD: ${OPENROAD}"
-
-export PATH=${modroot}/install/OpenROAD/bin:${modroot}/install/yosys/bin:${modroot}/install/LSOracle/bin:$PATH
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export PATH="/Applications/KLayout/klayout.app/Contents/MacOS:$PATH"
