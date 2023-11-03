20047ca1322d4e66675c4bbaabb42215a57ff7f1
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-11-03 14:34:05 +0100

    makefile: add ABSTRACT_SOURCE variable for generate-abstract
    This can be used to generate an abstract using for instance
    2_floorplan stage, which allows quickly previewing
    floorplan changes at the top level.
    
    When I tested it with MegaBoom, I could view the floorplan
    after mpl2 in minutes instead of days.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/generate_abstract.tcl
+++ flow/scripts/generate_abstract.tcl
@@ -1,8 +1,13 @@
 source $::env(SCRIPTS_DIR)/load.tcl
-load_design 6_final.odb 6_final.sdc "Starting generation of abstract views"
 
-read_spef $::env(RESULTS_DIR)/6_final.spef
-set_propagated_clock [all_clocks]
+set stem [expr {[info exists ::env(ABSTRACT_SOURCE)] ? $::env(ABSTRACT_SOURCE) : "6_final"}]
+
+load_design $stem.odb $stem.sdc "Starting generation of abstract views"
+
+if {[file exists $::env(RESULTS_DIR)/$stem.spef]} {
+  read_spef $::env(RESULTS_DIR)/$stem.spef
+  set_propagated_clock [all_clocks]
+}
 
 puts "Starting generation of abstract views"
 write_timing_model $::env(RESULTS_DIR)/$::env(DESIGN_NAME).lib
@@ -10,7 +15,7 @@ write_abstract_lef -bloat_occupied_layers $::env(RESULTS_DIR)/$::env(DESIGN_NAME
 
 if {[info exist ::env(CDL_FILES)]} {
   cdl read_masters $::env(CDL_FILES)
-  cdl out $::env(RESULTS_DIR)/6_final.cdl
+  cdl out $::env(RESULTS_DIR)/$stem.cdl
 }
 
 if {![info exists standalone] || $standalone} {
