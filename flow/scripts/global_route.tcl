02efa6c90eac40bc9fc0e278e2c6ba60d5ea21b3
Øyvind Harboe <oyvind.harboe@zylin.com>
2023-11-30 12:33:32 +0100

    global and detailed route: consistent implementation cts output of arguments
    all arguments passed to cts, detailed and global route are printed
    so that it is easy to copy and paste from the log,
    modify and override in the config.mk file.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/global_route.tcl
+++ flow/scripts/global_route.tcl
@@ -22,10 +22,14 @@ if {[info exist env(FASTROUTE_TCL)]} {
 # If GLOBAL_ROUTE_ARGS is specified, then we do only what the
 # GLOBAL_ROUTE_ARGS specifies.
 proc do_global_route {} {
-  global_route -guide_file $::env(RESULTS_DIR)/route.guide \
-                -congestion_report_file $::env(REPORTS_DIR)/congestion.rpt \
-                {*}[expr {[info exists ::env(GLOBAL_ROUTE_ARGS)] ? $::env(GLOBAL_ROUTE_ARGS) : \
-                {-congestion_iterations 30 -congestion_report_iter_step 5 -verbose}}]
+  set all_args [concat [list -guide_file $::env(RESULTS_DIR)/route.guide \
+    -congestion_report_file $::env(REPORTS_DIR)/congestion.rpt] \
+    [expr {[info exists ::env(GLOBAL_ROUTE_ARGS)] ? $::env(GLOBAL_ROUTE_ARGS) : \
+     {-congestion_iterations 30 -congestion_report_iter_step 5 -verbose}}]]
+
+  puts "global_route [join $all_args " "]"
+
+  global_route {*}$all_args
 }
 
 set result [catch {do_global_route} errMsg]
