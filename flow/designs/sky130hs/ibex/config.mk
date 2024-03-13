5e80390540a890438defd16d800433467c61d85b
Augusto Berndt <gudehh@gmail.com>
2024-03-13 15:58:07 -0300

    gpl: udpate metrics and use past default target RC for some designs we require the two designs to use the past target RC value of 1.25 because they trigger a consistent error on antenna checker. This should be removed in the future when antenna check is fixed.
    Signed-off-by: Augusto Berndt <gudehh@gmail.com>

--- flow/designs/sky130hs/ibex/config.mk
+++ flow/designs/sky130hs/ibex/config.mk
@@ -44,3 +44,5 @@ export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 export CORE_UTILIZATION = 45
 export PLACE_DENSITY_LB_ADDON = 0.2
 export TNS_END_PERCENT = 100
+
+export GPL_TARGET_RC = 1.25
--- flow/designs/sky130hs/jpeg/config.mk
+++ flow/designs/sky130hs/jpeg/config.mk
@@ -14,5 +14,4 @@ export CORE_MARGIN = 2
 export PLACE_DENSITY_LB_ADDON = 0.25
 export TNS_END_PERCENT = 100
 
-# Temporary
-export SKIP_ANTENNA_REPAIR = 1
+export GPL_TARGET_RC = 1.25
