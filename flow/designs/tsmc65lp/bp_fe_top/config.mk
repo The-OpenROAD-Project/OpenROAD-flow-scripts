ba95ce64f6d5c9c38548fc9b9f77a530453be175
Matt Liberty <mliberty@precisioninno.com>
2024-04-25 09:51:51 -0700

    Get tsmc65lp/be_fe_top working again
    Setting HAS_IO_CONSTRAINTS=0 to restore the old results even though the
    design does have io constraints.  The ppl results change, within the
    constraints, leading to dpl failure.
    
    It would be good to revisit this later and remove the need for this
    setting.
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/designs/tsmc65lp/bp_fe_top/config.mk
+++ flow/designs/tsmc65lp/bp_fe_top/config.mk
@@ -25,6 +25,7 @@ export ADDITIONAL_GDS  = $(PLATFORM_DIR)/gds/tsmc65lp_1rf_lg6_w8_bit.gds2 \
 export DIE_AREA    = 0 0 880 780 
 export CORE_AREA   = 10 12 870 770
 export PLACE_PINS_ARGS = -exclude left:* -exclude right:0-400 -exclude bottom:*
+export HAS_IO_CONSTRAINTS = 0
 
 export MACRO_PLACE_HALO = 10 10
 export MACRO_PLACE_CHANNEL = 20 20
