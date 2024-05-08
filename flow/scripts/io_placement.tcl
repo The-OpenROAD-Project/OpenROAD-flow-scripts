c9cdecdd60d1f9d27b5c2c8e41cfeeca020009fc
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-05-08 14:24:49 +0200

    io_placement: less repetition, more consistent logging and GUI interactive IO placement
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/io_placement.tcl
+++ flow/scripts/io_placement.tcl
@@ -1,15 +1,6 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 3_1_place_gp_skip_io.odb 2_floorplan.sdc
 
-if {[info exists ::env(FLOORPLAN_DEF)]} {
-    puts "Skipping IO placement as DEF file was used to initialize floorplan."
-} else {
-  if {[info exists ::env(IO_CONSTRAINTS)]} {
-    source $::env(IO_CONSTRAINTS)
-  }
-  place_pins -hor_layer $::env(IO_PLACER_H) \
-           -ver_layer $::env(IO_PLACER_V) \
-           {*}$::env(PLACE_PINS_ARGS)
-}
+source $::env(SCRIPTS_DIR)/io_placement_util.tcl
 
 write_db $::env(RESULTS_DIR)/3_2_place_iop.odb
