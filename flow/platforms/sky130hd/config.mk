3b6a73614418d7a2973673a6867b605a3bef799d
Matt Liberty <mliberty@precisioninno.com>
2024-05-14 16:59:54 -0700

    Set zero padding in all the public PDKs
    asap7/aes-block
    
    | Metric                                        | Old      | New      | Type     |
    | ------                                        | ---      | ---      | ----     |
    | cts__design__instance__count__hold_buffer     |     1309 |     1491 | Failing  |
    | detailedroute__route__wirelength              |    82822 |    80385 | Tighten  |
    | finish__timing__setup__ws                     |  -338.86 |  -311.26 | Tighten  |
    
    gf180/ibex
    
    | Metric                                        | Old      | New      | Type     |
    | ------                                        | ---      | ---      | ----     |
    | finish__timing__setup__ws                     |    -3.85 |    -2.56 | Tighten  |
    | finish__timing__drv__setup_violation_count    |      916 |      750 | Tighten  |
    | finish__timing__drv__hold_violation_count     |      100 |      261 | Failing  |
    | finish__timing__wns_percent_delay             |   -29.52 |   -25.78 | Tighten  |
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/platforms/sky130hd/config.mk
+++ flow/platforms/sky130hd/config.mk
@@ -102,7 +102,7 @@ export MACRO_PLACE_CHANNEL ?= 80 80
 # Place
 # --------------------------------------------------------
 # default cell padding for cells 
-export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT ?= 1
+export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT ?= 0
 export CELL_PAD_IN_SITES_DETAIL_PLACEMENT ?= 0
 
 export PLACE_DENSITY ?= 0.60
