d870b0fe9f17f093958b2e29601040aba84d5189
habibayassin <habibayassin@aucegypt.edu>
2024-06-02 23:40:23 +0000

    pass private pdk
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- flow/util/genRuleFile.py
+++ flow/util/genRuleFile.py
@@ -91,8 +91,8 @@ def gen_rule_file(design_dir, update, tighten, failing, variant, golden_metrics=
     platform = path_parts[-3]
     design = path_parts[-2]
 
-    if platform is in ["intel22", "intel16", "gf12", "gf55", "tsmc65lp"]:
-        continue
+    if platform in ["intel22", "intel16", "gf12", "gf55", "tsmc65lp"]:
+        pass
 
     # dict format
     # 'metric_name': {
