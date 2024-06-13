34f149dad69f17664712dabb50c1e28e0a8fec33
Jack Luar <39641663+luarss@users.noreply.github.com>
2024-06-13 01:39:44 +0000

    attempt fix at skipping artifacts
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- flow/util/genReport.py
+++ flow/util/genReport.py
@@ -261,6 +261,8 @@ for log_dir, dirs, files in sorted(os.walk(LOGS_FOLDER, topdown=False)):
     d["log_errors"] = list()
     d["log_warnings"] = list()
     for name_ in sorted(files):
+        # ignore autotuner artifacts (json/pkl)
+        if not name.endswith('.log'): continue 
         temp_e, temp_w = parse_messages(os.path.join(log_dir, name_))
         d["log_errors"] += temp_e
         d["log_warnings"] += temp_w
