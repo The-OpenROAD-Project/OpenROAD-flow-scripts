a0802c04042315588ebf65b9cdb8492f8345333a
Vitor Bandeira <vvbandeira@precisioninno.com>
2024-07-03 14:04:18 -0300

    docker: use shorter tags
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

---
+++ etc/DockerTag.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cd $(dirname $(realpath $0))/../
+
+if [[ "$@" == "-dev" ]]; then
+    file_list=(
+        "./build_openroad.sh"
+        "./docker/Dockerfile.builder"
+        "./docker/Dockerfile.dev"
+        "./etc/DependencyInstaller.sh"
+        "./etc/DockerHelper.sh"
+        "./tools/OpenROAD/docker/Dockerfile.builder"
+        "./tools/OpenROAD/docker/Dockerfile.dev"
+        "./tools/OpenROAD/docker/Dockerfile.binary"
+        "./tools/OpenROAD/etc/DependencyInstaller.sh"
+    )
+    cat "${file_list[@]}" | sha256sum | awk '{print substr($1, 1, 6)}'
+elif [[ "$@" == "-master" ]]; then
+    git describe
+else
+    echo "Usage: $0 {-dev|-master}"
+    exit 1
+fi
