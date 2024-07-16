5c5dca003640c4ddbe1e8c601f61e04bc7c19325
habibayassin <habibayassin@aucegypt.edu>
2024-07-16 15:40:09 +0000

    move for loop
    Signed-off-by: habibayassin <habibayassin@aucegypt.edu>

--- flow/util/updateRules.py
+++ flow/util/updateRules.py
@@ -36,6 +36,15 @@ api_base_url = args.apiURL
 
 runFilename = f'rules-base.json'
 
+platform_list = []
+platforms_path = 'platforms'
+for item in os.listdir():
+    if item == 'sky130hd_fakestack':
+        continue
+    item_path = os.path.join(platforms_path, item)
+    if os.path.isdir(item_path):
+        platform_list.append(item)
+
 for designsDir, dirs, files in sorted(os.walk('designs', topdown=False)):
     dirList = designsDir.split(os.sep)
     if len(dirList) != 3:
@@ -45,7 +54,7 @@ for designsDir, dirs, files in sorted(os.walk('designs', topdown=False)):
     design = dirList[2]
     test = '{} {}'.format(platform, design)
     dataFile = os.path.join(designsDir, runFilename)
-    if os.path.exists(dataFile) and (platform != 'sky130hd_fakestack' or platform != 'src'):
+    if os.path.exists(dataFile) and platform in platform_list:
         metrics, error_metrics = get_metrics(args.commitSHA, # commit
                             platform, # platform
                             design, # design
