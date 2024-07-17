3ff4b7e51686e4fec174bd7aacccada94bc64b31
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-07-17 13:50:12 +0200

    mock-alu: lock TNS_END_PERCENT to 5
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/asap7/mock-alu/config.mk
+++ flow/designs/asap7/mock-alu/config.mk
@@ -5,3 +5,6 @@ include designs/src/mock-alu/defaults.mk
 export PLACE_DENSITY          = 0.75
 export CORE_UTILIZATION       = 50
 export CORNER            = BC
+# PLACE_DENSITY is very tight, to adjust this up, PLACE_DENSITY also
+# needs to be adjusted.
+export TNS_END_PERCENT = 5
