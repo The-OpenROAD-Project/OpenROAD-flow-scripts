15c4bd77e1769997308f0accbc31963d1c169a34
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-07-18 10:41:03 +0200

    mock-array: no read_library Element.lib and read Element spef file correctly
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/asap7/mock-array/power.tcl
+++ flow/designs/asap7/mock-array/power.tcl
@@ -1,5 +1,7 @@
 foreach libFile $::env(LIB_FILES) {
-read_liberty $libFile
+  if {[lsearch -exact $::env(ADDITIONAL_LIBS) $libFile] == -1} {
+    read_liberty $libFile
+  }
 }
 
 read_verilog results/asap7/mock-array_Element/base/6_final.v
@@ -10,9 +12,13 @@ link_design MockArray
 
 read_sdc $::env(RESULTS_DIR)/6_final.sdc
 read_spef $::env(RESULTS_DIR)/6_final.spef
-# FIXME lots of warnings
-#read_spef results/asap7/mock-array_Element/base/6_final.spef
+for {set x 0} {$x < 8} {incr x} {
+  for {set y 0} {$y < 8} {incr y} {
+    read_spef -path ces_${x}_${y} results/asap7/mock-array_Element/base/6_final.spef
+  }
+}
 
+report_parasitic_annotation
 report_power
 read_power_activities -scope TOP/MockArrayTestbench/postSynthesis -vcd designs/src/mock-array/MockArrayTestbench.vcd
 report_power
