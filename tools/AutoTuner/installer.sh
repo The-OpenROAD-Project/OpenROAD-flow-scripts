85438411b6cf40e188bf728a747fe3b573a3316b
Vitor Bandeira <vvbandeira@users.noreply.github.com>
2024-07-23 09:51:00 -0300

    Update tools/AutoTuner/installer.sh
    Signed-off-by: Vitor Bandeira <vvbandeira@users.noreply.github.com>

--- tools/AutoTuner/installer.sh
+++ tools/AutoTuner/installer.sh
@@ -15,7 +15,7 @@ fi
 
 # Define the virtual environment name
 venv_name="autotuner_env"
-python3.8 -m venv "$script_dir/$venv_name"
+python${required_version} -m venv "$script_dir/$venv_name"
 source "$script_dir/$venv_name/bin/activate"
 pip3 install -U -r requirements.txt
 deactivate
