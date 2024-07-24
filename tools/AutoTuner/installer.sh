30ec33f21ee5062099d49e707de68812c51d2622
luarss <39641663+luarss@users.noreply.github.com>
2024-07-25 01:28:48 +0800

    remove python version check
    Signed-off-by: luarss <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/installer.sh
+++ tools/AutoTuner/installer.sh
@@ -4,15 +4,6 @@
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 cd $script_dir
 
-# Check if the Python version is 3.8
-required_version="3.8"
-if command -v python${required_version} &> /dev/null ; then
-    echo "Found python${required_version}"
-else
-    echo "Error: Python ${required_version} is required."
-    exit 1
-fi
-
 # Define the virtual environment name
 venv_name="autotuner_env"
 python${required_version} -m venv "$script_dir/$venv_name"
