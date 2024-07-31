4e97716fb2af607c74cad14bbadba07eecf26269
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-07-31 10:37:49 +0200

    test: another deltadebug use-case with bisecting make instead of make issue
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/test/test_delta_debug.sh
+++ flow/test/test_delta_debug.sh
@@ -6,14 +6,19 @@
 # variable names such as $(false), unused variables, etc.
 set -ue -o pipefail
 
-cd flow
-testname=uart
+cd "$(dirname "$0")/.."
+. ../env.sh
 
+echo "Test deltaDebug.py with a make invocation"
+make DESIGN_CONFIG=designs/asap7/gcd/config.mk clean_all floorplan
+openroad -exit -python ../tools/OpenROAD/etc/deltaDebug.py --persistence 2 --use_stdout --error_string "100 overflow" --base_db_path results/asap7/gcd/base/2_floorplan.odb --step "make DESIGN_CONFIG=designs/asap7/gcd/config.mk do-3_1_place_gp_skip_io"
+
+echo "Test deltaDebug.py with a make issue"
+testname=uart
 make DESIGN_CONFIG=designs/asap7/$testname/config.mk place
 make DESIGN_CONFIG=designs/asap7/$testname/config.mk global_place_issue
 latest_file=$(ls -t global_place_${testname}_asap7_base*.tar.gz | head -n1)
 echo "Testing $latest_file"
-. ../env.sh
 rm -rf results/delta-debug/
 mkdir -p results/delta-debug/
 cd results/delta-debug/
