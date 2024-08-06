4970aa59c703c72fbbcfd51338e890c3b26982f6
luarss <39641663+luarss@users.noreply.github.com>
2024-08-07 00:38:55 +0800

    fix black
    Signed-off-by: luarss <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/test/smoke_test_tune.py
+++ tools/AutoTuner/test/smoke_test_tune.py
@@ -1,23 +1,31 @@
 import unittest
 import subprocess
 import os
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(cur_dir, "../src/autotuner")
 os.chdir(src_dir)
+
 
 class BaseTuneSmokeTest(unittest.TestCase):
     platform = ""
     design = ""
 
     def setUp(self):
-        self.config = os.path.join(cur_dir, f"../../../flow/designs/{self.platform}/{self.design}/autotuner.json")
+        self.config = os.path.join(
+            cur_dir,
+            f"../../../flow/designs/{self.platform}/{self.design}/autotuner.json",
+        )
         self.experiment = f"smoke-test-tune-{self.platform}"
-        self.command = ("python3 distributed.py"
-                        f" --design {self.design}"
-                        f" --platform {self.platform}"
-                        f" --experiment {self.experiment}"
-                        f" --config {self.config}"
-                        f" tune --samples 1")
+        self.command = (
+            "python3 distributed.py"
+            f" --design {self.design}"
+            f" --platform {self.platform}"
+            f" --experiment {self.experiment}"
+            f" --config {self.config}"
+            f" tune --samples 1"
+        )
+
 
 class ASAP7TuneSmokeTest(BaseTuneSmokeTest):
     platform = "asap7"
@@ -28,6 +36,7 @@ class ASAP7TuneSmokeTest(BaseTuneSmokeTest):
         successful = out.returncode == 0
         self.assertTrue(successful)
 
+
 class SKY130HDTuneSmokeTest(BaseTuneSmokeTest):
     platform = "sky130hd"
     design = "gcd"
@@ -36,6 +45,7 @@ class SKY130HDTuneSmokeTest(BaseTuneSmokeTest):
         out = subprocess.run(self.command, shell=True, check=True)
         successful = out.returncode == 0
         self.assertTrue(successful)
+
 
 class IHPSG13G2TuneSmokeTest(BaseTuneSmokeTest):
     platform = "ihp-sg13g2"
@@ -46,5 +56,6 @@ class IHPSG13G2TuneSmokeTest(BaseTuneSmokeTest):
         successful = out.returncode == 0
         self.assertTrue(successful)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
