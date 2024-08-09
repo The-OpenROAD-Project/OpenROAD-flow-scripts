2687ef8b26c0071483b7edcd6d40fd2a59212d97
luarss <39641663+luarss@users.noreply.github.com>
2024-08-09 14:55:48 +0800

    fix python lint
    Signed-off-by: luarss <39641663+luarss@users.noreply.github.com>

--- tools/AutoTuner/test/smoke_test_sample_iteration.py
+++ tools/AutoTuner/test/smoke_test_sample_iteration.py
@@ -1,27 +1,33 @@
 import unittest
 import subprocess
 import os
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(cur_dir, "../src/autotuner")
 os.chdir(src_dir)
+
 
 class BaseSampleIterationSmokeTest(unittest.TestCase):
     platform = ""
     design = ""
 
     def setUp(self):
-        self.config = os.path.join(cur_dir, f"../../../flow/designs/{self.platform}/{self.design}/autotuner.json")
+        self.config = os.path.join(
+            cur_dir,
+            f"../../../flow/designs/{self.platform}/{self.design}/autotuner.json",
+        )
         self.experiment = f"smoke-test-sample-iteration-{self.platform}"
-        self.matrix = [(5,1), (1,5), (2,2), (1,1)]
+        self.matrix = [(5, 1), (1, 5), (2, 2), (1, 1)]
         self.commands = [
-                        f"python3 distributed.py"
-                        f" --design {self.design}"
-                        f" --platform {self.platform}"
-                        f" --experiment {self.experiment}"
-                        f" --config {self.config}"
-                        f" tune --samples {s} --iterations {i}"
-                        for s, i in self.matrix
-                        ] 
+            f"python3 distributed.py"
+            f" --design {self.design}"
+            f" --platform {self.platform}"
+            f" --experiment {self.experiment}"
+            f" --config {self.config}"
+            f" tune --samples {s} --iterations {i}"
+            for s, i in self.matrix
+        ]
+
 
 class ASAP7SampleIterationSmokeTest(BaseSampleIterationSmokeTest):
     platform = "asap7"
@@ -34,6 +40,7 @@ class ASAP7SampleIterationSmokeTest(BaseSampleIterationSmokeTest):
             successful = out.returncode == 0
             self.assertTrue(successful)
 
+
 class SKY130HDSampleIterationSmokeTest(BaseSampleIterationSmokeTest):
     platform = "sky130hd"
     design = "gcd"
@@ -44,6 +51,7 @@ class SKY130HDSampleIterationSmokeTest(BaseSampleIterationSmokeTest):
             out = subprocess.run(command, shell=True, check=True)
             successful = out.returncode == 0
             self.assertTrue(successful)
+
 
 class IHPSG13G2SampleIterationSmokeTest(BaseSampleIterationSmokeTest):
     platform = "ihp-sg13g2"
@@ -56,6 +64,6 @@ class IHPSG13G2SampleIterationSmokeTest(BaseSampleIterationSmokeTest):
             successful = out.returncode == 0
             self.assertTrue(successful)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-    
