9aa6cabec49cdb990fad7ad029f1eff43d842e2f
Cho Moon <cmoon@precisioninno.com>
2024-08-14 22:13:06 +0000

    fixed merge issue with global_route.tcl
    Signed-off-by: Cho Moon <cmoon@precisioninno.com>

--- flow/scripts/global_route.tcl
+++ flow/scripts/global_route.tcl
@@ -53,20 +53,6 @@ proc global_route_helper {} {
   set_propagated_clock [all_clocks]
   estimate_parasitics -global_routing
 
-  # process user settings
-  set additional_args "-verbose"
-  append_env_var additional_args SETUP_SLACK_MARGIN -setup_margin 1
-  append_env_var additional_args HOLD_SLACK_MARGIN -hold_margin 1
-  append_env_var additional_args TNS_END_PERCENT -repair_tns 1
-  append_env_var additional_args SKIP_PIN_SWAP -skip_pin_swap 0
-  append_env_var additional_args SKIP_GATE_CLONING -skip_gate_cloning 0
-  append_env_var additional_args SKIP_BUFFER_REMOVAL -skip_buffer_removal 0
-  puts "repair_timing [join $additional_args " "]"
-  repair_timing {*}$additional_args
-
-  if {[info exist ::env(DETAILED_METRICS)]} {
-    report_metrics 5 "global route post repair timing"
-
   if {[info exist ::env(DONT_USE_CELLS)]} {
     set_dont_use $::env(DONT_USE_CELLS)
   }
@@ -96,8 +82,12 @@ proc global_route_helper {} {
 
     # process user settings
     set additional_args "-verbose"
+    append_env_var additional_args SETUP_SLACK_MARGIN -setup_margin 1
+    append_env_var additional_args HOLD_SLACK_MARGIN -hold_margin 1
+    append_env_var additional_args TNS_END_PERCENT -repair_tns 1
     append_env_var additional_args SKIP_PIN_SWAP -skip_pin_swap 0
     append_env_var additional_args SKIP_GATE_CLONING -skip_gate_cloning 0
+    append_env_var additional_args SKIP_BUFFER_REMOVAL -skip_buffer_removal 0
     puts "repair_timing [join $additional_args " "]"
     repair_timing {*}$additional_args
 
