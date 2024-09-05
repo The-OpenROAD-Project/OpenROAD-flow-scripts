c0fc69c30e743c7e2f67e3882560589e549fbf51
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-05 03:49:58 +0200

    fast route: avoid repetition
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/gui.tcl
+++ flow/scripts/gui.tcl
@@ -53,15 +53,7 @@ if {![info exist ::env(GUI_NO_TIMING)]} {
     log_cmd estimate_parasitics -placement
   }
 
-  if {[info exist env(FASTROUTE_TCL)]} {
-    source $env(FASTROUTE_TCL)
-  } else {
-    set_global_routing_layer_adjustment $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER) 0.5
-    set_routing_layers -signal $env(MIN_ROUTING_LAYER)-$env(MAX_ROUTING_LAYER)
-    if {[info exist env(MACRO_EXTENSION)]} {
-      set_macro_extension $env(MACRO_EXTENSION)
-    }
-  }
+  fast_route
   
   # Cleanup temporary variables
   unset sdc_file s design_stage
