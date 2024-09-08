6383f4db5ed88eb68f49c4f810e7d36e1719e0d3
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-08 21:02:10 +0200

    scripts: add machine readable list of where variables are used
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/util.tcl
+++ flow/scripts/util.tcl
@@ -14,3 +14,26 @@ proc fast_route {} {
     }
   }
 }
+
+# Clean out unused environment variables so as to catch
+# errors in the environment.yaml file that enumerates which
+# envvars is used in which stage.
+proc clean_environment_yaml {} {
+  set env_file [file join $::env(SCRIPTS_FOLDER) environment.yaml]
+  set env_data [yaml::load [read_file $env_file]]
+  # we want only the name, not the full path of the last argument,
+  # exclude the .tcl ending.
+  set script [file tail [file rootname [file tail $::env(SCRIPT)]]]
+  set stage [dict get $script [dict get script_stages $env_data]]
+  foreach env_var [array names ::env] {
+    if {![dict exists $env_data $env_var]} {
+      continue
+    }
+    set stages [dict get $env_data $env_var]
+    if {[lsearch -exact $stages $stage] == -1} {
+      unset ::env($env_var)
+    }
+  }
+}
+
+clean_environment_yaml
