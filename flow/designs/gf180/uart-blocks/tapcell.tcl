eb1cab27d6adac3eaf4dd654e48f35afb6c5e087
Jeff Ng <jeffng@precisioninno.com>
2024-09-09 11:16:26 -0700

    fixed bad merge
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/designs/gf180/uart-blocks/tapcell.tcl
+++ flow/designs/gf180/uart-blocks/tapcell.tcl
@@ -1,13 +1,8 @@
-if {[info exists ::env(TAP_CELL_NAME)] && $::env(TAP_CELL_NAME) != "" &&
-    [info exists ::env(ENDCAP_CELL_NAME)] && $::env(ENDCAP_CELL_NAME) != ""} {
-    tapcell \
-	-endcap_cpp             "12" \
-	-distance               100 \
-	-tapcell_master         $::env(TAP_CELL_NAME) \
-	-endcap_master          $::env(ENDCAP_CELL_NAME) \
-	-halo_width_x $::env(MACRO_HALO_X) \
-	-halo_width_y $::env(MACRO_HALO_Y)
-} else {
-    puts "Skipping tapcell insertion since TAP_CELL_NAME and/or ENDCAP_CELL_NAME are not defined"
-}
+  tapcell \
+    -endcap_cpp             "12" \
+    -distance               100 \
+    -tapcell_master         $::env(TIE_CELL) \
+    -endcap_master          $::env(ENDCAP_CELL) \
+    -halo_width_x $::env(MACRO_HALO_X) \
+    -halo_width_y $::env(MACRO_HALO_Y)
 
