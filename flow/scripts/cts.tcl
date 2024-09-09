37b8614f36e92fbabd90730f43ab38dd04c15df8
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-09 21:24:07 +0200

    scripts: less repetition
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/cts.tcl
+++ flow/scripts/cts.tcl
@@ -71,22 +71,12 @@ if {[info exist ::env(CTS_SNAPSHOTS)]} {
   save_progress 4_1_pre_repair_hold_setup
 }
 
-# process user settings
-set additional_args "-verbose"
-append_env_var additional_args SETUP_SLACK_MARGIN -setup_margin 1
-append_env_var additional_args HOLD_SLACK_MARGIN -hold_margin 1
-append_env_var additional_args TNS_END_PERCENT -repair_tns 1
-append_env_var additional_args SKIP_PIN_SWAP -skip_pin_swap 0
-append_env_var additional_args SKIP_GATE_CLONING -skip_gate_cloning 0
-append_env_var additional_args SKIP_BUFFER_REMOVAL -skip_buffer_removal 0
-
 if {[info exists ::env(SKIP_CTS_REPAIR_TIMING)] == 0 || $::env(SKIP_CTS_REPAIR_TIMING) == 0} {
   if {[info exists ::env(EQUIVALENCE_CHECK)] && $::env(EQUIVALENCE_CHECK) == 1} {
       write_eqy_verilog 4_before_rsz.v
   }
 
-  puts "repair_timing [join $additional_args " "]"
-  repair_timing {*}$additional_args
+  repair_timing_helper
 
   if {[info exists ::env(EQUIVALENCE_CHECK)] && $::env(EQUIVALENCE_CHECK) == 1} {
       run_equivalence_test
--- flow/scripts/global_route.tcl
+++ flow/scripts/global_route.tcl
@@ -72,16 +72,7 @@ proc global_route_helper {} {
     puts "Repair setup and hold violations..."
     estimate_parasitics -global_routing
 
-    # process user settings
-    set additional_args "-verbose"
-    append_env_var additional_args SETUP_SLACK_MARGIN -setup_margin 1
-    append_env_var additional_args HOLD_SLACK_MARGIN -hold_margin 1
-    append_env_var additional_args TNS_END_PERCENT -repair_tns 1
-    append_env_var additional_args SKIP_PIN_SWAP -skip_pin_swap 0
-    append_env_var additional_args SKIP_GATE_CLONING -skip_gate_cloning 0
-    append_env_var additional_args SKIP_BUFFER_REMOVAL -skip_buffer_removal 0
-    puts "repair_timing [join $additional_args " "]"
-    repair_timing {*}$additional_args
+    repair_timing_helper
 
     if {[info exist ::env(DETAILED_METRICS)]} {
       report_metrics 5 "global route post repair timing"
