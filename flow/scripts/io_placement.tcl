39c5495f52f46e8e57df6733fce1c2202ddef27a
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-23 20:05:15 +0200

    makefile: less iffy code
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/io_placement.tcl
+++ flow/scripts/io_placement.tcl
@@ -1,6 +1,8 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 3_1_place_gp_skip_io.odb 2_floorplan.sdc
 
-source $::env(SCRIPTS_DIR)/io_placement_util.tcl
+if {![env_var_equals IS_CHIP 1]} {
+  source $::env(SCRIPTS_DIR)/io_placement_util.tcl
+}
 
 write_db $::env(RESULTS_DIR)/3_2_place_iop.odb
