4e2dbd52918e59f74b7c665c404a2a210d849c0f
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-23 20:04:00 +0200

    makefile: less iffy code
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/io_placement_random.tcl
+++ flow/scripts/io_placement_random.tcl
@@ -1,8 +1,9 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 load_design 2_1_floorplan.odb 1_synth.sdc
 
-lappend ::env(PLACE_PINS_ARGS) -random
-
-source $::env(SCRIPTS_DIR)/io_placement_util.tcl
+if {![env_var_equals IS_CHIP 1]} {
+  lappend ::env(PLACE_PINS_ARGS) -random
+  source $::env(SCRIPTS_DIR)/io_placement_util.tcl
+}
 
 write_db $::env(RESULTS_DIR)/2_2_floorplan_io.odb
