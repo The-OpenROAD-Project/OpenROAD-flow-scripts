e8a0a6ee0fd858a728b8b82248d75d30ad71b71b
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-23 20:22:32 +0200

    global route: restore cp when fill is skipped
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/fillcell.tcl
+++ flow/scripts/fillcell.tcl
@@ -4,14 +4,18 @@ if {[expr [file exists $::env(REPORTS_DIR)/congestion.rpt] && \
     in DRC viewer to view congestion"
 }
 
-source $::env(SCRIPTS_DIR)/load.tcl
-load_design 5_2_route.odb 4_cts.sdc
+source $::env(SCRIPTS_DIR)/util.tcl
 
 if {[env_var_exists_and_non_empty FILL_CELLS]} {
+  source $::env(SCRIPTS_DIR)/load.tcl
+  load_design 5_2_route.odb 4_cts.sdc
+
   set_propagated_clock [all_clocks]
 
   filler_placement $::env(FILL_CELLS)
   check_placement
-}
 
-write_db $::env(RESULTS_DIR)/5_3_fillcell.odb
+  write_db $::env(RESULTS_DIR)/5_3_fillcell.odb
+} else {
+  log_cmd exec cp $::env(RESULTS_DIR)/5_2_route.odb $::env(RESULTS_DIR)/5_3_fillcell.odb
+}
