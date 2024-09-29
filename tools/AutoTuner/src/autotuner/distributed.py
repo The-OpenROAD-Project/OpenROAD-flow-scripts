969c41eed6d9481c16db72c7f2f6ff7aed3144a4
Jack Luar <jluar@precisioninno.com>
2024-09-29 10:24:19 +0000

    add initial scaffold
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- tools/AutoTuner/src/autotuner/distributed.py
+++ tools/AutoTuner/src/autotuner/distributed.py
@@ -538,7 +538,7 @@ def openroad(base_dir, parameters, flow_variant, path=""):
     make_command += f" NPROC={args.openroad_threads} SHELL=bash"
     run_command(
         make_command,
-        timeout=args.timeout,
+        timeout=args.timeout_per_trial,
         stderr_file=f"{log_path}error-make-finish.log",
         stdout_file=f"{log_path}make-finish-stdout.log",
     )
@@ -653,11 +653,18 @@ def parse_arguments():
         " FLOW_VARIANT and to set the Ray log destination.",
     )
     parser.add_argument(
-        "--timeout",
+        "--timeout_per_trial",
         type=float,
         metavar="<float>",
         default=None,
         help="Time limit (in hours) for each trial run. Default is no limit.",
+    )
+    parser.add_argument(
+        "--timeout",
+        type=float,
+        metavar="<float>",
+        default=None,
+        help="Time limit (in hours) for the whole Autotuning process.",
     )
     tune_parser.add_argument(
         "--resume", action="store_true", help="Resume previous run."
@@ -772,12 +779,19 @@ def parse_arguments():
     )
 
     # Workload
+    # parser.add_argument(
+    #     "--jobs",
+    #     type=int,
+    #     metavar="<int>",
+    #     default=int(np.floor(cpu_count() / 2)),
+    #     help="Max number of concurrent jobs.",
+    # )
     parser.add_argument(
-        "--jobs",
+        "--cpu_budget",
         type=int,
         metavar="<int>",
         default=int(np.floor(cpu_count() / 2)),
-        help="Max number of concurrent jobs.",
+        help="Number of cpus to be used per hour.",
     )
     parser.add_argument(
         "--openroad_threads",
@@ -810,23 +824,30 @@ def parse_arguments():
         " training stderr\n\t2: also print training stdout.",
     )
 
-    arguments = parser.parse_args()
-    if arguments.mode == "tune":
-        arguments.algorithm = arguments.algorithm.lower()
+    args = parser.parse_args()
+    if args.mode == "tune":
+        args.algorithm = args.algorithm.lower()
         # Validation of arguments
-        if arguments.eval == "ppa-improv" and arguments.reference is None:
+        if args.eval == "ppa-improv" and args.reference is None:
             print(
                 '[ERROR TUN-0006] The argument "--eval ppa-improv"'
                 ' requires that "--reference <FILE>" is also given.'
             )
             sys.exit(7)
 
-    arguments.experiment += f"-{arguments.mode}-{DATE}"
+    args.experiment += f"-{args.mode}-{DATE}"
 
-    if arguments.timeout is not None:
-        arguments.timeout = round(arguments.timeout * 3600)
+    # Convert time to seconds
+    if args.timeout_per_trial is not None:
+        args.timeout_per_trial = round(args.timeout_per_trial * 3600)
+    if args.timeout is not None:
+        args.timeout = round(args.timeout * 3600)
 
-    return arguments
+    # Calculate jobs
+    jobs = int(args.cpu_budget / args.resources_per_trial)
+    args["jobs"] = jobs
+
+    return args
 
 
 def set_algorithm(experiment_name, config):
@@ -996,6 +1017,7 @@ if __name__ == "__main__":
             name=args.experiment,
             metric=METRIC,
             mode="min",
+            time_budget_s=args.timeout,
             num_samples=args.samples,
             fail_fast=False,
             local_dir=LOCAL_DIR,
