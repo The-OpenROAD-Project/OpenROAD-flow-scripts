7915f2befd1f2542845f84a940f1191b4816dfb2
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-09-29 15:13:25 +0200

    scripts: cleanup
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/util.tcl
+++ flow/scripts/util.tcl
@@ -4,12 +4,12 @@ proc log_cmd {cmd args} {
 }
 
 proc fast_route {} {
-  if {[info exist ::env(FASTROUTE_TCL)]} {
+  if {[env_var_exists_and_non_empty FASTROUTE_TCL]} {
     source $::env(FASTROUTE_TCL)
   } else {
     set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) $::env(ROUTING_LAYER_ADJUSTMENT)
     set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
-    if {[info exist ::env(MACRO_EXTENSION)]} {
+    if {[env_var_exists_and_non_empty MACRO_EXTENSION]} {
       set_macro_extension $::env(MACRO_EXTENSION)
     }
   }
