ec98eb527c386fce374df9e3012d49e0c1852031
Jack Luar <jluar@precisioninno.com>
2024-10-01 17:54:19 +0000

    add smoke test timeout
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- flow/test/test_helper.sh
+++ flow/test/test_helper.sh
@@ -110,6 +110,9 @@ if [ $RUN_AUTOTUNER -eq 1 ]; then
 
   echo "Running Autotuner smoke sweep test"
   python3 -m unittest tools.AutoTuner.test.smoke_test_sweep.${PLATFORM}SweepSmokeTest.test_sweep
+
+  echo "Running AutoTuner smoke timeout test"
+  python3 -m unittest tools.AutoTuner.test.smoke_test_timeout.${PLATFORM}TimeoutSmokeTest.test_timeout
 fi
 
 exit $ret
