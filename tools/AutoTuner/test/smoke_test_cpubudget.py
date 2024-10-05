e5c2e3e6dd231a47bffd555efa92e5d233982d3a
Jack Luar <jluar@precisioninno.com>
2024-10-05 02:08:17 +0000

    add prototype for CPUBudgetSmokeTest
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

---
+++ tools/AutoTuner/test/smoke_test_cpubudget.py
@@ -0,0 +1,87 @@
+import unittest
+import subprocess
+import os
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(cur_dir, "../src/autotuner")
+os.chdir(src_dir)
+
+
+class BaseCPUBudgetSmokeTest(unittest.TestCase):
+    platform = ""
+    design = ""
+
+    def setUp(self):
+        self.config = os.path.join(
+            cur_dir,
+            f"../../../flow/designs/{self.platform}/{self.design}/autotuner.json",
+        )
+        self.experiment = f"smoke-test-timeout-{self.platform}"
+
+        cpu_budget = 1
+        self.expected_timeout = cpu_budget / os.cpu_count()
+
+        self.command = (
+            "python3 distributed.py"
+            f" --design {self.design}"
+            f" --platform {self.platform}"
+            f" --experiment {self.experiment}"
+            f" --config {self.config}"
+            f" --cpu_budget 1"
+            f" --yes"
+            f" tune --samples 1000"
+        )
+
+    def test_cpu_budget(self):
+        raise NotImplementedError(
+            "This method needs to be implemented in the derivative classes."
+        )
+
+
+class ASAP7CPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
+    platform = "asap7"
+    design = "gcd"
+
+    def test_cpu_budget(self):
+        try:
+            out = subprocess.run(
+                self.command, shell=True, check=True, timeout=self.expected_timeout
+            )
+            successful = out.returncode == 0
+        except subprocess.TimeoutExpired:
+            successful = False
+        self.assertFalse(successful)
+
+
+class SKY130HDCPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
+    platform = "sky130hd"
+    design = "gcd"
+
+    def test_cpu_budget(self):
+        try:
+            out = subprocess.run(
+                self.command, shell=True, check=True, timeout=self.expected_timeout
+            )
+            successful = out.returncode == 0
+        except subprocess.TimeoutExpired:
+            successful = False
+        self.assertFalse(successful)
+
+
+class IHPSG13G2CPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
+    platform = "ihp-sg13g2"
+    design = "gcd"
+
+    def test_cpu_budget(self):
+        try:
+            out = subprocess.run(
+                self.command, shell=True, check=True, timeout=self.expected_timeout
+            )
+            successful = out.returncode == 0
+        except subprocess.TimeoutExpired:
+            successful = False
+        self.assertFalse(successful)
+
+
+if __name__ == "__main__":
+    unittest.main()
