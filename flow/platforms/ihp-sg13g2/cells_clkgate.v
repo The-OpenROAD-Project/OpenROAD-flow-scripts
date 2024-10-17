a0a34eee43aea435e7375cbd9684ed4dad826d4d
Matt Liberty <mliberty@precisioninno.com>
2024-10-17 20:33:23 +0000

    Remove the conditional in OPENROAD_CLKGATE PDK definitions
    The condition is never set and the clock gate never instatiated.  This
    is likely left over from when we didn't support them in CTS.
    Significant QOR changes are expected (less flop merging, actual gates,
    etc).
    
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- flow/platforms/ihp-sg13g2/cells_clkgate.v
+++ flow/platforms/ihp-sg13g2/cells_clkgate.v
@@ -3,14 +3,6 @@ module OPENROAD_CLKGATE (CK, E, GCK);
   input E;
   output GCK;
 
-`ifdef OPENROAD_CLKGATE
-
 sg13g2_lgcp_1 latch (.CLK (CK), .GATE(E), .GCLK(GCK));
-
-`else
-
-assign GCK = CK;
-
-`endif
 
 endmodule
--- flow/platforms/nangate45/cells_clkgate.v
+++ flow/platforms/nangate45/cells_clkgate.v
@@ -3,14 +3,6 @@ module OPENROAD_CLKGATE (CK, E, GCK);
   input E;
   output GCK;
 
-`ifdef OPENROAD_CLKGATE
-
 CLKGATE_X1 latch (.CK (CK), .E(E), .GCK(GCK));
-
-`else
-
-assign GCK = CK;
-
-`endif
 
 endmodule
