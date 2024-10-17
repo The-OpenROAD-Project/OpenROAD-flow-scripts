34602ee9535a58af32df490476f2d0910975f331
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-10-17 07:10:15 +0200

    detail route: more direct check on having global routes
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/detail_route.tcl
+++ flow/scripts/detail_route.tcl
@@ -1,12 +1,10 @@
 utl::set_metrics_stage "detailedroute__{}"
 source $::env(SCRIPTS_DIR)/load.tcl
-if {[file exists $::global_route_congestion_report]} {
-  if {[file size $::global_route_congestion_report] != 0} {
-    error "Global routing failed, run `make gui_grt` and load $::global_route_congestion_report \
-          in DRC viewer to view congestion"
-  }
-}
 load_design 5_1_grt.odb 4_cts.sdc
+if {![grt::have_routes]} {
+  error "Global routing failed, run `make gui_grt` and load $::global_route_congestion_report \
+        in DRC viewer to view congestion"
+}
 erase_non_stage_variables route
 set_propagated_clock [all_clocks]
 
