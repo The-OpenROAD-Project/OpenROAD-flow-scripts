c22aebfd6aa57bb89339e50d9b73c7ede5109feb
Eryk Szpotanski <eszpotanski@antmicro.com>
2024-10-21 15:27:56 +0200

    tools: Autotuner: Add dependencies for Vizier
    Signed-off-by: Eryk Szpotanski <eszpotanski@antmicro.com>

--- tools/AutoTuner/installer.sh
+++ tools/AutoTuner/installer.sh
@@ -3,6 +3,17 @@
 # Get the directory where the script is located
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 
+dependencies=""
+if [[ "$#" -eq 0 ]]; then
+  echo "Installing dependencies for Ray Tune and Vizier"
+  dependencies="ray,vizier"
+elif [[ "$#" -ne 1 ]] || ([[ "$1" != "ray" ]] && [[ "$1" != "vizier" ]]); then
+  echo "Please specify whether 'ray' or 'vizier' dependencies should be installed" >&2
+  exit 1
+else
+  dependencies="$1"
+fi
+
 # Define the virtual environment name
 venv_name="autotuner_env"
 python3 -m venv "$script_dir/$venv_name"
@@ -29,5 +40,5 @@ if [ "$success" = false ]; then
     exit 1
 fi
 
-pip3 install --no-cache-dir -e "$script_dir"
+pip3 install --no-cache-dir -e "$script_dir[$dependencies]"
 deactivate
