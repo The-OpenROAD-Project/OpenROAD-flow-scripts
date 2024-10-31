e5b3246a483ffd7607b2b10048ecdce0f7e9a74c
Jack Luar <jluar@precisioninno.com>
2024-10-31 18:33:57 +0000

    fix pip cache, pip-compile lock reqs
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- tools/AutoTuner/installer.sh
+++ tools/AutoTuner/installer.sh
@@ -7,5 +7,6 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 venv_name="autotuner_env"
 python3 -m venv "$script_dir/$venv_name"
 source "$script_dir/$venv_name/bin/activate"
-pip3 install -U -r $script_dir/requirements.txt
+pip3 cache purge
+pip3 install --no-cache-dir -U -r $script_dir/requirements.txt
 deactivate
