c6ace0766cfe27c772ea7cf8d5a892cdf7343eab
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-01-17 12:28:06 +0100

    open: more log_cmd added for long running commands
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/open.tcl
+++ flow/scripts/open.tcl
@@ -5,18 +5,18 @@ source $::env(SCRIPTS_DIR)/read_liberty.tcl
 # Read def
 if {[env_var_exists_and_non_empty DEF_FILE]} {
     # Read lef
-    read_lef $::env(TECH_LEF)
-    read_lef $::env(SC_LEF)
+    log_cmd read_lef $::env(TECH_LEF)
+    log_cmd read_lef $::env(SC_LEF)
     if {[env_var_exists_and_non_empty ADDITIONAL_LEFS]} {
       foreach lef $::env(ADDITIONAL_LEFS) {
-        read_lef $lef
+        log_cmd read_lef $lef
       }
     }
     set input_file $::env(DEF_FILE)
-    read_def $input_file
+    log_cmd read_def $input_file
 } else {
     set input_file $::env(ODB_FILE)
-    read_db $input_file
+    log_cmd read_db $input_file
 }
 
 proc read_timing {input_file} {
@@ -27,7 +27,7 @@ proc read_timing {input_file} {
   if {$sdc_file == ""} {
     set sdc_file $::env(SDC_FILE)
   }
-  read_sdc $sdc_file
+  log_cmd read_sdc $sdc_file
   if [file exists $::env(PLATFORM_DIR)/derate.tcl] {
     source $::env(PLATFORM_DIR)/derate.tcl
   }
