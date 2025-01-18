bab21abf56754d4e08823ea1246afd056f5bd248
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-01-18 15:51:19 +0100

    synthesis: add SYNTH_STRIP to improve canonicalization
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/synth_canonicalize.tcl
+++ flow/scripts/synth_canonicalize.tcl
@@ -2,5 +2,11 @@ source $::env(SCRIPTS_DIR)/synth_preamble.tcl
 hierarchy -check -top $::env(DESIGN_NAME)
 # Get rid of unused modules
 opt_clean -purge
+
+if {$::env(SYNTH_STRIP)} {
+  puts "Stripping source code information"
+  attrmap -remove \src
+}
+
 # The hash of this file will not change if files not part of synthesis do not change
 write_rtlil $::env(RESULTS_DIR)/1_synth.rtlil
