96fb5ef74bbfeaab22883d8e1c1396a809b60ea9
Augusto Berndt <augusto.berndt@precisioninno.com>
2025-01-21 14:10:10 +0000

    gpl: use similar format as global_place.tcl
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- flow/scripts/global_place_skip_io.tcl
+++ flow/scripts/global_place_skip_io.tcl
@@ -5,14 +5,20 @@ load_design 2_floorplan.odb 2_floorplan.sdc
 if { [env_var_exists_and_non_empty FLOORPLAN_DEF] } {
   puts "FLOORPLAN_DEF is set. Skipping global placement without IOs"
 } else {
+
+  set global_placement_args {}
+
   if {$::env(GPL_ALLOW_REVERT_IF_DIVERGE)} {
     lappend global_placement_args {-allow_revert_if_diverge}
   }
   
-  log_cmd global_placement -skip_io -density [place_density_with_lb_addon] \
+  set all_args [concat [list -skip_io -density [place_density_with_lb_addon] \
       -pad_left $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
-      -pad_right $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
-      {*}$::env(GLOBAL_PLACEMENT_ARGS)
+      -pad_right $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT)] \
+      $global_placement_args]
+
+  lappend all_args {*}$::env(GLOBAL_PLACEMENT_ARGS)
+  log_cmd global_placement {*}$all_args
 }
 
 write_db $::env(RESULTS_DIR)/3_1_place_gp_skip_io.odb
