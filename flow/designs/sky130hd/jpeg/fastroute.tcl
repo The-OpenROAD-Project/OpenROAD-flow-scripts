da5b88ab3881c5a8193986b2d6b0672db7dd7834
Jeff Ng <jeffng@precisioninno.com>
2025-01-28 09:37:15 -0800

    configs for checkconnectivity error
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/designs/sky130hd/jpeg/fastroute.tcl
+++ flow/designs/sky130hd/jpeg/fastroute.tcl
@@ -3,3 +3,5 @@ set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING
 set_routing_layers -clock $::env(MIN_CLK_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
 set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
 
+
+set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.22378684689076636
