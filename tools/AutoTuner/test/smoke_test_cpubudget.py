a06b8aed7229154c477eaf3dc12d2a73c46e7ff2
Jack Luar <jluar@precisioninno.com>
2025-02-08 19:31:23 +0000

    standardise cpubudget test
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- tools/AutoTuner/test/smoke_test_cpubudget.py
+++ tools/AutoTuner/test/smoke_test_cpubudget.py
@@ -3,8 +3,6 @@ import subprocess
 import os
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.join(cur_dir, "../src/autotuner")
-os.chdir(src_dir)
 
 
 class BaseCPUBudgetSmokeTest(unittest.TestCase):
@@ -19,76 +17,49 @@ class BaseCPUBudgetSmokeTest(unittest.TestCase):
         self.experiment = f"smoke-test-cpubudget-{self.platform}"
 
         # Tests should fail with such a low budget.
-        cpu_budget = 1
-        self.expected_timeout = cpu_budget / os.cpu_count() * 3600
+        self.cpu_budget = 1
+        self.expected_timeout = self.cpu_budget / os.cpu_count() * 3600
 
         self.command = (
-            "python3 distributed.py"
+            "python3 -m autotuner.distributed"
             f" --design {self.design}"
             f" --platform {self.platform}"
             f" --experiment {self.experiment}"
             f" --config {self.config}"
-            f" --cpu_budget 1"
+            f" --cpu_budget {self.cpu_budget}"
             f" --yes"
             f" tune --samples 1"
         )
         self.command = self.command.split()
 
     def test_cpu_budget(self):
-        raise NotImplementedError(
-            "This method needs to be implemented in the derivative classes."
-        )
+        if not (self.platform and self.design):
+            raise unittest.SkipTest("Platform and design have to be defined")
+        try:
+            _ = subprocess.run(
+                self.command,
+                stderr=subprocess.PIPE,
+                timeout=self.expected_timeout,
+            )
+            failed = False
+        except subprocess.TimeoutExpired:
+            failed = True
+        self.assertTrue(failed)
 
 
 class asap7CPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
     platform = "asap7"
     design = "gcd"
 
-    def test_cpu_budget(self):
-        try:
-            _ = subprocess.run(
-                self.command,
-                stderr=subprocess.PIPE,
-                timeout=self.expected_timeout,
-            )
-            failed = False
-        except subprocess.TimeoutExpired:
-            failed = True
-        self.assertTrue(failed)
-
 
 class sky130hdCPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
     platform = "sky130hd"
     design = "gcd"
 
-    def test_cpu_budget(self):
-        try:
-            _ = subprocess.run(
-                self.command,
-                stderr=subprocess.PIPE,
-                timeout=self.expected_timeout,
-            )
-            failed = False
-        except subprocess.TimeoutExpired:
-            failed = True
-        self.assertTrue(failed)
-
 
 class ihpsg13g2CPUBudgetSmokeTest(BaseCPUBudgetSmokeTest):
     platform = "ihp-sg13g2"
     design = "gcd"
-
-    def test_cpu_budget(self):
-        try:
-            _ = subprocess.run(
-                self.command,
-                stderr=subprocess.PIPE,
-                timeout=self.expected_timeout,
-            )
-            failed = False
-        except subprocess.TimeoutExpired:
-            failed = True
-        self.assertTrue(failed)
 
 
 if __name__ == "__main__":
