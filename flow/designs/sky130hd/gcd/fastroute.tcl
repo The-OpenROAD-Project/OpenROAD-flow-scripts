a008798a91a026cad8cd4489c7b54344735914c3
Jeff Ng <jeffng@precisioninno.com>
2025-02-13 19:22:29 +0000

    files to recreate GRT/DPL failure on sky130hd gcd
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

---
+++ flow/designs/sky130hd/gcd/fastroute.tcl
@@ -0,0 +1,7 @@
+set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.2
+
+set_routing_layers -clock $::env(MIN_CLK_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
+set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
+
+
+set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.26279020624529953
