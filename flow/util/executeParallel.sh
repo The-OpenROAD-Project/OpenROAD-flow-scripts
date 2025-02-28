2152d38abaaedbdb03eff76fe559194dfff4c816
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-02-27 23:42:34 -0300

    ci: remove stale and no longer used files
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- flow/util/executeParallel.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/home/tool/gnu/parallel/parallel-20160822/bin/parallel --joblog out.log --bar --env GRB_LICENSE_FILE --env LD_LIBRARY_PATH --env PATH --env LM_LICENSE_FILE --sshloginfile node --workdir $PWD < run.sh
