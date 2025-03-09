cc796b0dbcc51d472523fbc30487e0528352d967
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-09 07:39:52 +0100

    synthesis: add a do-yosys-coarse stage
    This is the minimum time to run synthesis before
    reports can be made about which modules will be kept,
    sram sizes to be realized as flip flops.
    
    After this stage, it is possible to run the rest of
    synthesis in parallel for kept modules and combine the
    netlists.
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/synth.tcl
+++ flow/scripts/synth.tcl
@@ -1,39 +1,14 @@
 source $::env(SCRIPTS_DIR)/synth_preamble.tcl
 
-hierarchy -check -top $::env(DESIGN_NAME)
-
-if { [env_var_equals SYNTH_GUT 1] } {
-  # /deletes all cells at the top level, which will quickly optimize away
-  # everything else, including macros.
-  delete $::env(DESIGN_NAME)/c:*
-}
-
-if {![env_var_equals SYNTH_HIERARCHICAL 1]} {
-  # Perform standard coarse-level synthesis script, flatten right away
-  # (-flatten part of $synth_args per default)
-  synth -run :fine {*}$::env(SYNTH_FULL_ARGS)
-} else {
-  # Perform standard coarse-level synthesis script,
-  # defer flattening until we have decided what hierarchy to keep
-  synth -run :fine
-
-  if {[env_var_exists_and_non_empty MAX_UNGROUP_SIZE]} {
-    set ungroup_threshold $::env(MAX_UNGROUP_SIZE)
-    puts "Ungroup modules below estimated size of $ungroup_threshold instances"
-
-    convert_liberty_areas
-    keep_hierarchy -min_cost $ungroup_threshold
-  } else {
-    keep_hierarchy
-  }
-
+if {[env_var_equals SYNTH_HIERARCHICAL 1]} {
   # Re-run coarse-level script, this time do pass -flatten
+  #
+  # This is done here instead of in synth_coarse.tcl, because this part of
+  # the flow can be done in parallel for all the kept modules as
+  # well as being faster here, when taking SYNTH_BLACKBOXES into
+  # account for parallel builds.
   synth -run coarse:fine {*}$::env(SYNTH_FULL_ARGS)
 }
-
-json -o $::env(RESULTS_DIR)/mem.json
-# Run report and check here so as to fail early if this synthesis run is doomed
-exec -- python3 $::env(SCRIPTS_DIR)/mem_dump.py --max-bits $::env(SYNTH_MEMORY_MAX_BITS) $::env(RESULTS_DIR)/mem.json
 
 if {![env_var_exists_and_non_empty SYNTH_WRAPPED_OPERATORS]} {
   synth -top $::env(DESIGN_NAME) -run fine: {*}$::env(SYNTH_FULL_ARGS)
