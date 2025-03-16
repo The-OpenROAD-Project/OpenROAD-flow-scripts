7e091e797fab8421a54f5af0fafc243b285dd190
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-16 22:45:50 +0100

    synth: less iffy VERILOG_TOP_PARAMS code
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/scripts/synth_canonicalize.tcl
+++ flow/scripts/synth_canonicalize.tcl
@@ -1,10 +1,8 @@
 source $::env(SCRIPTS_DIR)/synth_preamble.tcl
 
-# Apply toplevel parameters (if exist)
-if {[env_var_exists_and_non_empty VERILOG_TOP_PARAMS]} {
-  dict for {key value} $::env(VERILOG_TOP_PARAMS) {
-    chparam -set $key $value $::env(DESIGN_NAME)
-  }
+dict for {key value} $::env(VERILOG_TOP_PARAMS) {
+  # Apply toplevel parameters
+  chparam -set $key $value $::env(DESIGN_NAME)
 }
 
 hierarchy -check -top $::env(DESIGN_NAME)
