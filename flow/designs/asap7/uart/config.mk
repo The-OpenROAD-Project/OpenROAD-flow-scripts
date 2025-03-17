7cca78583be719d93b5a3c6dcbac7e947eb13653
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-17 11:05:24 +0100

    synth: VERILOG_TOP_PARAMS smoke test
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/asap7/uart/config.mk
+++ flow/designs/asap7/uart/config.mk
@@ -13,3 +13,4 @@ export TNS_END_PERCENT        = 100
 export EQUIVALENCE_CHECK     ?=   1
 export REMOVE_CELLS_FOR_EQY   = TAPCELL*
 export SKIP_GATE_CLONING      = 1
+export VERILOG_TOP_PARAMS = DATA_WIDTH 8
