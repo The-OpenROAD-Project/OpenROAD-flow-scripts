aa2a3301bffa7ba51bc17c7ad22e397b3335bdf7
Jeff Ng <jeffng@precisioninno.com>
2025-03-24 20:43:50 +0000

    Re-factored asap7 platform config.mk - more consistent handling of Vt's changed ifdef to ifeq GDS_FILES needs = not ?= due to setting in ORFS Makefile updated ethmac_lvt config.mk Disabled ResumeCheck per Jack
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/test/test_autotuner.sh
+++ flow/test/test_autotuner.sh
@@ -30,7 +30,8 @@ if [ "$PLATFORM_WITHOUT_DASHES" == "asap7" ] && [ "$DESIGN_NAME" == "gcd" ]; the
   python3 -m unittest tools.AutoTuner.test.ref_file_check.RefFileCheck.test_files
 
   echo "Running AutoTuner resume test (only once)"
-  python3 -m unittest tools.AutoTuner.test.resume_check.ResumeCheck.test_tune_resume
+  # Temporarily disable resume check test due to flakiness
+  #python3 -m unittest tools.AutoTuner.test.resume_check.ResumeCheck.test_tune_resume
 
   echo "Running AutoTuner binary check (only once)"
   openroad_autotuner -h
