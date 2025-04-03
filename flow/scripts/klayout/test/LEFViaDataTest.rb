3cfd6fff1d8a64ec16b63983ab935b7517d05b79
Jeff Ng <jeffng@precisioninno.com>
2025-04-03 15:28:24 +0000

    KLayout mods based on new info
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/scripts/klayout/test/LEFViaDataTest.rb
+++ flow/scripts/klayout/test/LEFViaDataTest.rb
@@ -38,16 +38,20 @@ VIA VIA89 Default
  RECT -0.02 -0.02 0.02 0.02 ;
 END VIA89
 VIA_TEXT
-        
+
+        exp_layers = {
+            "VIA9Pad" => [ "M9", "Pad", "V9" ],
+            "VIA89" => [ "M8", "V8", "M9" ],
+        }
+            
         data_stream = StringIO.new(data_str)
         @rep.read(data_stream)
         via_map = @rep.get_map()
         assert(via_map)
         assert(via_map.length == 2,
                sprintf("via map length is not 2: %d", via_map.length))
-        via_map.each do | key, layer_list |
-            tokens = key.split("_")
-            assert(tokens.sort == layer_list.sort)
+        via_map.each do | via_name, layer_list |
+            assert(exp_layers[via_name].sort == layer_list.sort)
         end
     end
 end
