796d586fe34838ba68826971df2cea6d576d58c0
Martin Povišer <povik@cutebit.org>
2025-04-15 11:04:30 +0200

    Use glob pattern in Ibex configs
    Signed-off-by: Martin Povišer <povik@cutebit.org>

--- flow/designs/asap7/ibex/config.mk
+++ flow/designs/asap7/ibex/config.mk
@@ -3,26 +3,7 @@ export PLATFORM               = asap7
 export DESIGN_NICKNAME        = ibex
 export DESIGN_NAME            = ibex_core
 
-export VERILOG_FILES = $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_pkg.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_alu.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_compressed_decoder.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_controller.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_cs_registers.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_counter.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_decoder.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_ex_block.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_id_stage.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_if_stage.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_wb_stage.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_load_store_unit.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_multdiv_slow.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_multdiv_fast.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_prefetch_buffer.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_fetch_fifo.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_pmp.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_csr.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_core.sv \
-    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/ibex_register_file_ff.sv \
+export VERILOG_FILES = $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/*.sv)) \
     $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/syn/rtl/prim_clock_gating.v
 
 export VERILOG_INCLUDE_DIRS = \
