832bfa27b130e0329f0997d52de427a30603eba2
Jeff Ng <jeffng@precisioninno.com>
2025-04-29 15:52:28 +0000

    test case for reproducing dpl seg fault
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

---
+++ flow/designs/sky130hd/gcd/fastroute.tcl
@@ -0,0 +1,4 @@
+#set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.2
+set_global_routing_layer_adjustment $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER) 0.28857429823519476
+set_routing_layers -clock $::env(MIN_CLK_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
+set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
