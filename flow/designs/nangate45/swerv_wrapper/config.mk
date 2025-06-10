2ecea0dba1afefffb122f6814fb7d5493c88bcf1
Martin Povišer <povik@cutebit.org>
2025-06-10 16:35:20 +0200

    Release virtual TD overrides
    Signed-off-by: Martin Povišer <povik@cutebit.org>

--- flow/designs/nangate45/swerv_wrapper/config.mk
+++ flow/designs/nangate45/swerv_wrapper/config.mk
@@ -23,5 +23,3 @@ export PLACE_DENSITY_LB_ADDON = 0.08
 export TNS_END_PERCENT        = 100
 
 export FASTROUTE_TCL = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/fastroute.tcl
-
-export GPL_KEEP_OVERFLOW = 0
