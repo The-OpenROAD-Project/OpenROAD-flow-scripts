cb73ed67fde9fec59874c6c03536439ba563e995
Jeff Ng <jeffng@precisioninno.com>
2025-06-13 13:08:32 -0700

    updated asap7 cva6 memory sizes to remove tied off connections updated metrics
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

---
+++ flow/platforms/asap7/verilog/fakeram7_64x25.sv
@@ -0,0 +1,10 @@
+(* blackbox *)
+module fakeram7_64x25 (
+   output reg [24:0] rd_out,
+   input [5:0] addr_in,
+   input we_in,
+   input [24:0] wd_in,
+   input clk,
+   input ce_in
+);
+endmodule
