04cf9bc96661764377220cb58aae240563bbad6c
Jack Luar <jluar@precisioninno.com>
2025-07-02 16:03:59 +0000

    lint flow/designs
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- flow/designs/gf12/ariane/io.tcl
+++ flow/designs/gf12/ariane/io.tcl
@@ -1,1 +1,2 @@
-exclude_io_pin_region -region left:0-150 -region left:450-600 -region right:* -region top:* -region bottom:*
+exclude_io_pin_region -region left:0-150 -region left:450-600 -region right:* \
+  -region top:* -region bottom:*
--- flow/designs/ihp-sg13g2/i2c-gpio-expander/I2cDeviceCtrl/pdn.tcl
+++ flow/designs/ihp-sg13g2/i2c-gpio-expander/I2cDeviceCtrl/pdn.tcl
@@ -26,9 +26,13 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 
 # stdcell grid
 define_pdn_grid -name {grid} -voltage_domains {CORE}
-add_pdn_stripe -grid {grid} -layer {Metal1} -width {0.44} -pitch {7.56} -offset {0} -followpins -extend_to_core_ring
-add_pdn_ring -grid {grid} -layers {Metal3 Metal4} -widths {3.0} -spacings {2.0} -core_offsets {4.5} -connect_to_pads
-add_pdn_stripe -grid {grid} -layer {Metal3} -width {1.840} -pitch {75.6} -offset {37.8} -extend_to_core_ring
-add_pdn_stripe -grid {grid} -layer {Metal4} -width {1.840} -pitch {75.6} -offset {37.8} -extend_to_core_ring
+add_pdn_stripe -grid {grid} -layer {Metal1} -width {0.44} -pitch {7.56} -offset {0} \
+  -followpins -extend_to_core_ring
+add_pdn_ring -grid {grid} -layers {Metal3 Metal4} -widths {3.0} -spacings {2.0} \
+  -core_offsets {4.5} -connect_to_pads
+add_pdn_stripe -grid {grid} -layer {Metal3} -width {1.840} -pitch {75.6} -offset {37.8} \
+  -extend_to_core_ring
+add_pdn_stripe -grid {grid} -layer {Metal4} -width {1.840} -pitch {75.6} -offset {37.8} \
+  -extend_to_core_ring
 add_pdn_connect -grid {grid} -layers {Metal1 Metal3}
 add_pdn_connect -grid {grid} -layers {Metal3 Metal4}
