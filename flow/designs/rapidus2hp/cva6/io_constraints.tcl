259efe8d06b839e83b813aa1422894e121fcbc22
Jeff Ng <jeffng@precisioninno.com>
2025-07-16 18:13:36 +0000

    Increased rapidus cva6 util to 45 for Verific tcllint fixes removed comments
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/designs/rapidus2hp/cva6/io_constraints.tcl
+++ flow/designs/rapidus2hp/cva6/io_constraints.tcl
@@ -1,34 +1,21 @@
 # left (bottom to top)
-# cvxif_req_o
-# axi_req_o - noc_req_o in ours
-# scan_output - doesn't exist in ours
-set_io_pin_constraint -group -order -region left:20-38.7 -pin_names {cvxif_req_o*}
-set_io_pin_constraint -group -order -region left:38.8-90.2 -pin_names {noc_req_o*}
+set_io_pin_constraint -group -order -region left:4.09-40.70 -pin_names {cvxif_req_o[*]}
+set_io_pin_constraint -group -order -region left:40.85-90.13 -pin_names {noc_req_o[*]}
 
 # right (bottom to top)
-# scan_input - doesn't exist in ours
-# test_mode - doesn't exist in ours
-# scan_enable - doesn't exist in ours
-# axi_resp_i - noc_resp_i in ours
-# cvxif_resp_i
-# debug_req_i
-# time_irq_i
-# ipi_i
-# irq_i
-# hart_id_i
-# boot_addr_i
-# rst_ni
-# clk_i
-set_io_pin_constraint -group -order -region right:5-31.4 -pin_names {noc_resp_i*}
-set_io_pin_constraint -group -order -region right:31.5-73.1 -pin_names {cvxif_resp_i*}
-set_io_pin_constraint -group -order -region right:73.3-73.7 -pin_names {debug_req_i \
-								        time_irq_i ipi_i}
-set_io_pin_constraint -group -order -region right:74-74.3 -pin_names {irq_i*}
-set_io_pin_constraint -group -order -region right:74.5-82 -pin_names {hart_id_i*}
-set_io_pin_constraint -group -order -region right:82.2-89.8 -pin_names {boot_addr_i*}
-set_io_pin_constraint -group -order -region right:89.9-90.3 -pin_names {rst_n_i clk_i}
+# The intervals have been expanded based on pin placer feedback
+set_io_pin_constraint -group -order -region right:5.25-45.34 -pin_names {noc_resp_i[*]}
+set_io_pin_constraint -group -order -region right:45.62-93.07 -pin_names {cvxif_resp_i[*]}
+set_io_pin_constraint -group -order -region right:93.32-93.73 \
+  -pin_names {
+    debug_req_i time_irq_i ipi_i
+  }
+set_io_pin_constraint -group -order -region right:94.01-94.28 -pin_names {irq_i[*]}
+set_io_pin_constraint -group -order -region right:94.51-102.01 -pin_names {hart_id_i[*]}
+set_io_pin_constraint -group -order -region right:102.25-109.74 -pin_names {boot_addr_i[*]}
+set_io_pin_constraint -group -order -region right:109.99-110.25 -pin_names {rst_ni clk_i}
 
-# don't exist in reference design implementation - does it make us I/O bound?
+# The rvfi_probes_o pins don't exist in reference design implementation
 # put a third of them on the top, a third on the bottom, and let the placer
 # decide where to put the remaining third
 set num_rvfi_probes_ports 4295
@@ -41,6 +28,7 @@ set bottom_group {}
 for { } { $i < $third_rvfi_probes_ports * 2 } { incr i } {
   lappend bottom_group "rvfi_probes_o\[$i\]"
 }
+
 
 set_io_pin_constraint -group -order -region bottom:* -pin_names $top_group
 set_io_pin_constraint -group -order -region top:* -pin_names $bottom_group
