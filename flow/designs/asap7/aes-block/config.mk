69369582b97dc4777dfc825e958deb7668b2a0ed
João Mai <jmai@precisioninno.com>
2025-07-31 13:44:28 +0000

    update asap7/aes-block config
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- flow/designs/asap7/aes-block/config.mk
+++ flow/designs/asap7/aes-block/config.mk
@@ -11,7 +11,7 @@ export ABC_AREA               = 1
 export CORE_UTILIZATION       = 40
 export CORE_ASPECT_RATIO      = 1
 export CORE_MARGIN            = 2
-export PLACE_DENSITY          = 0.53
+export PLACE_DENSITY          = 0.52
 
 export BLOCKS ?= aes_rcon aes_sbox
 export SYNTH_HIERARCHICAL = 1
