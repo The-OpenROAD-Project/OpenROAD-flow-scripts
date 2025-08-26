20c7b8469842f13fcc0258ba91920d81278a77ec
Cho Moon <cmoon@precisioninno.com>
2025-08-26 21:38:02 +0000

    tclint fix
    Signed-off-by: Cho Moon <cmoon@precisioninno.com>

--- flow/scripts/floorplan.tcl
+++ flow/scripts/floorplan.tcl
@@ -3,30 +3,30 @@ source $::env(SCRIPTS_DIR)/load.tcl
 erase_non_stage_variables floorplan
 load_design 1_synth.v 1_synth.sdc
 
-proc report_unused_masters {} {
-    set db [ord::get_db]
-    set libs [$db getLibs]
-    set masters ""
-    foreach lib $libs {
-        foreach master [$lib getMasters] {
-            # filter out non-block masters, or you can remove this conditional to detect any unused master
-            if {[$master getType] == "BLOCK"} {
-                lappend masters $master
-            }
-        }
+proc report_unused_masters { } {
+  set db [ord::get_db]
+  set libs [$db getLibs]
+  set masters ""
+  foreach lib $libs {
+    foreach master [$lib getMasters] {
+      # filter out non-block masters, or you can remove this conditional to detect any unused master
+      if { [$master getType] == "BLOCK" } {
+        lappend masters $master
+      }
     }
+  }
 
-    set block [ord::get_db_block]
-    set insts [$block getInsts]
+  set block [ord::get_db_block]
+  set insts [$block getInsts]
 
-    foreach inst $insts {
-        set inst_master [$inst getMaster]
-        set masters [lsearch -all -not -inline $masters $inst_master]
-    }
+  foreach inst $insts {
+    set inst_master [$inst getMaster]
+    set masters [lsearch -all -not -inline $masters $inst_master]
+  }
 
-    foreach master $masters {
-        puts "Master [$master getName] is loaded but not used in the design"
-    }
+  foreach master $masters {
+    puts "Master [$master getName] is loaded but not used in the design"
+  }
 }
 
 report_unused_masters
@@ -46,86 +46,86 @@ append_env_var additional_args ADDITIONAL_SITES -additional_sites 1
 set use_floorplan_def [env_var_exists_and_non_empty FLOORPLAN_DEF]
 set use_footprint [env_var_exists_and_non_empty FOOTPRINT]
 set use_die_and_core_area \
-    [expr {[env_var_exists_and_non_empty DIE_AREA] && [env_var_exists_and_non_empty CORE_AREA]}]
+  [expr { [env_var_exists_and_non_empty DIE_AREA] && [env_var_exists_and_non_empty CORE_AREA] }]
 set use_core_utilization [env_var_exists_and_non_empty CORE_UTILIZATION]
 
 set methods_defined \
-    [expr {$use_floorplan_def + $use_footprint + $use_die_and_core_area + $use_core_utilization}]
-if {$methods_defined > 1} {
-    puts "Error: Floorplan initialization methods are mutually exclusive, pick one."
-    exit 1
+  [expr { $use_floorplan_def + $use_footprint + $use_die_and_core_area + $use_core_utilization }]
+if { $methods_defined > 1 } {
+  puts "Error: Floorplan initialization methods are mutually exclusive, pick one."
+  exit 1
 }
 
-if {$use_floorplan_def} {
-    # Initialize floorplan by reading in floorplan DEF
-    log_cmd read_def -floorplan_initialize $env(FLOORPLAN_DEF)
-} elseif {$use_footprint} {
-    # Initialize floorplan using ICeWall FOOTPRINT
-    ICeWall load_footprint $env(FOOTPRINT)
+if { $use_floorplan_def } {
+  # Initialize floorplan by reading in floorplan DEF
+  log_cmd read_def -floorplan_initialize $env(FLOORPLAN_DEF)
+} elseif { $use_footprint } {
+  # Initialize floorplan using ICeWall FOOTPRINT
+  ICeWall load_footprint $env(FOOTPRINT)
 
-    initialize_floorplan \
-        -die_area [ICeWall get_die_area] \
-        -core_area [ICeWall get_core_area] \
-        -site $::env(PLACE_SITE)
+  initialize_floorplan \
+    -die_area [ICeWall get_die_area] \
+    -core_area [ICeWall get_core_area] \
+    -site $::env(PLACE_SITE)
 
-    ICeWall init_footprint $env(SIG_MAP_FILE)
-} elseif {$use_die_and_core_area} {
-    initialize_floorplan -die_area $::env(DIE_AREA) \
-        -core_area $::env(CORE_AREA) \
-        -site $::env(PLACE_SITE) \
-        {*}$additional_args
-} elseif {$use_core_utilization} {
-    initialize_floorplan -utilization $::env(CORE_UTILIZATION) \
-        -aspect_ratio $::env(CORE_ASPECT_RATIO) \
-        -core_space $::env(CORE_MARGIN) \
-        -site $::env(PLACE_SITE) \
-        {*}$additional_args
+  ICeWall init_footprint $env(SIG_MAP_FILE)
+} elseif { $use_die_and_core_area } {
+  initialize_floorplan -die_area $::env(DIE_AREA) \
+    -core_area $::env(CORE_AREA) \
+    -site $::env(PLACE_SITE) \
+    {*}$additional_args
+} elseif { $use_core_utilization } {
+  initialize_floorplan -utilization $::env(CORE_UTILIZATION) \
+    -aspect_ratio $::env(CORE_ASPECT_RATIO) \
+    -core_space $::env(CORE_MARGIN) \
+    -site $::env(PLACE_SITE) \
+    {*}$additional_args
 } else {
-    puts "Error: No floorplan initialization method specified"
-    exit 1
+  puts "Error: No floorplan initialization method specified"
+  exit 1
 }
 
-if {[env_var_exists_and_non_empty MAKE_TRACKS]} {
-    log_cmd source $::env(MAKE_TRACKS)
-} elseif {[file exists $::env(PLATFORM_DIR)/make_tracks.tcl]} {
-    log_cmd source $::env(PLATFORM_DIR)/make_tracks.tcl
+if { [env_var_exists_and_non_empty MAKE_TRACKS] } {
+  log_cmd source $::env(MAKE_TRACKS)
+} elseif { [file exists $::env(PLATFORM_DIR)/make_tracks.tcl] } {
+  log_cmd source $::env(PLATFORM_DIR)/make_tracks.tcl
 } else {
-    make_tracks
+  make_tracks
 }
 
 source_env_var_if_exists FOOTPRINT_TCL
 
-if {![env_var_equal SKIP_REPAIR_TIE_FANOUT 1]} {
-    # This needs to come before any call to remove_buffers.  You could have one
-    # tie driving multiple buffers that drive multiple outputs.
-    # Repair tie lo fanout
-    puts "Repair tie lo fanout..."
-    set tielo_cell_name [lindex $::env(TIELO_CELL_AND_PORT) 0]
-    set tielo_lib_name [get_name [get_property [lindex [get_lib_cell $tielo_cell_name] 0] library]]
-    set tielo_pin $tielo_lib_name/$tielo_cell_name/[lindex $::env(TIELO_CELL_AND_PORT) 1]
-    repair_tie_fanout -separation $::env(TIE_SEPARATION) $tielo_pin
+if { ![env_var_equal SKIP_REPAIR_TIE_FANOUT 1] } {
+  # This needs to come before any call to remove_buffers.  You could have one
+  # tie driving multiple buffers that drive multiple outputs.
+  # Repair tie lo fanout
+  puts "Repair tie lo fanout..."
+  set tielo_cell_name [lindex $::env(TIELO_CELL_AND_PORT) 0]
+  set tielo_lib_name [get_name [get_property [lindex [get_lib_cell $tielo_cell_name] 0] library]]
+  set tielo_pin $tielo_lib_name/$tielo_cell_name/[lindex $::env(TIELO_CELL_AND_PORT) 1]
+  repair_tie_fanout -separation $::env(TIE_SEPARATION) $tielo_pin
 
-    # Repair tie hi fanout
-    puts "Repair tie hi fanout..."
-    set tiehi_cell_name [lindex $::env(TIEHI_CELL_AND_PORT) 0]
-    set tiehi_lib_name [get_name [get_property [lindex [get_lib_cell $tiehi_cell_name] 0] library]]
-    set tiehi_pin $tiehi_lib_name/$tiehi_cell_name/[lindex $::env(TIEHI_CELL_AND_PORT) 1]
-    repair_tie_fanout -separation $::env(TIE_SEPARATION) $tiehi_pin
+  # Repair tie hi fanout
+  puts "Repair tie hi fanout..."
+  set tiehi_cell_name [lindex $::env(TIEHI_CELL_AND_PORT) 0]
+  set tiehi_lib_name [get_name [get_property [lindex [get_lib_cell $tiehi_cell_name] 0] library]]
+  set tiehi_pin $tiehi_lib_name/$tiehi_cell_name/[lindex $::env(TIEHI_CELL_AND_PORT) 1]
+  repair_tie_fanout -separation $::env(TIE_SEPARATION) $tiehi_pin
 }
 
-if {[env_var_exists_and_non_empty SWAP_ARITH_OPERATORS]} {
-    estimate_parasitics -placement
-    replace_arith_modules
+if { [env_var_exists_and_non_empty SWAP_ARITH_OPERATORS] } {
+  estimate_parasitics -placement
+  replace_arith_modules
 }
 
-if {[env_var_equals REMOVE_ABC_BUFFERS 1]} {
-    # remove buffers inserted by yosys/abc
-    remove_buffers
+if { [env_var_equals REMOVE_ABC_BUFFERS 1] } {
+  # remove buffers inserted by yosys/abc
+  remove_buffers
 } else {
-    # Skip clone & split
-    set ::env(SETUP_MOVE_SEQUENCE) "unbuffer,sizeup,swap,buffer,vt_swap"
-    set ::env(SKIP_LAST_GASP) 1
-    repair_timing_helper -setup
+  # Skip clone & split
+  set ::env(SETUP_MOVE_SEQUENCE) "unbuffer,sizeup,swap,buffer,vt_swap"
+  set ::env(SKIP_LAST_GASP) 1
+  repair_timing_helper -setup
 }
 
 puts "Default units for flow"
