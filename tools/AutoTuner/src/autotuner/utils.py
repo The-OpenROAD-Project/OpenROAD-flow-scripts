68564efd5e4a1be4fd4a5e3d1d151012b40c1b13
Vitor Bandeira <vvbandeira@precisioninno.com>
2025-08-26 11:15:26 +0000

    at: cast to int memory limit, ulimit does not parse floats
    Signed-off-by: Vitor Bandeira <vvbandeira@precisioninno.com>

--- tools/AutoTuner/src/autotuner/utils.py
+++ tools/AutoTuner/src/autotuner/utils.py
@@ -325,7 +325,8 @@ def openroad(
 
     make_command = export_command
     if args.memory_limit is not None:
-        make_command += f"ulimit -v {args.memory_limit * 1_000_000}; "
+        limit = int(args.memory_limit * 1_000_000)
+        make_command += f"ulimit -v {limit}; "
     make_command += f"make -C {base_dir}/flow DESIGN_CONFIG=designs/"
     make_command += f"{args.platform}/{args.design}/config.mk"
     make_command += f" PLATFORM={args.platform}"
