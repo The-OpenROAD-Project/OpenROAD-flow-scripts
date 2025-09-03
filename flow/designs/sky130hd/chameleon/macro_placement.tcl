a6a4e9e80d1730aa0eec7a729d057cc831990a66
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-09-03 19:29:45 -0300

    remove read_macro_placement and use MPL commands instead
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

---
+++ flow/designs/sky130hd/chameleon/macro_placement.tcl
@@ -0,0 +1,6 @@
+place_macro -macro_name {RAM.genblk1\[0\].RAM} -location {200 150} -orientation R0
+place_macro -macro_name {RAM.genblk1\[1\].RAM} -location {1600 150} -orientation R0
+place_macro -macro_name {RAM.genblk1\[2\].RAM} -location {200 1950} -orientation R180
+place_macro -macro_name {ahb_sys_0_uut.S0.CACHE} -location {2100 2650} -orientation R0
+place_macro -macro_name {ahb_sys_0_uut.apb_sys_inst_0} -location {1470 2643} -orientation R0
+place_macro -macro_name {ibex_core} -location {2150 1700} -orientation R0
