25b123784cd1ce84e2fe10734645d5c3674f2cb1
Arthur Koucher <arthurkoucher@precisioninno.com>
2025-09-09 16:51:06 -0300

    make ngt45/black_parrot our first test case for using MPL when there are preplaced macros
    Signed-off-by: Arthur Koucher <arthurkoucher@precisioninno.com>

--- flow/designs/sky130hd/microwatt/macro_placement.tcl
+++
@@ -1,4 +0,0 @@
-place_macro -macro_name soc0/bram.bram0/ram_0/memory_0 \
-  -location {110.4 2511.24} -orientation R0
-place_macro -macro_name soc0/processor/register_file_0/register_file_0 \
-  -location {110.4 111.4} -orientation R180
