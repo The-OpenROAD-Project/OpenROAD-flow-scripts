68992203421647e7670c2c27a81a12e51b7ef64b
Jaehyun Kim <jhkim@precisioninno.com>
2025-10-03 11:38:15 +0900

    Added finish__timing__hold__tns
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- flow/util/genRuleFile.py
+++ flow/util/genRuleFile.py
@@ -137,6 +137,14 @@ def gen_rule_file(
             "round_value": False,
             "compare": ">=",
         },
+        "finish__timing__hold__tns": {
+            "mode": "padding",
+            "padding": 30,
+            "min_max": min,
+            "min_max_direct": 0,
+            "round_value": False,
+            "compare": ">=",
+        },
         "finish__design__instance__area": {
             "mode": "padding",
             "padding": 15,
