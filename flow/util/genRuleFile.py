89824954f9565474773afdfda0d853d33e7f8000
Jaehyun Kim <jhkim@precisioninno.com>
2025-10-22 22:06:17 +0900

    Updated comment
    Signed-off-by: Jaehyun Kim <jhkim@precisioninno.com>

--- flow/util/genRuleFile.py
+++ flow/util/genRuleFile.py
@@ -58,6 +58,9 @@ def gen_rule_file(
     #     'min_max_direct': <float>, optional
     #     'min_max_sum': <float>, optional
     #     'min_max_period': <float>, optional
+    #     'level': <bool>, severity level. ['warning' or 
+    #                                        others (means ERROR, default) ]
+    #               
     # }
 
     rules_dict = {
