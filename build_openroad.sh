26d6be8df3ce66e9e68ccda65f898cc5040fe0c3
Noam Cohen <noam.chn1@gmail.com>
2025-12-13 14:17:12 +0200

    compiling kf

--- build_openroad.sh
+++ build_openroad.sh
@@ -261,7 +261,7 @@ __local_build()
         # CMAKE_FLAGS added to work around yosys-slang#141 (unable to build outside of git checkout)
         ${NICE} make install -C tools/yosys-slang -j "${PROC}" YOSYS_PREFIX="${INSTALL_PATH}/yosys/bin/" CMAKE_FLAGS="-DYOSYS_SLANG_REVISION=unknown -DSLANG_REVISION=unknown"
 
-        echo "[INFO FLW-0030] Compiling kepler-formal"
+        echo "[INFO FLW-0031] Compiling kepler-formal"
         # CMAKE_FLAGS added to work around yosys-slang#141 (unable to build outside of git checkout)
         ${NICE} make install -C tools/kepler-formal -j4  CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -march=native -ffast-math -flto" -DCMAKE_EXE_LINKER_FLAGS="-flto""
         
