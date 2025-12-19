e1305523f8f0af041f2679f8e3fd7499bd04e09d
Noam Cohen <noam.chn1@gmail.com>
2025-12-19 11:05:31 +0100

    fixes

--- flow/scripts/synth.sh
+++ flow/scripts/synth.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -u -eo pipefail
 mkdir -p $RESULTS_DIR $LOG_DIR $REPORTS_DIR $OBJECTS_DIR
-eval "$TIME_CMD $YOSYS_EXE $YOSYS_FLAGS -c $1" 2>&1 | tee -a $(realpath $2)
+$YOSYS_EXE -V > $(realpath $2)
+eval "$TIME_CMD $YOSYS_EXE $YOSYS_FLAGS -c $1" 2>&1 | tee --append $(realpath $2)
