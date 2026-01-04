f642b8f44dcf02ba6d0f788edfe23f60ee411999
Jeff Ng <jeffng@precisioninno.com>
2026-01-04 15:54:03 +0000

    Initial PDK 0.3 support
    Signed-off-by: Jeff Ng <jeffng@precisioninno.com>

--- flow/designs/rapidus2hp/ethmac/config.mk
+++ flow/designs/rapidus2hp/ethmac/config.mk
@@ -12,9 +12,15 @@ export VERILOG_FILES          = $(sort $(wildcard $(SRC_HOME)/*.v))
 export SDC_FILE               = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 export ABC_AREA               = 1
 
-export CORE_UTILIZATION = $(strip $(if $(filter 0.3,$(RAPIDUS_PDK_VERSION)), \
-	65, \
-	70))
+export CORE_UTILIZATION = $(strip \
+    $(if $(filter 0.15,$(RAPIDUS_PDK_VERSION)), \
+	$(if $(filter ra02h138_DST_45CPP,$(PLACE_SITE)), \
+		63, \
+		65), \
+        $(if $(filter 0.3,$(RAPIDUS_PDK_VERSION)), \
+	    65, \
+            70) \
+    ))
 
 export CORE_ASPECT_RATIO      = 1
 export CORE_MARGIN            = 0.75
