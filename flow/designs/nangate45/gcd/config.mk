77848ae1bdbeda82457ecc04d4c3eb9a98586f27
Øyvind Harboe <oyvind.harboe@zylin.com>
2026-01-15 13:16:59 +0100

    synth: improve canonicalization, introduce SYNTH_REPEATABLE_BUILD
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

--- flow/designs/nangate45/gcd/config.mk
+++ flow/designs/nangate45/gcd/config.mk
@@ -12,6 +12,7 @@ export CORE_UTILIZATION ?= 55
 export PLACE_DENSITY_LB_ADDON = 0.20
 export TNS_END_PERCENT        = 100
 export REMOVE_CELLS_FOR_EQY   = TAPCELL*
+export SYNTH_REPEATABLE_BUILD ?= 1
 
 # This needs a smaller pitch to accomodate a small block
 export PDN_TCL ?= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/grid_strategy-M1-M4-M7.tcl
