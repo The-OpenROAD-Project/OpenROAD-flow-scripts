21a69ed90ee3e4a2f03a859bd7c346d9acf0d854
Divinesoumyadip <soumyacode7@gmail.com>
2026-04-03 05:12:41 +0000

    setup: refactor to use helper function for SUDO_USER handling
    Signed-off-by: Divinesoumyadip <soumyacode7@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,16 +5,20 @@ set -euo pipefail
 # allow this script to be invoked from any folder
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ "$OSTYPE" == "darwin"* ]] && [[ $EUID -eq 0 ]]; then
-  echo "Do NOT run this script with sudo on macOS"
+if [ $EUID -ne 0 ]; then
+  echo "This script must be run with sudo"
   exit 1
 fi
 
-if [[ "$OSTYPE" != "darwin"* ]] && [[ $EUID -ne 0 ]]; then
-  echo "This script must be run with sudo on Linux"
-  exit 1
-fi
 
+# Run a command as the original user if invoked via sudo, otherwise run directly
+run_as_user() {
+  if [[ -n "${SUDO_USER:-}" ]]; then
+    sudo -u "$SUDO_USER" "$@"
+  else
+    "$@"
+  fi
+}
 tmpfile=$(mktemp)
 # any error messages from this command will stand out
 # more clearly when run as a separate command rather than
@@ -22,15 +26,15 @@ tmpfile=$(mktemp)
 git submodule status --recursive > "$tmpfile"
 
 if grep -q "^-" "$tmpfile"; then
+<<<<<<< Updated upstream
+  sudo -u $SUDO_USER git submodule update --init --recursive
+=======
   if [[ "$OSTYPE" == "darwin"* ]]; then
     git submodule update --init --recursive
   else
-    if [[ -n "${SUDO_USER:-}" ]]; then
-      sudo -u "$SUDO_USER" git submodule update --init --recursive
-    else
-      git submodule update --init --recursive
-    fi
+    run_as_user git submodule update --init --recursive
   fi
+>>>>>>> Stashed changes
 elif grep -q "^+" "$tmpfile"; then
   # Make it easy for users who are not hacking ORFS to do the right thing,
   # run with current submodules, at the cost of having ORFS
@@ -40,12 +44,12 @@ elif grep -q "^+" "$tmpfile"; then
 fi
 
 "$DIR/etc/DependencyInstaller.sh" -base
+<<<<<<< Updated upstream
+sudo -u $SUDO_USER "$DIR/etc/DependencyInstaller.sh" -common -prefix="$DIR/dependencies"
+=======
 if [[ "$OSTYPE" == "darwin"* ]]; then
   "$DIR/etc/DependencyInstaller.sh" -common -prefix="$DIR/dependencies"
 else
-  if [[ -n "${SUDO_USER:-}" ]]; then
-    sudo -u "$SUDO_USER" "$DIR/etc/DependencyInstaller.sh" -common -prefix="$DIR/dependencies"
-  else
-    "$DIR/etc/DependencyInstaller.sh" -common -prefix="$DIR/dependencies"
-  fi
+  run_as_user "$DIR/etc/DependencyInstaller.sh" -common -prefix="$DIR/dependencies"
 fi
+>>>>>>> Stashed changes
