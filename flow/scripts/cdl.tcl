9556e77f479be1634a58f48a64ed0459aee0b75a
Daniel Schultz <dnltz@aesc-silicon.de>
2026-04-14 15:09:10 +0200

    flow: scripts: include filler cells in CDL netlist for LVS accuracy
    Pass -include_fillers to write_cdl so filler cells appear in the
    exported CDL netlist. Without this, LVS tools may flag filler
    instances as unresolved.
    
    This change is required to run LVS tests with the IHP PDK.
    
    Signed-off-by: Daniel Schultz <dnltz@aesc-silicon.de>

--- flow/scripts/cdl.tcl
+++ flow/scripts/cdl.tcl
@@ -2,4 +2,5 @@ source $::env(SCRIPTS_DIR)/load.tcl
 erase_non_stage_variables final
 load_design 6_final.odb 6_final.sdc
 
-write_cdl -masters $::env(CDL_FILE) $::env(RESULTS_DIR)/6_final.cdl
+write_cdl -masters $::env(CDL_FILE) -include_fillers \
+  $::env(RESULTS_DIR)/6_final.cdl
