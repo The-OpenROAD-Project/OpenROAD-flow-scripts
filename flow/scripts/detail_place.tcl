38cc9dd5618961bf949db9bccfc19995d99dd69a
Augusto Berndt <augusto.berndt@precisioninno.com>
2026-05-22 18:14:35 +0000

    activate debug negotiation print
    Signed-off-by: Augusto Berndt <augusto.berndt@precisioninno.com>

--- flow/scripts/detail_place.tcl
+++ flow/scripts/detail_place.tcl
@@ -16,6 +16,7 @@ proc do_dpl { } {
     -left $::env(CELL_PAD_IN_SITES_DETAIL_PLACEMENT) \
     -right $::env(CELL_PAD_IN_SITES_DETAIL_PLACEMENT)
   set dpl_args [env_var_or_empty DETAIL_PLACEMENT_ARGS]
+  set_debug_level DPL negotiation 1
   append_env_var dpl_args DPL_USE_OLD_DIAMOND -use_old_diamond 0
   log_cmd detailed_placement {*}$dpl_args
 
