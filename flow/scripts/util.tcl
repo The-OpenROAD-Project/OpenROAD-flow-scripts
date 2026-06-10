f853b11d0e765b172ae9b2c8f50cd0369cd7d1a2
dsengupta0628 <dsengupta@precisioninno.com>
2026-06-10 20:22:57 +0000

    cleanup
    Signed-off-by: dsengupta0628 <dsengupta@precisioninno.com>

--- flow/scripts/util.tcl
+++ flow/scripts/util.tcl
@@ -75,7 +75,7 @@ proc recover_power_helper { } {
 
 proc extract_stage { input_file } {
   # Match the stage prefix on the basename, not the full path: an ancestor
-  # dir like ".../4_slices/3_place.odb" would otherwise match "4_" -> stage 4.
+  # dir like ".../4_something/3_place.odb" would otherwise match "4_" -> stage 4.
   set stage_file [file tail $input_file]
   if { ![regexp {^([0-9])_(?:([0-9])_)?} $stage_file match num1 num2] } {
     puts "Error: Could not determine design stage from $input_file"
