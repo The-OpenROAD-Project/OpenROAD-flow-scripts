50fc1e60b0e84e3c6d69c6f175bcd6450cf20299
Noam Cohen <noam.cohen@keplertech.io>
2026-06-26 14:02:46 +0200

    global lec by default
    Signed-off-by: Noam Cohen <noam.chn1@gmail.com>

--- flow/settings.mk
+++ flow/settings.mk
@@ -1,6 +1,6 @@
-# Enable formal checks only if kepler-formal is installed.
+# Enable LEC only if kepler-formal is installed.
 # kepler-formal is primarily an OpenROAD/ORFS developer tool, not an end-user
 # tool. End-users would typically run these checks transactionally at project
 # completion, not in every CI run where it wastes CI time.
 export LEC_CHECK ?= $(if $(wildcard $(KEPLER_FORMAL_EXE)),1,0)
-export SEC_CHECK ?= $(if $(wildcard $(KEPLER_FORMAL_EXE)),1,0)
+export SEC_CHECK ?= 0
