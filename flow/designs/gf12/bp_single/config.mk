b900d7f38da9fa42f8be25bd18011935dbcb1bc7
João Mai <jmai@precisioninno.com>
2026-08-14 02:32:21 +0000

    gf12/bp_single: restore place density
    Signed-off-by: João Mai <jmai@precisioninno.com>

--- flow/designs/gf12/bp_single/config.mk
+++ flow/designs/gf12/bp_single/config.mk
@@ -46,9 +46,9 @@ export FOOTPRINT_TCL   = $(PLATFORM_DIR)/bp/footprint.tcl
 export DIE_AREA =    0   0 3000 3000
 export CORE_AREA = 200 200 2800 2800
 
-export LEC_CHECK = 0
-
 export ABC_CLOCK_PERIOD_IN_PS = 1250
+
+export PLACE_DENSITY = 0.80
 
 export MACRO_WRAPPERS = $(PLATFORM_DIR)/bp/wrappers/wrappers.tcl
 export RTLMP_FENCE_LX = 850
