aac02578dee251845ec768a2eeb83a7355deb4ac
Mohammed <99575128+GITTYCAT12@users.noreply.github.com>
2026-09-01 02:08:15 +0530

    setup.sh: make submodule checks independent of working directory

--- setup.sh
+++ setup.sh
@@ -19,13 +19,13 @@ tmpfile=$(mktemp)
 # any error messages from this command will stand out
 # more clearly when run as a separate command rather than
 # being piped
-git submodule status --recursive > "$tmpfile"
+git -C "$DIR" submodule status --recursive > "$tmpfile"
 
 if grep -q "^-" "$tmpfile"; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    git submodule update --init --recursive
+    git -C "$DIR" submodule update --init --recursive
   else
-    sudo -u $SUDO_USER git submodule update --init --recursive
+    sudo -u $SUDO_USER git -C "$DIR" submodule update --init --recursive
   fi
 elif grep -q "^+" "$tmpfile"; then
   # Make it easy for users who are not hacking ORFS to do the right thing,
